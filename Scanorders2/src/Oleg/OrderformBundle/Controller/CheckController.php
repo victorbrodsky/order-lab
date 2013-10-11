3d5c2e155887ea6368e30a1bae13f6703b43b9dc
unknown <oli2002@collage.a.wcmc-ad.net>
2013-10-11 17:02:40 -0400

    working on form check: get the form from db if exists

---
+++ Scanorders2/src/Oleg/OrderformBundle/Controller/CheckController.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Oleg\OrderformBundle\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Component\HttpFoundation\JsonResponse;
+
+use Oleg\OrderformBundle\Form\PatientType;
+
+/**
+ * OrderInfo controller.
+ *
+ * @Route("/check")
+ * @Template("OlegOrderformBundle:Patient:edit.html.twig")
+ */
+class CheckController extends Controller {
+      
+    /**
+     * @Route("/patient", name="get-patient")
+     * @Method("GET")
+     */
+    public function getPatientAction() {
+
+        $request = $this->get('request');
+        $id   = $request->get('mrn');
+        $id = "NOMRNPROVIDED-0000000001";
+
+        $em = $this->getDoctrine()->getManager();
+        $entity = $em->getRepository('OlegOrderformBundle:Patient')->findOneByMrn($id);
+
+        $form   = $this->createForm(new PatientType(), $entity);
+
+        $delete_form = $this->createFormBuilder(array('id' => $id))
+            ->add('id', 'hidden')
+            ->getForm();
+
+        return array(
+            'entity'   => $entity,
+            'edit_form'   => $form->createView(),
+            'delete_form' => $delete_form->createView(),
+        );
+    }
+    
+
+    
+}
