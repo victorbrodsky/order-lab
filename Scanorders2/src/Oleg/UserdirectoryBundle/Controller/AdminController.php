a0eb8d62863e728627ceb7fe1c9f86a8d15eec39
Admin <Admin>
2015-12-08 17:37:52 -0500

    add br to about page

--- Scanorders2/src/Oleg/UserdirectoryBundle/Controller/AdminController.php
+++ Scanorders2/src/Oleg/UserdirectoryBundle/Controller/AdminController.php
@@ -915,9 +915,13 @@ class AdminController extends Controller
                         <a href="http://www.ghostscript.com/">Ghostscript</a>
                     </li>
 
+                    <br>
+
                     <li>
                         <a href="https://github.com/bermi/password-generator">Password Generator</a>
                     </li>
+
+                    <br>
 
                     <li>
                         <a href="https://github.com/andreausu/UsuScryptPasswordEncoderBundle">Password Encoder</a>
