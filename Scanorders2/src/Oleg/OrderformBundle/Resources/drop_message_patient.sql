caf6e88305ac59815f7cae874a45a818c695bbea
Oleg Ivanov <oli2002@med.cornell.edu>
2015-12-15 15:32:28 -0500

    fixed sql

--- Scanorders2/src/Oleg/OrderformBundle/Resources/drop_message_patient.sql
+++ Scanorders2/src/Oleg/OrderformBundle/Resources/drop_message_patient.sql
@@ -1,3 +1,12 @@
 -- Drop message and patient hierarchy
 
+-- Patient
 
+-- Encounter
+
+-- Procedure
+
+-- Accession
+DROP TABLE [ScanOrder].[dbo].[scan_accessionDate];
+DROP TABLE [ScanOrder].[dbo].[scan_accessionaccession];
+DROP TABLE [ScanOrder].[dbo].[scan_accession];
--- Scanorders2/src/Oleg/OrderformBundle/Resources/drop_stains.sql
+++ Scanorders2/src/Oleg/OrderformBundle/Resources/drop_stains.sql
@@ -1,6 +1,6 @@
 -- Stain list has two dependencies:
 -- scan_stain and scan_blockSpecialStain tables which have to be dropped first before dropping scan_stainList table
 
-DROP TABLE dbo.scan_blockSpecialStain;
-DROP TABLE dbo.scan_stain;
-DROP TABLE dbo.scan_stainlist;
+DROP TABLE [ScanOrder].[dbo].[scan_blockSpecialStains];
+DROP TABLE [ScanOrder].[dbo].[scan_stain];
+DROP TABLE [ScanOrder].[dbo].[scan_stainlist];
