bacd007619240505f586db66a7f2c3559f6a062c
Oleg Ivanov <oli2002@med.cornell.edu>
2015-12-15 14:21:15 -0500

    added drop_stains.sql

--- Scanorders2/src/Oleg/OrderformBundle/Resources/drop_stains.sql
+++ Scanorders2/src/Oleg/OrderformBundle/Resources/drop_stains.sql
@@ -1,1 +1,6 @@
-DROP TABLE ProductVendor1 ;
+-- Stain list has two dependencies:
+-- scan_stain and scan_blockSpecialStain tables which have to be dropped first before dropping scan_stainList table
+
+DROP TABLE dbo.scan_blockSpecialStain;
+DROP TABLE dbo.scan_stain;
+DROP TABLE dbo.scan_stainlist;
