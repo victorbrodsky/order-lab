c40815a70a7bb23a5cd3ec388db5618044af5f78
Oleg Ivanov <oli2002@med.cornell.edu>
2016-02-11 13:17:24 -0500

    fixed order list

--- Scanorders2/src/Oleg/OrderformBundle/Controller/ScanOrderController.php
+++ Scanorders2/src/Oleg/OrderformBundle/Controller/ScanOrderController.php
@@ -182,7 +182,7 @@ class ScanOrderController extends Controller {
 //            $dql = $dql . " ORDER BY $postData[sort] $postData[direction]";
 //        }
 
-        //echo "order List Action: dql=".$dql;
+        echo "order List Action: dql=".$dql;
 
         if( $increaseMaxExecTime ) {
             $max_exec_time = ini_get('max_execution_time');
@@ -1484,7 +1484,7 @@ class ScanOrderController extends Controller {
                 if( $crituser != "" ) {
                     $crituser .= " AND ";
                 }
-                $crituser .= $institutionalCriteriaStr;
+                $crituser .= "(" . $institutionalCriteriaStr . ")";
                 echo "crituser=".$crituser."<br>";
 
                 $institution = true;
