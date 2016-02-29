a321dc0c99f4c719732e345a930c5e1dd74ef1ff
Oleg Ivanov <oli2002@med.cornell.edu>
2016-02-29 16:14:17 -0500

    working on #467 (5)

--- Scanorders2/src/Oleg/UserdirectoryBundle/Controller/AccessRequestController.php
+++ Scanorders2/src/Oleg/UserdirectoryBundle/Controller/AccessRequestController.php
@@ -432,7 +432,7 @@ class AccessRequestController extends Controller
             array(
                 'defaultSortFieldName' => 'accreq.createdate',
                 'defaultSortDirection' => 'DESC',
-                'wrap-queries'=>true
+                //'wrap-queries'=>true
             )
         );
 
