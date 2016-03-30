d1439d6fc4227f9b09977f8888131c8cf4e37428
Oleg Ivanov <oli2002@med.cornell.edu>
2016-03-30 15:10:51 -0400

    testing delete sheet from server

--- Scanorders2/src/Oleg/FellAppBundle/Util/GoogleSheetManagement.php
+++ Scanorders2/src/Oleg/FellAppBundle/Util/GoogleSheetManagement.php
@@ -511,13 +511,13 @@ class GoogleSheetManagement {
 
             //continue; //testing
 
-            //$this->em->remove($document);
-            //$this->em->flush();
+            $this->em->remove($document);
+            $this->em->flush();
 
             //remove file from folder
             if( is_file($documentPath) ) {
                 echo "file exists!!! ";
-                //unlink($documentPath);
+                unlink($documentPath);
             } else {
                 echo "file does exists??? ";
             }
