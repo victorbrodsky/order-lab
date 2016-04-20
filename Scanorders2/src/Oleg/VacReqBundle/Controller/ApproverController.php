ddb776d52268d3c265a8bcf9914c4a519d1199d9
Oleg Ivanov <oli2002@med.cornell.edu>
2016-04-20 17:58:42 -0400

    working on group managemenet

--- Scanorders2/src/Oleg/VacReqBundle/Controller/ApproverController.php
+++ Scanorders2/src/Oleg/VacReqBundle/Controller/ApproverController.php
@@ -49,13 +49,11 @@ class ApproverController extends Controller
 
 
     /**
-     * Creates a new VacReqRequest entity.
-     *
-     * @Route("/organizational-institution-group/{institutionId}", name="vacreq_organizationalinstitutions")
+     * @Route("/organizational-institutions/{institutionId}", name="vacreq_orginst_list")
      * @Method({"GET", "POST"})
-     * @Template("OlegVacReqBundle:Approver:organizational-institution-group.html.twig")
+     * @Template("OlegVacReqBundle:Approver:orginst-list.html.twig")
      */
-    public function OrganizationalInstitutionAction(Request $request, $institutionId)
+    public function organizationalInstitutionAction(Request $request, $institutionId)
     {
 
         if( false == $this->get('security.context')->isGranted('ROLE_VACREQ_APPROVER') || false == $this->get('security.context')->isGranted('ROLE_VACREQ_ADMIN') ) {
@@ -65,8 +63,6 @@ class ApproverController extends Controller
         //echo " => institutionId=".$institutionId."<br>";
 
         $em = $this->getDoctrine()->getManager();
-
-        $user = $this->get('security.context')->getToken()->getUser();
 
         //find role approvers by institution
         $approvers = array();
@@ -97,5 +93,55 @@ class ApproverController extends Controller
         );
     }
 
+
+
+    /**
+     * Creates a new VacReqRequest entity.
+     *
+     * @Route("/organizational-institution-management/{institutionId}", name="vacreq_orginst_management")
+     * @Method({"GET", "POST"})
+     * @Template("OlegVacReqBundle:Approver:orginst-management.html.twig")
+     */
+    public function orgInstManagementAction(Request $request, $institutionId)
+    {
+
+        if( false == $this->get('security.context')->isGranted('ROLE_VACREQ_APPROVER') || false == $this->get('security.context')->isGranted('ROLE_VACREQ_ADMIN') ) {
+            return $this->redirect( $this->generateUrl('vacreq-nopermission') );
+        }
+
+        //echo " => institutionId=".$institutionId."<br>";
+
+        $em = $this->getDoctrine()->getManager();
+
+        //$user = $this->get('security.context')->getToken()->getUser();
+
+        //find role approvers by institution
+        $approvers = array();
+        $roleApprovers = $em->getRepository('OlegUserdirectoryBundle:User')->findRolesBySiteAndPartialRoleName( "vacreq", 'ROLE_VACREQ_APPROVER', $institutionId);
+        $roleApprover = $roleApprovers[0];
+        //echo "roleApprover=".$roleApprover."<br>";
+        if( $roleApprover ) {
+            $approvers = $em->getRepository('OlegUserdirectoryBundle:User')->findUserByRole($roleApprover->getName());
+        }
+        //echo "approvers=".count($approvers)."<br>";
+
+        //find role submitters by institution
+        $submitters = array();
+        $roleSubmitters = $em->getRepository('OlegUserdirectoryBundle:User')->findRolesBySiteAndPartialRoleName( "vacreq", 'ROLE_VACREQ_SUBMITTER', $institutionId);
+        $roleSubmitter = $roleSubmitters[0];
+        //echo "roleSubmitter=".$roleSubmitter."<br>";
+        if( $roleSubmitter ) {
+            $submitters = $em->getRepository('OlegUserdirectoryBundle:User')->findUserByRole($roleSubmitter->getName());
+        }
+
+        $organizationalGroupInstitution = $em->getRepository('OlegUserdirectoryBundle:Institution')->find($institutionId);
+
+        return array(
+            'approvers' => $approvers,
+            'submitters' => $submitters,
+            'organizationalGroupId' => $institutionId,
+            'organizationalGroupName' => $organizationalGroupInstitution.""
+        );
+    }
 
 }
