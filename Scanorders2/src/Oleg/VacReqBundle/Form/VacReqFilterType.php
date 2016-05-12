1cc3b53b258370c0ae20e02aaf1608d8f5682e23
Oleg Ivanov <oli2002@med.cornell.edu>
2016-05-12 14:58:14 -0400

    fixed academic year filter

--- Scanorders2/src/Oleg/VacReqBundle/Form/VacReqFilterType.php
+++ Scanorders2/src/Oleg/VacReqBundle/Form/VacReqFilterType.php
@@ -58,7 +58,7 @@ class VacReqFilterType extends AbstractType
                 'widget' => 'single_text',
                 'required' => false,
                 'format' => 'yyyy',
-                'attr' => array('class'=>'datepicker-only-year form-control datepicker-ignore', 'placeholder' => 'Academic Year', 'title'=>'Academic Year', 'data-toggle'=>'tooltip'),
+                'attr' => array('class'=>'datepicker-only-year form-control', 'placeholder' => 'Academic Year', 'title'=>'Academic Year', 'data-toggle'=>'tooltip'),
             ));
 
             $this->addGroup($builder);
