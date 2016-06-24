32a590c6dd8a08dd6148f1ff3bdaf76072575ab5
Oleg Ivanov <oli2002@med.cornell.edu>
2016-06-24 14:50:58 -0400

    testing spool email

--- Scanorders2/src/Oleg/VacReqBundle/Util/VacReqUtil.php
+++ Scanorders2/src/Oleg/VacReqBundle/Util/VacReqUtil.php
@@ -358,7 +358,7 @@ class VacReqUtil
         //$emailUtil->sendEmail( $submitter->getSingleEmail(), $subject, $message, $cssArr, null );
 
         $subject = "Copy of the email: ".$subject;
-        $addText = "### This is a copy of the email sent to the approvers ".implode(", ",$approversNameArr)."###";
+        $addText = "### This is a copy of the email sent to the approvers ".implode("; ",$approversNameArr)."###";
         $message = $addText.$break.$break.$message;
         $emailUtil->sendEmail( $cssArr, $subject, $message, null, null );
     }
@@ -2512,7 +2512,7 @@ class VacReqUtil
 
         } //foreach approver
 
-        $message = str_replace("###emailuser###",implode(", ",$approversShortNameArr),$originalMessage);
+        $message = str_replace("###emailuser###",implode("; ",$approversShortNameArr),$originalMessage);
 
         if( count($approverEmailArr) > 0 ) {
             $emailUtil->sendEmail($approverEmailArr, $subject, $message, null, null);
@@ -2520,7 +2520,7 @@ class VacReqUtil
 
         //send email to email users
         $subject = "Copy of the email: ".$subject;
-        $addText = "### This is a copy of the email sent to the approvers ".implode(", ",$approversNameArr)."###";
+        $addText = "### This is a copy of the email sent to the approvers ".implode("; ",$approversNameArr)."###";
         $message = $addText.$break.$break.$message;
         $settings = $this->getSettingsByInstitution($institution->getId());
         if( $settings ) {
