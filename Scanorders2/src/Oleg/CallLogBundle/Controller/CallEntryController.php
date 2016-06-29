9a9b0b1816a3f8b5bca7ea3f79de4d2972805110
Oleg Ivanov <oli2002@med.cornell.edu>
2016-06-29 15:04:34 -0400

    working patient creation

--- Scanorders2/src/Oleg/CallLogBundle/Controller/CallEntryController.php
+++ Scanorders2/src/Oleg/CallLogBundle/Controller/CallEntryController.php
@@ -167,66 +167,66 @@ class CallEntryController extends Controller
             return $this->redirect($this->generateUrl('calllog-nopermission'));
         }
 
-        $entities = null;
-
         //$allgets = $request->query->all();;
-        $mrn = trim($request->get('mrn'));
-        $mrntype = trim($request->get('mrntype'));
-        $dob = trim($request->get('dob'));
-        $lastname = trim($request->get('lastname'));
-        $firstname = trim($request->get('firstname'));
-        //print_r($allgets);
-        //echo "mrn=".$mrn."<br>";
+//        $mrn = trim($request->get('mrn'));
+//        $mrntype = trim($request->get('mrntype'));
+//        $dob = trim($request->get('dob'));
+//        $lastname = trim($request->get('lastname'));
+//        $firstname = trim($request->get('firstname'));
+//        //print_r($allgets);
+//        //echo "mrn=".$mrn."<br>";
 
-        $em = $this->getDoctrine()->getManager();
-
-        $parameters = array('status' => 'valid');
-
-        $repository = $em->getRepository('OlegOrderformBundle:Patient');
-        $dql = $repository->createQueryBuilder("patient");
-        $dql->leftJoin("patient.mrn", "mrn");
-        $dql->leftJoin("patient.dob", "dob");
-        $dql->leftJoin("patient.lastname", "lastname");
-        $dql->leftJoin("patient.firstname", "firstname");
-
-        $dql->where("mrn.status = :status");
-
-        $where = false;
-
-        //mrn
-        if( $mrntype && $mrn ) {
-            $dql->andWhere("mrn.keytype = :keytype AND mrn.field = :mrn");
-            $parameters['keytype'] = $mrntype;
-            $parameters['mrn'] = $mrn;
-            $where = true;
-        }
-
-        //Last Name AND DOB
-        if( $where == false && $lastname && $dob ) {
-            $dobDateTime = \DateTime::createFromFormat('m/d/Y', $dob)->format('Y-m-d');
-            //echo "dob=".$dob." => ".$dobDateTime."<br>";
-            $dql->andWhere("lastname.field = :lastname AND dob.field = :dob");
-            $parameters['lastname'] = $lastname;
-            $parameters['dob'] = $dobDateTime;
-            $where = true;
-        }
-
-//        //firstname, Last Name AND DOB
-//        if( $lastname && $firstname && $dob ) {
-//            $dql->andWhere("lastname.field = :lastname AND firstname.field = :firstname");
-//            $parameters['lastname'] = $lastname;
-//            $parameters['firstname'] = $firstname;
+//        $em = $this->getDoctrine()->getManager();
+//
+//        $parameters = array('status' => 'valid');
+//
+//        $repository = $em->getRepository('OlegOrderformBundle:Patient');
+//        $dql = $repository->createQueryBuilder("patient");
+//        $dql->leftJoin("patient.mrn", "mrn");
+//        $dql->leftJoin("patient.dob", "dob");
+//        $dql->leftJoin("patient.lastname", "lastname");
+//        $dql->leftJoin("patient.firstname", "firstname");
+//
+//        $dql->where("mrn.status = :status");
+//
+//        $where = false;
+//
+//        //mrn
+//        if( $mrntype && $mrn ) {
+//            $dql->andWhere("mrn.keytype = :keytype AND mrn.field = :mrn");
+//            $parameters['keytype'] = $mrntype;
+//            $parameters['mrn'] = $mrn;
 //            $where = true;
 //        }
+//
+//        //Last Name AND DOB
+//        if( $where == false && $lastname && $dob ) {
+//            $dobDateTime = \DateTime::createFromFormat('m/d/Y', $dob)->format('Y-m-d');
+//            //echo "dob=".$dob." => ".$dobDateTime."<br>";
+//            $dql->andWhere("lastname.field = :lastname AND dob.field = :dob");
+//            $parameters['lastname'] = $lastname;
+//            $parameters['dob'] = $dobDateTime;
+//            $where = true;
+//        }
+//
+////        //firstname, Last Name AND DOB
+////        if( $lastname && $firstname && $dob ) {
+////            $dql->andWhere("lastname.field = :lastname AND firstname.field = :firstname");
+////            $parameters['lastname'] = $lastname;
+////            $parameters['firstname'] = $firstname;
+////            $where = true;
+////        }
+//
+//        if( $where ) {
+//            $query = $em->createQuery($dql);
+//            $query->setParameters($parameters);
+//            //echo "sql=".$query->getSql()."<br>";
+//            $patients = $query->getResult();
+//        } else {
+//            $patients = array();
+//        }
 
-        if( $where ) {
-            $query = $em->createQuery($dql);
-            $query->setParameters($parameters);
-            //echo "sql=".$query->getSql()."<br>";
-            $patients = $query->getResult();
-        } else {
-            $patients = array();
-        }
+        $patients = $this->searchPatient( $request );
         //echo "patients=".count($patients)."<br>";
 
         $patientsArr = array();
@@ -320,4 +320,145 @@ class CallEntryController extends Controller
         return $response;
     }
 
+    public function searchPatient( $request, $params=null ) {
+
+        $mrn = trim($request->get('mrn'));
+        $mrntype = trim($request->get('mrntype'));
+        $dob = trim($request->get('dob'));
+        $lastname = trim($request->get('lastname'));
+        $firstname = trim($request->get('firstname'));
+        //print_r($allgets);
+        //echo "mrn=".$mrn."<br>";
+
+        if( $params ) {
+            $mrntype = ( array_key_exists('mrntype', $params) ? $params['mrntype'] : null);
+            $mrn = ( array_key_exists('mrn', $params) ? $params['mrn'] : null);
+            $dob = ( array_key_exists('dob', $params) ? $params['dob'] : null);
+            $lastname = ( array_key_exists('lastname', $params) ? $params['lastname'] : null);
+            $firstname = ( array_key_exists('firstname', $params) ? $params['firstname'] : null);
+        }
+
+        $em = $this->getDoctrine()->getManager();
+
+        $parameters = array('status' => 'valid');
+
+        $repository = $em->getRepository('OlegOrderformBundle:Patient');
+        $dql = $repository->createQueryBuilder("patient");
+        $dql->leftJoin("patient.mrn", "mrn");
+        $dql->leftJoin("patient.dob", "dob");
+        $dql->leftJoin("patient.lastname", "lastname");
+        $dql->leftJoin("patient.firstname", "firstname");
+
+        $dql->where("mrn.status = :status");
+
+        $where = false;
+
+        //mrn
+        if( $mrntype && $mrn ) {
+            $dql->andWhere("mrn.keytype = :keytype AND mrn.field = :mrn");
+            $parameters['keytype'] = $mrntype;
+            $parameters['mrn'] = $mrn;
+            $where = true;
+        }
+
+        //Last Name AND DOB
+        if( $where == false && $lastname && $dob ) {
+            $dobDateTime = \DateTime::createFromFormat('m/d/Y', $dob)->format('Y-m-d');
+            //echo "dob=".$dob." => ".$dobDateTime."<br>";
+            $dql->andWhere("lastname.field = :lastname AND dob.field = :dob");
+            $parameters['lastname'] = $lastname;
+            $parameters['dob'] = $dobDateTime;
+            $where = true;
+        }
+
+//        //firstname, Last Name AND DOB
+//        if( $lastname && $firstname && $dob ) {
+//            $dql->andWhere("lastname.field = :lastname AND firstname.field = :firstname");
+//            $parameters['lastname'] = $lastname;
+//            $parameters['firstname'] = $firstname;
+//            $where = true;
+//        }
+
+        if( $where ) {
+            $query = $em->createQuery($dql);
+            $query->setParameters($parameters);
+            //echo "sql=".$query->getSql()."<br>";
+            $patients = $query->getResult();
+        } else {
+            $patients = array();
+        }
+
+        return $patients;
+    }
+
+
+    /**
+     * Create a new Patient
+     * @Route("/patient/create", name="calllog_create_patient", options={"expose"=true})
+     * @Method("GET")
+     * @Template()
+     */
+    public function createPatientAction(Request $request)
+    {
+        //TODO: The server should DOUBLECHECK that the user has a role with a permission of "Create Patient Record"
+        if (false == $this->get('security.context')->isGranted('ROLE_CALLLOG_USER')) {
+            return $this->redirect($this->generateUrl('calllog-nopermission'));
+        }
+
+        $em = $this->getDoctrine()->getManager();
+
+        $mrn = trim($request->get('mrn'));
+        $mrntype = trim($request->get('mrntype'));
+        $dob = trim($request->get('dob'));
+        $lastname = trim($request->get('lastname'));
+        $firstname = trim($request->get('firstname'));
+        $middlename = trim($request->get('middlename'));
+        $suffix = trim($request->get('suffix'));
+        $sex = trim($request->get('sex'));
+        //print_r($allgets);
+        //echo "mrn=".$mrn."<br>";
+
+        $output = 'OK';
+        $response = new Response();
+        $response->headers->set('Content-Type', 'application/json');
+
+        //first check if the patient already exists
+        $patients = $this->searchPatient( $request );
+        if( count($patients) > 0 ) {
+            $output = "Can not create a new Patient. The patient with specified parameters already exists:<br>";
+
+            if( $mrntype ) {
+                $mrntypeObj = $em->getRepository('OlegOrderformBundle:MrnType')->findOneById( $mrntype );
+                $output .= "MRN Type:".$mrntypeObj."<br>";
+            }
+            if( $mrn )
+                $output .= "MRN:".$mrn."<br>";
+            if( $lastname )
+                $output .= "Last Name:".$lastname."<br>";
+            //if( $firstname )
+            //    $output .= "First Name:".$firstname."<br>";
+            if( $dob )
+                $output .= "DOB:".$dob."<br>";
+
+            $response->setContent(json_encode($output));
+            return $response;
+        }
+
+        //Create a new Patient
+        $user = $this->get('security.context')->getToken()->getUser();
+        $securityUtil = $this->get('order_security_utility');
+        $sourcesystem = $securityUtil->getDefaultSourceSystem(); //'scanorder';
+        $status = 'valid';
+
+        $patient = new Patient(false,$status,$user,$sourcesystem);
+        $patient->addMrn( new PatientMrn($status,$user,$sourcesystem) );
+        $patient->addDob( new PatientDob($status,$user,$sourcesystem) );
+
+        $encounter = new Encounter(false,$status,$user,$sourcesystem);
+        $patient->addEncounter($encounter);
+
+
+        $response->setContent(json_encode($output));
+        return $response;
+    }
 }
