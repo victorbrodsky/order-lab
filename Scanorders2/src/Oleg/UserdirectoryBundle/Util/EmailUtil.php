1cd5ccf2f34de3f9b0994e767ad47c2a7cba3455
Oleg Ivanov <oli2002@med.cornell.edu>
2016-07-11 10:37:15 -0400

    add siteEmail to emailer

--- Scanorders2/src/Oleg/UserdirectoryBundle/Util/EmailUtil.php
+++ Scanorders2/src/Oleg/UserdirectoryBundle/Util/EmailUtil.php
@@ -81,6 +81,12 @@ class EmailUtil {
         );
 
         if( $ccs ) {
+
+            //send copy email to siteEmail
+            $userSecUtil = $this->container->get('user_security_utility');
+            $siteEmail = $userSecUtil->getSiteSettingParameter('siteEmail');
+            $ccs[] = $siteEmail;
+
             $transport->setCc($ccs);
         }
 
