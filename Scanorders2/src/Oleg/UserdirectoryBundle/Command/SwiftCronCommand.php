0ee1697dfb88b8fb19a2f2a5ab1a48422c193060
Oleg Ivanov <oli2002@med.cornell.edu>
2016-07-22 12:53:56 -0400

    testing swift spool file

--- Scanorders2/src/Oleg/UserdirectoryBundle/Command/SwiftCronCommand.php
+++ Scanorders2/src/Oleg/UserdirectoryBundle/Command/SwiftCronCommand.php
@@ -34,7 +34,7 @@ class SwiftCronCommand extends ContainerAwareCommand {
 
         //$userSecUtil = $this->getContainer()->get('user_security_utility');
 
-        $cmd = 'php ../app/console swiftmailer:spool:send --env=prod';
+        $cmd = 'php app/console swiftmailer:spool:send --env=prod';
 
         //$oExec = pclose(popen("start /B ". $cmd, "r"));
         $result = exec($cmd);
