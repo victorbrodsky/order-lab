53562f955b2174f0dca394d37519ad14ed7edb4f
Oleg Ivanov <oli2002@med.cornell.edu>
2017-03-22 15:25:12 -0400

    testing deploy linux

--- Scanorders2/src/Oleg/UserdirectoryBundle/Controller/AdminController.php
+++ Scanorders2/src/Oleg/UserdirectoryBundle/Controller/AdminController.php
@@ -327,7 +327,7 @@ class AdminController extends Controller
         $output = chdir($old_path);
         echo "<pre>$output</pre>";
 
-        exit('exit runDeployScript');
+        //exit('exit runDeployScript');
     }
     public function clearCache() {
         //echo exec('whoami') . "<br>";
