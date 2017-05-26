351f782866231f50d5d0cd8a8693304cb927cce9
Oleg Ivanov <oli2002@med.cornell.edu>
2017-05-26 17:33:58 -0400

    fixed 10

--- Scanorders2/src/Oleg/FellAppBundle/Controller/FellAppManagement.php
+++ Scanorders2/src/Oleg/FellAppBundle/Controller/FellAppManagement.php
@@ -41,6 +41,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 use Symfony\Component\Security\Core\Authentication\Token\UsernamePasswordToken;
 
 
@@ -58,16 +59,26 @@ class FellAppManagement extends Controller {
             return $this->redirect( $this->generateUrl('fellapp-nopermission') );
         }
 
-        $em = $this->getDoctrine()->getManager();
-        $user = $this->get('security.context')->getToken()->getUser();
+        //$em = $this->getDoctrine()->getManager();
+        //$user = $this->get('security.context')->getToken()->getUser();
         $fellappUtil = $this->container->get('fellapp_util');
 
         //get all fellowship types using institution
         $fellowshipTypes = $fellappUtil->getFellowshipTypesByInstitution(true);
 
+        //manual message how to add/remove fellowship types
+        $linkUrl = $this->generateUrl(
+            "fellowshipsubspecialtys-list",
+            array(),
+            UrlGeneratorInterface::ABSOLUTE_URL
+        );
+        $manual = "Tips: Fellowship types can be added or removed by editing 'Fellowship Subspecialties' list.";
+        $manual = $manual." ".'<a href="'.$linkUrl.'" target="_blank">Please associate the department with the appropriate fellowship subspecialties.</a>';
+        $manual = $manual."<br>"."For example, to add a new fellowship type choose an appropriate subspecialty from the list and set the institution to 'Weill Cornell Medical College => Pathology and Laboratory Medicine'";
 
         return array(
-            'entities' => $fellowshipTypes
+            'entities' => $fellowshipTypes,
+            'manual' => $manual
         );
 
     }
