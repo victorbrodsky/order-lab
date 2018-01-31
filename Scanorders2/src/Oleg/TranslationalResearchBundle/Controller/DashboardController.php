be5fabf2845c9777b0e27dbed04c16c09ef474f4
Oleg Ivanov <oli2002@med.cornell.edu>
2018-01-31 16:50:06 -0500

    testing dashboard

--- Scanorders2/src/Oleg/TranslationalResearchBundle/Controller/DashboardController.php
+++ Scanorders2/src/Oleg/TranslationalResearchBundle/Controller/DashboardController.php
@@ -2,6 +2,7 @@
 
 namespace Oleg\TranslationalResearchBundle\Controller;
 
+use Oleg\TranslationalResearchBundle\Form\FilterDashboardType;
 use Oleg\UserdirectoryBundle\Util\LargeFileDownloader;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
@@ -18,6 +19,7 @@ class DashboardController extends Controller
 
     /**
      * @Route("/pi-level/", name="translationalresearch_dashboard_pilevel")
+     * @Route("/funded-level/", name="translationalresearch_dashboard_fundedlevel")
      * @Template("OlegTranslationalResearchBundle:Dashboard:pilevel.html.twig")
      */
     public function piLevelAction( Request $request ) {
@@ -26,33 +28,171 @@ class DashboardController extends Controller
             return $this->redirect( $this->generateUrl($this->container->getParameter('translationalresearch.sitename').'-nopermission') );
         }
 
-        $title = "Dashboard for PI Level";
+        $transResFormNodeUtil = $this->container->get('transres_formnode_util');
+        $routeName = $request->get('_route');
         $infos = array();
+
+        //////////// Filter ////////////
+        $params = array();
+        $filterform = $this->createForm(FilterDashboardType::class, null,array(
+            'method' => 'GET',
+            'form_custom_value'=>$params
+        ));
+        $filterform->handleRequest($request);
+        //////////// EOF Filter ////////////
+
+        $layoutArray = array(
+            'height' => 800,
+            'width' =>  800
+        );
+
+//            var data = [{
+//                values: [19, 26, 55],
+//                labels: ['Residential', 'Non-Residential', 'Utility'],
+//                type: 'pie'
+//            }];
+        $dataArray = array();
+        $chartDataArray = array();
+        $labels = array();
+        $values = array();
+        $type = 'pie';
+
+        //$labels = array('Residential', 'Non-Residential', 'Utility');
+        //$values = array(19, 26, 55);
+
+        $projects = $this->getProjectPis($filterform);
+
+        //Projects per PIs
+        if( $routeName == "translationalresearch_dashboard_pilevel" ) {
+            $title = "Dashboard: Projects per PI";
+            $nameValueArr = array();
+            foreach ($projects as $project) {
+                $pis = $project->getPrincipalInvestigators();
+                foreach ($pis as $pi) {
+                    $userName = $pi->getUsernameOptimal();
+                    if (isset($nameValueArr[$userName])) {
+                        $count = $nameValueArr[$userName] + 1;
+                    } else {
+                        $count = 1;
+                    }
+                    $nameValueArr[$userName] = $count;
+                }
+            }
+
+            foreach ($nameValueArr as $name => $value) {
+                $labels[] = $name;
+                $values[] = $value;
+            }
+        }
+
+        if( $routeName == "translationalresearch_dashboard_fundedlevel" ) {
+            $title = "Dashboard: Funded vs Un-Funded Projects";
+            $nameValueArr = array();
+            $fundedCount = 0;
+            $unfundedCount = 0;
+            foreach ($projects as $project) {
+                $fundingNumber = $transResFormNodeUtil->getProjectFormNodeFieldByName($project,"If funded, please provide account number");
+                if( $fundingNumber ) {
+                    $fundedCount++;
+                } else {
+                    $unfundedCount++;
+                }
+            }
+            //echo "fundedCount=".$fundedCount."<br>";
+            //echo "unfundedCount=".$unfundedCount."<br>";
+
+            $labels = array('Number of Funded Projects','Number of Un-Funded Projects');
+            $values = array($fundedCount,$unfundedCount);
+        }
+
+        $chartDataArray['values'] = $values;
+        $chartDataArray['labels'] = $labels;
+        $chartDataArray['type'] = $type;
+        $dataArray[] = $chartDataArray;
 
         return array(
             'infos' => $infos,
             'title' => $title,
+            'filterform' => $filterform->createView(),
+            'dataArray' => $dataArray,
+            'layoutArray' => $layoutArray
         );
     }
 
 
-    /**
-     * @Route("/funded-level/", name="translationalresearch_dashboard_fundedlevel")
-     * @Template("OlegTranslationalResearchBundle:Dashboard:pilevel.html.twig")
-     */
-    public function fundedLevelAction( Request $request ) {
+//    /**
+//     * @Route("/funded-level/", name="translationalresearch_dashboard_fundedlevel")
+//     * @Template("OlegTranslationalResearchBundle:Dashboard:pilevel.html.twig")
+//     */
+//    public function fundedLevelAction( Request $request ) {
+//
+//        if( false === $this->get('security.authorization_checker')->isGranted('ROLE_TRANSRES_USER') ) {
+//            return $this->redirect( $this->generateUrl($this->container->getParameter('translationalresearch.sitename').'-nopermission') );
+//        }
+//
+//        $title = "Dashboard for Funded Project Level";
+//        $infos = array();
+//
+//        //////////// Filter ////////////
+//        $params = array();
+//        $filterform = $this->createForm(FilterDashboardType::class, null,array(
+//            'method' => 'GET',
+//            'form_custom_value'=>$params
+//        ));
+//        $filterform->handleRequest($request);
+//        //////////// EOF Filter ////////////
+//
+//        $params = array();
+//        $filterform = $this->createForm(FilterDashboardType::class, null,array(
+//            'method' => 'GET',
+//            'form_custom_value'=>$params
+//        ));
+//
+//        $filterform->handleRequest($request);
+//
+//        return array(
+//            'infos' => $infos,
+//            'title' => $title,
+//        );
+//    }
 
-        if( false === $this->get('security.authorization_checker')->isGranted('ROLE_TRANSRES_USER') ) {
-            return $this->redirect( $this->generateUrl($this->container->getParameter('translationalresearch.sitename').'-nopermission') );
+
+    public function getProjectPis($filterform) {
+        $em = $this->getDoctrine()->getManager();
+
+        $repository = $em->getRepository('OlegTranslationalResearchBundle:Project');
+        $dql =  $repository->createQueryBuilder("project");
+        $dql->select('project');
+
+        //$dql->where("project.state=:state");
+        //$projects = $repository->findAll();
+        //$query = $dql->getQuery();
+        //echo "projectId=".$project->getId()."<br>";
+        //echo "reviewId=".$reviewId."<br>";
+
+        $startDate = $filterform['startDate']->getData();
+        $endDate = $filterform['endDate']->getData();
+        $dqlParameters = array();
+
+        if( $startDate ) {
+            //echo "startDate=" . $startDate->format('Y-m-d H:i:s') . "<br>";
+            $dql->andWhere('project.createDate >= :startDate');
+            $dqlParameters['startDate'] = $startDate->format('Y-m-d H:i:s');
+        }
+        if( $endDate ) {
+            $endDate->modify('+1 day');
+            $dql->andWhere('project.createDate <= :endDate');
+            $dqlParameters['endDate'] = $endDate->format('Y-m-d H:i:s');
         }
 
-        $title = "Dashboard for Funded Project Level";
-        $infos = array();
+        $query = $em->createQuery($dql);
 
-        return array(
-            'infos' => $infos,
-            'title' => $title,
-        );
+        $query->setParameters($dqlParameters);
+        //echo "query=".$query->getSql()."<br>";
+
+        $projects = $query->getResult();
+
+        return $projects;
     }
 
 }
---
+++ Scanorders2/src/Oleg/TranslationalResearchBundle/Form/FilterDashboardType.php
@@ -0,0 +1,148 @@
+<?php
+/**
+ * Copyright (c) 2017 Cornell University
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+namespace Oleg\TranslationalResearchBundle\Form;
+
+use Doctrine\ORM\EntityRepository;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class FilterDashboardType extends AbstractType
+{
+
+    private $params;
+
+    public function formConstructor( $params=null )
+    {
+        $this->params = $params;
+    }
+
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $this->formConstructor($options['form_custom_value']);
+
+//        $builder->add('projectSpecialty', EntityType::class, array(
+//            'class' => 'OlegTranslationalResearchBundle:SpecialtyList',
+//            //'choice_label' => 'name',
+//            'label' => false,   //'Project Specialty',
+//            //'disabled' => ($this->params['admin'] ? false : true),
+//            'required'=> false,
+//            'multiple' => false,
+//            'attr' => array('class'=>'combobox combobox-width'),
+//            'query_builder' => function(EntityRepository $er) {
+//                return $er->createQueryBuilder('list')
+//                    ->where("list.type = :typedef OR list.type = :typeadd")
+//                    ->orderBy("list.orderinlist","ASC")
+//                    ->setParameters( array(
+//                        'typedef' => 'default',
+//                        'typeadd' => 'user-added',
+//                    ));
+//            },
+//        ));
+
+//        $builder->add('state',ChoiceType::class, array(
+//            'label' => false,
+//            'required' => false,
+//            'multiple' => true,
+//            'choices' => $this->params['stateChoiceArr'],
+//            'attr' => array('class' => 'combobox'),
+//        ));
+
+        $builder->add('startDate', DateTimeType::class, array(
+            'label' => false,
+            'widget' => 'single_text',
+            'required' => false,
+            'format' => 'MM/dd/yyyy',
+            'attr' => array('class'=>'datepicker form-control submit-on-enter-field', 'placeholder'=>'From Submission Date'), //'title'=>'Start Year', 'data-toggle'=>'tooltip',
+        ));
+
+        $builder->add('endDate', DateTimeType::class, array(
+            'label' => false,
+            'widget' => 'single_text',
+            'required' => false,
+            'format' => 'MM/dd/yyyy',
+            'attr' => array('class'=>'datepicker form-control submit-on-enter-field', 'placeholder'=>'To Submission Date'), //'title'=>'End Year', 'data-toggle'=>'tooltip',
+        ));
+
+//        $builder->add('searchId', TextType::class, array(
+//            'required'=>false,
+//            'label' => false,
+//            'attr' => array('class'=>'form-control submit-on-enter-field', 'placeholder'=>'Search by ID'),
+//        ));
+//        $builder->add('searchTitle', TextType::class, array(
+//            'required'=>false,
+//            'label' => false,
+//            'attr' => array('class'=>'form-control submit-on-enter-field', 'placeholder'=>'Search by Title'),
+//        ));
+//        $builder->add('searchIrbNumber', TextType::class, array(
+//            'required'=>false,
+//            'label' => false,
+//            'attr' => array('class'=>'form-control submit-on-enter-field', 'placeholder'=>'Search by IRB number'),
+//        ));
+//
+//        $builder->add('fundingNumber', TextType::class, array(
+//            'required'=>false,
+//            'label' => false,
+//            'attr' => array('class'=>'form-control submit-on-enter-field', 'placeholder'=>'Search by Funding Number'),
+//        ));
+
+//        $builder->add('completed', CheckboxType::class, array(
+//            'required'=>false,
+//            'label' => 'Completed',
+//        ));
+//
+//        $builder->add('review', CheckboxType::class, array(
+//            'required'=>false,
+//            'label' => 'Review',
+//        ));
+//
+//        $builder->add('missinginfo', CheckboxType::class, array(
+//            'required'=>false,
+//            'label' => 'Requested additional information',
+//        ));
+//
+//        $builder->add('approved', CheckboxType::class, array(
+//            'required'=>false,
+//            'label' => 'Approved',
+//        ));
+//
+//        $builder->add('closed', CheckboxType::class, array(
+//            'required'=>false,
+//            'label' => 'Closed',
+//        ));
+        
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'csrf_protection' => false,
+            'form_custom_value' => null
+        ));
+    }
+
+    public function getBlockPrefix()
+    {
+        return 'filter';
+    }
+}
