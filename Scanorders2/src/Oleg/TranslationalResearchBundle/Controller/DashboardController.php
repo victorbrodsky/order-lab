871253e0ab1df74f757a1fe1ac6b828263046ddf
Oleg Ivanov <oli2002@med.cornell.edu>
2018-02-01 10:27:24 -0500

    testing dashboard with multi charts

--- Scanorders2/src/Oleg/TranslationalResearchBundle/Controller/DashboardController.php
+++ Scanorders2/src/Oleg/TranslationalResearchBundle/Controller/DashboardController.php
@@ -28,6 +28,7 @@ class DashboardController extends Controller
             return $this->redirect( $this->generateUrl($this->container->getParameter('translationalresearch.sitename').'-nopermission') );
         }
 
+        $transresUtil = $this->container->get('transres_util');
         $transResFormNodeUtil = $this->container->get('transres_formnode_util');
         $routeName = $request->get('_route');
         $infos = array();
@@ -43,7 +44,7 @@ class DashboardController extends Controller
 
         $layoutArray = array(
             'height' => 800,
-            'width' =>  800
+            'width' =>  800,
         );
 
 //            var data = [{
@@ -51,6 +52,8 @@ class DashboardController extends Controller
 //                labels: ['Residential', 'Non-Residential', 'Utility'],
 //                type: 'pie'
 //            }];
+
+        $chartsArray = array();
         $dataArray = array();
         $chartDataArray = array();
         $labels = array();
@@ -64,29 +67,84 @@ class DashboardController extends Controller
 
         //Projects per PIs
         if( $routeName == "translationalresearch_dashboard_pilevel" ) {
+
             $title = "Dashboard: Projects per PI";
-            $nameValueArr = array();
+            $piProjectCountArr = array();
+            $piTotalArr = array();
+
             foreach ($projects as $project) {
                 $pis = $project->getPrincipalInvestigators();
                 foreach ($pis as $pi) {
                     $userName = $pi->getUsernameOptimal();
-                    if (isset($nameValueArr[$userName])) {
-                        $count = $nameValueArr[$userName] + 1;
+
+                    //Projects per PI
+                    if (isset($piProjectCountArr[$userName])) {
+                        $count = $piProjectCountArr[$userName] + 1;
                     } else {
                         $count = 1;
                     }
-                    $nameValueArr[$userName] = $count;
+                    $piProjectCountArr[$userName] = $count;
+
+                    //Total per PI
+                    $invoicesInfos = $transresUtil->getInvoicesInfosByProject($project);
+                    if (isset($piTotalArr[$userName])) {
+                        $total = $piTotalArr[$userName] + $invoicesInfos['total'];
+                    } else {
+                        $total = $invoicesInfos['total'];
+                    }
+                    $piTotalArr[$userName] = $total;
                 }
             }
 
-            foreach ($nameValueArr as $name => $value) {
+            //Projects per PI
+            $layoutArray['title'] = "Number of Projects per PI";
+
+            foreach ($piProjectCountArr as $name => $value) {
                 $labels[] = $name;
                 $values[] = $value;
             }
+
+            $chartDataArray['values'] = $values;
+            $chartDataArray['labels'] = $labels;
+            $chartDataArray['type'] = $type;
+            $dataArray[] = $chartDataArray;
+
+            //$chartsArray['layout'] = $layoutArray;
+            //$chartsArray['data'] = $dataArray;
+
+            $chartsArray[] = array(
+                'layout' => $layoutArray,
+                'data' => $dataArray
+            );
+            //Projects per PI
+
+            //Total per PI
+            $layoutArray['title'] = "Total($) of Projects per PI";
+
+            foreach ($piTotalArr as $name => $value) {
+                $labels[] = $name;
+                $values[] = $value;
+            }
+
+            $chartDataArray['values'] = $values;
+            $chartDataArray['labels'] = $labels;
+            $chartDataArray['type'] = $type;
+            $dataArray[] = $chartDataArray;
+
+            //$chartsArray['layout'] = $layoutArray;
+            //$chartsArray['data'] = $dataArray;
+
+            $chartsArray[] = array(
+                'layout' => $layoutArray,
+                'data' => $dataArray
+            );
+            //Total per PI
+
         }
 
         if( $routeName == "translationalresearch_dashboard_fundedlevel" ) {
             $title = "Dashboard: Funded vs Un-Funded Projects";
+            $layoutArray['title'] = "Number of Funded vs Un-Funded Projects";
             $nameValueArr = array();
             $fundedCount = 0;
             $unfundedCount = 0;
@@ -103,19 +161,30 @@ class DashboardController extends Controller
 
             $labels = array('Number of Funded Projects','Number of Un-Funded Projects');
             $values = array($fundedCount,$unfundedCount);
+
+            $chartDataArray['values'] = $values;
+            $chartDataArray['labels'] = $labels;
+            $chartDataArray['type'] = $type;
+            $dataArray[] = $chartDataArray;
+
+            //$chartsArray['layout'] = $layoutArray;
+            //$chartsArray['data'] = $dataArray;
+
+            $chartsArray[] = array(
+                'layout' => $layoutArray,
+                'data' => $dataArray
+            );
         }
 
-        $chartDataArray['values'] = $values;
-        $chartDataArray['labels'] = $labels;
-        $chartDataArray['type'] = $type;
-        $dataArray[] = $chartDataArray;
+
 
         return array(
             'infos' => $infos,
             'title' => $title,
             'filterform' => $filterform->createView(),
-            'dataArray' => $dataArray,
-            'layoutArray' => $layoutArray
+            //'dataArray' => $dataArray,
+            //'layoutArray' => $layoutArray
+            'chartsArray' => $chartsArray
         );
     }
 
