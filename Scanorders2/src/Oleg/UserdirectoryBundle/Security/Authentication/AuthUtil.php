b3bac714844684a6e7eb6b5f597acb40ce2ff88c
Oleg Ivanov <oli2002@med.cornell.edu>
2018-05-14 12:07:53 -0400

    testing ldap

--- Scanorders2/src/Oleg/UserdirectoryBundle/Security/Authentication/AuthUtil.php
+++ Scanorders2/src/Oleg/UserdirectoryBundle/Security/Authentication/AuthUtil.php
@@ -208,10 +208,12 @@ class AuthUtil {
         }
 
         //first search this user if exists in ldap directory
-        $searchRes = $this->searchLdap($usernameClean);
-        if( $searchRes == NULL || count($searchRes) == 0 ) {
-            $this->logger->error("LdapAuthentication: can not find user by usernameClean=".$usernameClean);
-            return NULL;
+        if(0) {
+            $searchRes = $this->searchLdap($usernameClean);
+            if ($searchRes == NULL || count($searchRes) == 0) {
+                $this->logger->error("LdapAuthentication: can not find user by usernameClean=" . $usernameClean);
+                return NULL;
+            }
         }
 
         //echo "user exists in ldap directory<br>";
@@ -648,8 +650,8 @@ class AuthUtil {
         $cnx = $this->connectToLdap($LDAPHost);
 
         //TODO: use local function with SASL ($this->ldapBind)
-        //$res = @ldap_bind($cnx,$LDAPUserAdmin,$LDAPUserPasswordAdmin);
-        $res = $this->ldapBind($LDAPUserAdmin,$LDAPUserPasswordAdmin);
+        $res = @ldap_bind($cnx,$LDAPUserAdmin,$LDAPUserPasswordAdmin);
+        //$res = $this->ldapBind($LDAPUserAdmin,$LDAPUserPasswordAdmin);
         if( !$res ) {
             $this->logger->error("searchLdap: ldap_bind failed with admin authentication username=".$LDAPUserAdmin);
         	//echo "Could not bind to LDAP: user=".$LDAPUserAdmin."<br>";
