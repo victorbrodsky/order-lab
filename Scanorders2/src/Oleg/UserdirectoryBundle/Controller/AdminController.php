e75bdf6d6a3709abcd7fdd73d536112f218ebc8b
Oleg Ivanov <oli2002@med.cornell.edu>
2018-05-31 13:04:03 -0400

    testing

--- Scanorders2/src/Oleg/UserdirectoryBundle/Controller/AdminController.php
+++ Scanorders2/src/Oleg/UserdirectoryBundle/Controller/AdminController.php
@@ -306,7 +306,7 @@ class AdminController extends Controller
 
         //$linux
         if( $linux ) {
-            $this->runProcess("chown -R www-data:www-data ".$old_path);
+            $this->runProcess("sudo chown -R www-data:www-data ".$old_path);
             $this->runProcess("php bin" . $dirSep . "console assets:install");
             $this->runProcess("php bin" . $dirSep . "console cache:clear --env=prod --no-debug");
             $this->runProcess("php bin" . $dirSep . "console assetic:dump --env=prod --no-debug");
