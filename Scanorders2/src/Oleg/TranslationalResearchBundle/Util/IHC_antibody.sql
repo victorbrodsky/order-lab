f4beedf551b77d12802abe4557d5c02fd7e07805
Oleg Ivanov <oli2002@med.cornell.edu>
2018-07-24 14:32:14 -0400

    testing antibody on collage

--- Scanorders2/src/Oleg/TranslationalResearchBundle/Util/IHC_antibody.sql
+++ Scanorders2/src/Oleg/TranslationalResearchBundle/Util/IHC_antibody.sql
@@ -7,5 +7,5 @@
 -- Server version: 5.6.14
 -- PHP Version: 5.3.13
 
-INSERT INTO `transres_antibodyList` (`id`, `category`, `name`, `altname`, `company`, `catalog`, `lot`, `igconcentration`, `clone`, `host`, `reactivity`, `control`, `protocol`, `retrieval`, `dilution`, `storage`, `comment`, `datasheet`, `pdf`) VALUES
+INSERT INTO transres_antibodyList (id, category, name, altname, `company`, `catalog`, `lot`, `igconcentration`, `clone`, `host`, `reactivity`, `control`, `protocol`, `retrieval`, `dilution`, `storage`, `comment`, `datasheet`, `pdf`) VALUES
 (1, 'M', 'Androgen Receptor', 'AR ', 'Abcam', 'ab74272', 'GR32463-1', '0.2 mg/ml', 'Poly', 'Rabbit ', 'Human, mouse', 'Xenograft Control/Prostate Ca.', 'Envision Rabbit R. ', 'H130', '1:200', '-20 oC', 'Project: 12743 RS#: 30323 PI: Rubin/Kyung Condition confirmed by Dr. Rubin/Kyung on 03/09/2011', 'http://www.abcam.com/Androgen-Receptor-antibody-ab74272.html', 'upload/pdf/1296507249.pdf');
