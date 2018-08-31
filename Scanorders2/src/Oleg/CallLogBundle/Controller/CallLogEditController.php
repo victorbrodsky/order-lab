e3b5c18f27189ebecc1a16b63314e5ac47cf3160
Oleg Ivanov <oli2002@med.cornell.edu>
2018-08-31 15:59:06 -0400

    fixed roles for trp

--- Scanorders2/src/Oleg/CallLogBundle/Controller/CallLogEditController.php
+++ Scanorders2/src/Oleg/CallLogBundle/Controller/CallLogEditController.php
@@ -185,7 +185,7 @@ class CallLogEditController extends CallEntryController
             return $this->redirect($this->generateUrl('calllog-nopermission'));
         }
 
-        //ini_set('memory_limit', '5120M');
+        ini_set('memory_limit', '5120M');
 
         //$userSecUtil = $this->get('user_security_utility');
         $calllogUtil = $this->get('calllog_util');
