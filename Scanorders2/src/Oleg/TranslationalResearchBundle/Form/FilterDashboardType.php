605e67f6b9a12a6fd80fefe6cb39836ff52b895f
Oleg Ivanov <oli2002@med.cornell.edu>
2018-10-24 11:45:48 -0400

    fixed invoices link

--- Scanorders2/src/Oleg/TranslationalResearchBundle/Form/FilterDashboardType.php
+++ Scanorders2/src/Oleg/TranslationalResearchBundle/Form/FilterDashboardType.php
@@ -109,7 +109,7 @@ class FilterDashboardType extends AbstractType
 
         if( $this->params['exploded'] ) {
             $builder->add('exploded', CheckboxType::class, array(
-                'label' => "Exploded View",
+                'label' => "Show only the top ten ",
                 'required' => false,
                 //'attr' => array('class' => 'form-control checkbox')
             ));
