1b3591bfb8a6791a6d78049d29148f3b7f227555
Oleg Ivanov <oli2002@med.cornell.edu>
2019-01-04 08:52:57 -0500

    testing ref provider

--- Scanorders2/src/Oleg/CallLogBundle/Util/CallLogUtil.php
+++ Scanorders2/src/Oleg/CallLogBundle/Util/CallLogUtil.php
@@ -1652,7 +1652,7 @@ class CallLogUtil
         $em = $this->em;
 
         $output = array();
-        return $output;
+        //return $output;
 
         ///////////// 1) get all real users /////////////
         $query = $em->createQueryBuilder()
