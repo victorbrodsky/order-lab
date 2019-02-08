49e48c04623d909a79c86e4ef1c24156ad633e11
Oleg Ivanov <oli2002@med.cornell.edu>
2019-02-08 11:12:18 -0500

    added total to eventlog

--- Scanorders2/src/Oleg/UserdirectoryBundle/Controller/LoggerController.php
+++ Scanorders2/src/Oleg/UserdirectoryBundle/Controller/LoggerController.php
@@ -348,6 +348,8 @@ class LoggerController extends Controller
         if( $filtered ) {
             $eventlogTitle = $eventlogTitle . " showing " . count($pagination) . " matching event(s)";
         }
+        $eventlogTitle = $eventlogTitle . " (Total matching " . $pagination->getTotalItemCount() . ")";
+
 
         $route = $request->get('_route');
         //echo "route=".$route."<br>";
