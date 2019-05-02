01e185804a1e863b8cc27de9f92877732be35789
Oleg Ivanov <oli2002@med.cornell.edu>
2019-05-02 16:23:04 -0400

    fixing warnings

--- Scanorders2/src/Oleg/OrderformBundle/Repository/ArrayFieldAbstractRepository.php
+++ Scanorders2/src/Oleg/OrderformBundle/Repository/ArrayFieldAbstractRepository.php
@@ -1306,16 +1306,16 @@ class ArrayFieldAbstractRepository extends EntityRepository {
             $fieldIndex = str_replace($prefixname."-","",$lastFieldStr);
         } else {
             $fieldIndex = 0;
-            echo "fieldIndex assigned 0<br>";
+            //echo "fieldIndex assigned 0<br>";
         }
 
-        echo "fieldIndex=".$fieldIndex."<br>";
+        //echo "fieldIndex=".$fieldIndex."<br>";
 
         //$fieldIndex = ltrim($fieldIndex,'0') + 1;
         $fieldIndex = ltrim($fieldIndex,'0');
         $fieldIndex = (int)$fieldIndex + 1;
         $paddedfield = str_pad($fieldIndex,13,'0',STR_PAD_LEFT);
-        echo "paddedfield=".$paddedfield."<br>";
+        //echo "paddedfield=".$paddedfield."<br>";
         //exit();
         return $prefixname.'-'.$paddedfield;
     }
