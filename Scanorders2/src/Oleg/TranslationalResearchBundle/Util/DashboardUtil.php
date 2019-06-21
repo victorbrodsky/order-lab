ae0a04427376d30cd1f4795fd6d352b1c48b5a75
Oleg Ivanov <oli2002@med.cornell.edu>
2019-06-21 15:11:01 -0400

    changing total count

--- Scanorders2/src/Oleg/TranslationalResearchBundle/Util/DashboardUtil.php
+++ Scanorders2/src/Oleg/TranslationalResearchBundle/Util/DashboardUtil.php
@@ -283,7 +283,7 @@ class DashboardUtil
 
         if( !$showOthers ) {
             //“Show only the top 10” - if it is checked, show only the top ten projects, if it is not checked, show the top 100
-            $limit = 100;
+            $limit = 20;
         }
 
         //$otherId = "All other $showOthers combined";
