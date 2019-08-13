f0b9706dc83aa9f577cc0c3db7d88c89326852d0
Oleg Ivanov <oli2002@med.cornell.edu>
2019-08-13 16:34:40 -0400

    testing packer

--- packer/deploy-order-digital-ocean.sh
+++ packer/deploy-order-digital-ocean.sh
@@ -111,13 +111,13 @@ sed -i -e "s/bash_domainname/$domainname/g" order-packer.json
 sed -i -e "s/bash_sslcertificate/$sslcertificate/g" order-packer.json
 sed -i -e "s/bash_sslprivatekey/$sslprivatekey/g" order-packer.json
 
-echo "Testing: domainname=$domainname"
-if [ ! -z "$domainname" ] && [ "$domainname" != "domainname" ]
-  then 	 
-	echo "Testing: Create domain domainname=$domainname"
-  else
-	echo "Testing: Do not create domain domainname=$domainname"
-fi	
+#echo "Testing: domainname=$domainname"
+#if [ ! -z "$domainname" ] && [ "$domainname" != "domainname" ]
+#  then 	 
+#	echo "Testing: Create domain domainname=$domainname"
+#  else
+#	echo "Testing: Do not create domain domainname=$domainname"
+#fi	
 #exit 0
 
 echo "*** Building VM image ... ***"
