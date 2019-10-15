5da3a4f4317fe2dc8430184e000ca55a7f4ede38
Oleg Ivanov <oli2002@med.cornell.edu>
2019-10-15 11:11:43 -0400

    working on centos permission

--- Scanorders2/web/app_dev.php
+++ Scanorders2/web/app_dev.php
@@ -28,6 +28,9 @@ use Symfony\Component\HttpFoundation\Request;
 // https://symfony.com/doc/current/book/installation.html#configuration-and-setup for more information
 //umask(0000);
 
+//Testing permission
+umask(0000);
+
 // This check prevents access to debug front controllers that are deployed by
 // accident to production servers. Feel free to remove this, extend it, or make
 // something more sophisticated.
