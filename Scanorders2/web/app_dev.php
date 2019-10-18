62155cab5e5ffe3e7cdf87dfff60eb2950d26111
Oleg Ivanov <oli2002@med.cornell.edu>
2019-10-18 09:46:37 -0400

    working on centos

--- Scanorders2/web/app_dev.php
+++ Scanorders2/web/app_dev.php
@@ -35,9 +35,10 @@ use Symfony\Component\HttpFoundation\Request;
 // accident to production servers. Feel free to remove this, extend it, or make
 // something more sophisticated.
 echo "REMOTE_ADDR=".$_SERVER['REMOTE_ADDR']."<br>";
-echo "Web user:<br>";
+echo "Web user=";
 print posix_getpwuid(posix_geteuid())['name'];
 echo "<br>";
+echo "HTTPDUSER=".$HTTPDUSER."<br>";
 if (isset($_SERVER['HTTP_CLIENT_IP'])
     || isset($_SERVER['HTTP_X_FORWARDED_FOR'])
     || !(in_array(@$_SERVER['REMOTE_ADDR'], ['127.0.0.1', 'fe80::1', '::1', '140.251.6.82', '166.48.203.29'], true) || PHP_SAPI === 'cli-server')
