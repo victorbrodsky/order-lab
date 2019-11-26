46e1ddf1b2d2e97614ca4146de1ef7ece66d983d
Oleg Ivanov <oli2002@med.cornell.edu>
2019-11-26 16:33:30 -0500

    testing doctrine schema migration

--- Scanorders2/src/Oleg/Migration/PostgresMigration.php
+++ Scanorders2/src/Oleg/Migration/PostgresMigration.php
@@ -28,9 +28,17 @@ class PostgresMigration extends AbstractMigration
 
     }
 
-    //addSql($sql, array $params = Array, array $types = Array)
-    public function processSql( $sql, array $params = [], array $types = [] ) {
-        //wrapper for addSql
+    public function processSql( $sql ) {
+        //wrapper for processSql
+
+        //An exception occurred while executing 'DROP INDEX "primary"':
+//        if( $sql == 'DROP INDEX "primary"' ) {
+//            return false;
+//        }
+        if( strpos($sql, 'DROP INDEX ') !== false && strpos($sql, 'primary') !== false ) {
+            return false;
+        }
+
         if( strpos($sql, ' ADD PRIMARY KEY ') !== false ) {
             return false;
         }
@@ -41,7 +49,7 @@ class PostgresMigration extends AbstractMigration
             //it's ok to rename with 5 zeros '00000': ALTER INDEX idx_c6f1cf80537a132900000 RENAME TO IDX_C6F1CF80537A1329
             if( strpos($sql, '00000') !== false ) {
                 //has 00000
-                $this->addParentSql($sql);
+                $this->addSql($sql);
                 return false;
             } else {
                 //does not have 00000
@@ -55,7 +63,49 @@ class PostgresMigration extends AbstractMigration
             return false;
         }
 
-        $this->addParentSql($sql);
+        $this->addSql($sql);
+    }
+
+
+    //addSql($sql, array $params = Array, array $types = Array)
+    //public function processSql( $sql, array $params = [], array $types = [] ) {
+
+    public function processSql111( $sql ) {
+        //wrapper for addSql
+
+        //An exception occurred while executing 'DROP INDEX "primary"':
+//        if( $sql == 'DROP INDEX "primary"' ) {
+//            return false;
+//        }
+        if( strpos($sql, 'DROP INDEX ') !== false && strpos($sql, 'primary') !== false ) {
+            return false;
+        }
+
+        if( strpos($sql, ' ADD PRIMARY KEY ') !== false ) {
+            return false;
+        }
+
+        //'ALTER INDEX idx_e573a753bdd0acfa RENAME TO IDX_6BE23A97726D9566'
+        if( strpos($sql, ' RENAME TO IDX_') !== false ) {
+            //has string
+            //it's ok to rename with 5 zeros '00000': ALTER INDEX idx_c6f1cf80537a132900000 RENAME TO IDX_C6F1CF80537A1329
+            if( strpos($sql, '00000') !== false ) {
+                //has 00000
+                $this->addSql($sql);
+                return false;
+            } else {
+                //does not have 00000
+                echo "ignore ".$sql;
+                return false;
+            }
+        }
+
+        //Drop index DROP INDEX IDX_22984163C33F7837
+        if( strpos($sql, 'DROP INDEX IDX_') !== false ) {
+            return false;
+        }
+
+        $this->addSql($sql);
     }
 
     public function addParentSql($sql) {
