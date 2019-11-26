1f2f19d8d8b6c5e2fcfb57e20ee9877f397a8b5f
Oleg Ivanov <oli2002@med.cornell.edu>
2019-11-26 16:09:54 -0500

    testing doctrine schema migration

--- Scanorders2/src/Oleg/Migration/PostgresMigration.php
+++ Scanorders2/src/Oleg/Migration/PostgresMigration.php
@@ -29,7 +29,7 @@ class PostgresMigration extends AbstractMigration
     }
 
     //addSql($sql, array $params = Array, array $types = Array)
-    public function addSql( $sql, $params = null, array $types = null ) {
+    public function addSql( $sql, $params = array(), array $types = array() ) {
         //wrapper for addSql
         if( strpos($sql, ' ADD PRIMARY KEY ') !== false ) {
             return false;
