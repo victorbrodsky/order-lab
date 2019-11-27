97d90f50bcba2291101dc52fb6e6902bee5fced1
Oleg Ivanov <oli2002@med.cornell.edu>
2019-11-27 14:22:20 -0500

    testing doctrine schema migration

--- Scanorders2/src/Oleg/Migration/PostgresMigration.php
+++ Scanorders2/src/Oleg/Migration/PostgresMigration.php
@@ -131,9 +131,9 @@ class PostgresMigration extends AbstractMigration implements ContainerAwareInter
         $this->processComplexSql($sql,TRUE);
 
         //testing
-        if( strpos($sql, 'UNIQ_22984163C33F7837') !== FALSE ) {
-            exit("Testing exit sql=".$sql);
-        }
+        //if( strpos($sql, 'UNIQ_22984163C33F7837') !== FALSE ) {
+        //    exit("Testing exit sql=".$sql);
+        //}
     }
 //    public function processSimpleSql($sql) {
 //        $this->processComplexSql($sql,null);
