6817a4bbe582395c91cf21f6c275921a6553b2dd
Oleg Ivanov <oli2002@med.cornell.edu>
2019-12-13 08:18:10 -0500

    finished trp user test

--- Scanorders2/tests/Oleg/TestBundle/FellappTest.php
+++ Scanorders2/tests/Oleg/TestBundle/FellappTest.php
@@ -16,8 +16,8 @@ class FellappTest extends WebTestBase
         $this->getClient();
         $crawler = $this->client->request('GET', '/fellowship-applications/login');
 
-        //$content = $this->client->getResponse()->getContent();
-        //exit("content=$content");
+        $content = $this->client->getResponse()->getContent();
+        exit("content=$content");
 
         $this->assertGreaterThan(
             0,
