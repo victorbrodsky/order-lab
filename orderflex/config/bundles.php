cde0b76d78c2d8ed0473b26b017737af02499951
Oleg Ivanov <oli2002@med.cornell.edu>
2020-01-08 17:59:53 -0500

    working on converting bundlename:twig and stylesheets

--- orderflex/config/bundles.php
+++ orderflex/config/bundles.php
@@ -22,4 +22,5 @@ return [
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['dev' => true, 'test' => true],
+    Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
 ];
