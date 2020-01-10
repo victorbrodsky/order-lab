d8c8eee485a5f8c3328563199330cccc01a615a2
Oleg Ivanov <oli2002@med.cornell.edu>
2020-01-10 14:43:07 -0500

    testing webpack

---
+++ orderflex/assets/js/user.js
@@ -0,0 +1,52 @@
+/**
+ * Created by ch3 on 1/10/2020.
+ */
+
+
+//import 'public/orderassets/AppUserdirectoryBundle/jquery/jquery-1.11.0.min.js';
+//import 'public/orderassets/AppUserdirectoryBundle/jquery-ui-1.11.2/jquery-ui.js';
+//import 'public/orderassets/AppUserdirectoryBundle/bootstrap/js/bootstrap.min.js';
+//import 'public/orderassets/AppUserdirectoryBundle/bootstrap/js/bootstrap.min.js';
+
+//require("C:/Users/ch3/Documents/MyDocs/WCMC/ORDER/order-lab/orderflex/public/orderassets/AppUserdirectoryBundle/jquery/jquery-1.11.0.min.js");
+//require("C:/Users/ch3/Documents/MyDocs/WCMC/ORDER/order-lab/orderflex/public/orderassets/AppUserdirectoryBundle/jquery-ui-1.11.2/jquery-ui.js");
+
+//import "C:/Users/ch3/Documents/MyDocs/WCMC/ORDER/order-lab/orderflex/public/orderassets/AppUserdirectoryBundle/jquery/jquery-1.11.0.min.js";
+//import "C:/Users/ch3/Documents/MyDocs/WCMC/ORDER/order-lab/orderflex/public/orderassets/AppUserdirectoryBundle/jquery-ui-1.11.2/jquery-ui.js";
+
+//import "C:/Users/ch3/Documents/MyDocs/WCMC/ORDER/order-lab/orderflex/public/orderassets/AppUserdirectoryBundle/jquery/jquery-1.11.0.min.js";
+//import "C:/Users/ch3/Documents/MyDocs/WCMC/ORDER/order-lab/orderflex/public/orderassets/AppUserdirectoryBundle/bootstrap/js/bootstrap.min.js";
+
+const $ = require("../../public/orderassets/AppUserdirectoryBundle/jquery/jquery-1.11.0.min.js");
+const jQuery = $;
+
+//import "../../public/orderassets/AppUserdirectoryBundle/jquery/jquery-1.11.0.min.js";
+//import "../../public/orderassets/AppUserdirectoryBundle/jquery-ui-1.11.2/jquery-ui.js";
+import "../../public/orderassets/AppUserdirectoryBundle/bootstrap/js/bootstrap.min.js";
+import "../../public/orderassets/AppUserdirectoryBundle/ladda/spin.min.js";
+//import "../../public/orderassets/AppUserdirectoryBundle/ladda/ladda.min.js";
+//import "../../public/orderassets/AppUserdirectoryBundle/datepicker/js/bootstrap-datepicker.js";
+import "../../public/orderassets/AppUserdirectoryBundle/select2/select2.js";
+import "../../public/orderassets/AppUserdirectoryBundle/inputmask/jquery.inputmask.bundle.js";
+//import "../../public/orderassets/AppUserdirectoryBundle/pnotify/pnotify.custom.min.js";
+import "../../public/orderassets/AppUserdirectoryBundle/form/js/user-form.js";
+import "../../public/orderassets/AppUserdirectoryBundle/form/js/user-common.js";
+import "../../public/orderassets/AppUserdirectoryBundle/form/js/user-selectAjax.js";
+import "../../public/orderassets/AppUserdirectoryBundle/form/js/user-masking.js";
+import "../../public/orderassets/AppUserdirectoryBundle/form/js/user-formReady.js";
+import "../../public/orderassets/AppUserdirectoryBundle/form/js/user-basetitles.js";
+import "../../public/orderassets/AppUserdirectoryBundle/form/js/user-treeSelectAjax.js";
+import "../../public/orderassets/AppUserdirectoryBundle/form/js/user-validation.js";
+import "../../public/orderassets/AppUserdirectoryBundle/form/js/user-fileuploads.js";
+import "../../public/orderassets/AppUserdirectoryBundle/form/js/user-navbar.js";
+//import "../../public/orderassets/AppUserdirectoryBundle/vakata-jstree/jstree.min.js";
+import "../../public/orderassets/AppUserdirectoryBundle/form/js/user-jstree.js";
+import "../../public/orderassets/AppUserdirectoryBundle/typeahead/typeahead.bundle.js";
+import "../../public/orderassets/AppUserdirectoryBundle/form/js/user-typeahead.js";
+//import "../../public/orderassets/AppUserdirectoryBundle/fengyuanchen-image-cropper/cropper.min.js";
+//import "../../public/orderassets/AppUserdirectoryBundle/form/js/user-crop-avatar.js";
+import "../../public/orderassets/AppUserdirectoryBundle/idletimeout/jquery.idletimeout.js";
+import "../../public/orderassets/AppUserdirectoryBundle/idletimeout/jquery.idletimer.js";
+import "../../public/orderassets/AppUserdirectoryBundle/form/js/user-idleTimeout.js";
+import "../../public/orderassets/AppUserdirectoryBundle/jasny/js/rowlink.js";
+import "../../public/orderassets/AppUserdirectoryBundle/q-1/q.js";
--- orderflex/webpack.config.js
+++ orderflex/webpack.config.js
@@ -23,9 +23,52 @@ Encore
      * Each entry will result in one JavaScript file (e.g. app.js)
      * and one CSS file (e.g. app.css) if your JavaScript imports CSS.
      */
-    .addEntry('app', './assets/js/app.js')
+    //.addEntry('app', './assets/js/app.js')
     //.addEntry('page1', './assets/js/page1.js')
     //.addEntry('page2', './assets/js/page2.js')
+
+    // .addEntry('user', './public/orderassets/AppUserdirectoryBundle/jquery/jquery-1.11.0.min.js')
+    // .addEntry('user', './public/orderassets/AppUserdirectoryBundle/jquery-ui-1.11.2/jquery-ui.js')
+    // .addEntry('user', './public/orderassets/AppUserdirectoryBundle/bootstrap/js/bootstrap.min.js')
+    // .addEntry('user', './public/orderassets/AppUserdirectoryBundle/bootstrap/js/bootstrap.min.js')
+    // .addEntry('user', './public/orderassets/AppUserdirectoryBundle/ladda/spin.min.js')
+    // .addEntry('user', './public/orderassets/AppUserdirectoryBundle/ladda/ladda.min.js')
+    // .addEntry('user', './public/orderassets/AppUserdirectoryBundle/datepicker/js/bootstrap-datepicker.js')
+    // .addEntry('user', './public/orderassets/AppUserdirectoryBundle/select2/select2.js')
+    // .addEntry('user', './public/orderassets/AppUserdirectoryBundle/inputmask/jquery.inputmask.bundle.js')
+    // .addEntry('user', './public/orderassets/AppUserdirectoryBundle/pnotify/pnotify.custom.min.js')
+    // .addEntry('user', './public/orderassets/AppUserdirectoryBundle/form/js/user-form.js')
+    // .addEntry('user', './public/orderassets/AppUserdirectoryBundle/form/js/user-common.js')
+    // .addEntry('user', './public/orderassets/AppUserdirectoryBundle/form/js/user-selectAjax.js')
+    // .addEntry('user', './public/orderassets/AppUserdirectoryBundle/form/js/user-masking.js')
+    // .addEntry('user', './public/orderassets/AppUserdirectoryBundle/form/js/user-formReady.js')
+    // .addEntry('user', './public/orderassets/AppUserdirectoryBundle/form/js/user-basetitles.js')
+    // .addEntry('user', './public/orderassets/AppUserdirectoryBundle/form/js/user-treeSelectAjax.js')
+    // .addEntry('user', './public/orderassets/AppUserdirectoryBundle/form/js/user-validation.js')
+    // .addEntry('user', './public/orderassets/AppUserdirectoryBundle/form/js/user-fileuploads.js')
+    // .addEntry('user', './public/orderassets/AppUserdirectoryBundle/form/js/user-navbar.js')
+    // .addEntry('user', './public/orderassets/AppUserdirectoryBundle/vakata-jstree/jstree.min.js')
+    // .addEntry('user', './public/orderassets/AppUserdirectoryBundle/form/js/user-jstree.js')
+    // .addEntry('user', './public/orderassets/AppUserdirectoryBundle/typeahead/typeahead.bundle.js')
+    // .addEntry('user', './public/orderassets/AppUserdirectoryBundle/form/js/user-typeahead.js')
+    // .addEntry('user', './public/orderassets/AppUserdirectoryBundle/fengyuanchen-image-cropper/cropper.min.js')
+    // .addEntry('user', './public/orderassets/AppUserdirectoryBundle/form/js/user-crop-avatar.js')
+    // .addEntry('user', './public/orderassets/AppUserdirectoryBundle/idletimeout/jquery.idletimeout.js')
+    // .addEntry('user', './public/orderassets/AppUserdirectoryBundle/idletimeout/jquery.idletimer.js')
+    // .addEntry('user', './public/orderassets/AppUserdirectoryBundle/form/js/user-idleTimeout.js')
+    // .addEntry('user', './public/orderassets/AppUserdirectoryBundle/jasny/js/rowlink.js')
+    // .addEntry('user', './public/orderassets/AppUserdirectoryBundle/q-1/q.js')
+
+
+    // .addEntry('user', [
+    //     './public/orderassets/AppUserdirectoryBundle/jquery/jquery-1.11.0.min.js',
+    //     './public/orderassets/AppUserdirectoryBundle/jquery-ui-1.11.2/jquery-ui.js'
+    //     //'./public/orderassets/AppUserdirectoryBundle/bootstrap/js/bootstrap.min.js',
+    //     //'./public/orderassets/AppUserdirectoryBundle/bootstrap/js/bootstrap.min.js',
+    //     //'./public/orderassets/AppUserdirectoryBundle/ladda/spin.min.js'
+    // ])
+
+    .addEntry('user','./assets/js/user.js')
 
     // When enabled, Webpack "splits" your files into smaller pieces for greater optimization.
     .splitEntryChunks()
