d738df545862299ed66581118de75ac307baefef
Oleg Ivanov <oli2002@med.cornell.edu>
2020-01-14 12:35:39 -0500

    modified index_dev for localhost

--- orderflex/public/index_dev.php
+++ orderflex/public/index_dev.php
@@ -25,14 +25,14 @@ require dirname(__DIR__).'/config/bootstrap.php';
 //    Request::setTrustedHosts([$trustedHosts]);
 //}
 
-if (
-        isset($_SERVER['HTTP_CLIENT_IP'])
-        || isset($_SERVER['HTTP_X_FORWARDED_FOR'])
-        || !(in_array(@$_SERVER['REMOTE_ADDR'], ['127.0.0.1', 'fe80::1', '::1'], true) || PHP_SAPI === 'cli-server')
-) {
-    header('HTTP/1.0 403 Forbidden');
-    exit('You are not allowed to access this file. Check ' . basename(__FILE__) . ' for more information.');
-}
+//if (
+//        isset($_SERVER['HTTP_CLIENT_IP'])
+//        || isset($_SERVER['HTTP_X_FORWARDED_FOR'])
+//        || !(in_array(@$_SERVER['REMOTE_ADDR'], ['127.0.0.1', 'fe80::1', '::1'], true) || PHP_SAPI === 'cli-server')
+//) {
+//    header('HTTP/1.0 403 Forbidden');
+//    exit('You are not allowed to access this file. Check ' . basename(__FILE__) . ' for more information.');
+//}
 
 //$kernel = new Kernel($_SERVER['APP_ENV'], (bool) $_SERVER['APP_DEBUG']);
 $kernel = new Kernel('dev',true);
