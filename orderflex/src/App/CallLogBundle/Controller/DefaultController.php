2fc72ee279a640ef0b5c9aef6bb160b0a96a1fb5
Oleg Ivanov <oli2002@med.cornell.edu>
2020-04-13 14:02:26 -0400

    updating entry tags

--- orderflex/src/App/CallLogBundle/Controller/DefaultController.php
+++ orderflex/src/App/CallLogBundle/Controller/DefaultController.php
@@ -723,6 +723,8 @@ class DefaultController extends OrderAbstractController
      */
     public function updateEntryTagAction(Request $request)
     {
+        exit("Permitted only once");
+
         if (false === $this->get('security.authorization_checker')->isGranted('ROLE_PLATFORM_DEPUTY_ADMIN')) {
             return $this->redirect($this->generateUrl('employees-nopermission'));
         }
