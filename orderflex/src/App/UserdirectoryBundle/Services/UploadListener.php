fbc30f8808e440fbf8004b351ec217064ef7da85
Oleg Ivanov <oli2002@med.cornell.edu>
2020-05-12 14:37:48 -0400

    testing resapp upload on dev

--- orderflex/src/App/UserdirectoryBundle/Services/UploadListener.php
+++ orderflex/src/App/UserdirectoryBundle/Services/UploadListener.php
@@ -74,11 +74,16 @@ class UploadListener {
         $size = $file->getSize();
 
         //creator: subjectUser
-        echo "userid=".$userid."<br>";
+        //echo "userid=".$userid."<br>";
+        $user = null;
         if( $userid ) {
             $user = $this->em->getRepository('AppUserdirectoryBundle:User')->find($userid);
         } else {
-            $user = $this->container->get('security.context')->getToken()->getUser();
+            if( $this->container->get('security.token_storage') ) {
+                if( $this->container->get('security.token_storage')->getToken() ) {
+                    $user = $this->container->get('security.token_storage')->getToken()->getUser();
+                }
+            }
         }
 
         $authUser = $this->em->getRepository('AppUserdirectoryBundle:User')->find($authUserId);
