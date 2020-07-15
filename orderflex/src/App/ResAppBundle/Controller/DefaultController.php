e325573eb82a2737de9761cc9f8642806d2abf7b
Oleg Ivanov <oli2002@med.cornell.edu>
2020-07-15 18:23:01 -0400

    testing start/end date for resapp

--- orderflex/src/App/ResAppBundle/Controller/DefaultController.php
+++ orderflex/src/App/ResAppBundle/Controller/DefaultController.php
@@ -324,7 +324,7 @@ class DefaultController extends OrderAbstractController
      */
     public function updateApplicationSeasonStartDateAction( Request $request ) {
 
-        //exit("not allowed");
+        exit("not allowed");
 
         if( false === $this->get('security.authorization_checker')->isGranted('ROLE_PLATFORM_DEPUTY_ADMIN') ) {
             return $this->redirect( $this->generateUrl($this->getParameter('resapp.sitename').'-nopermission') );
