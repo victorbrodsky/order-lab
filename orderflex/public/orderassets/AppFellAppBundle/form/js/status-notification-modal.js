bf5d3ff3f84288514275cac19463bde77c242634
Oleg Ivanov <oli2002@med.cornell.edu>
2020-08-21 17:10:15 -0400

    working on 180 coloring for felapp

--- orderflex/public/orderassets/AppFellAppBundle/form/js/status-notification-modal.js
+++ orderflex/public/orderassets/AppFellAppBundle/form/js/status-notification-modal.js
@@ -8,7 +8,7 @@ function fellappStatusNotificationConfirmAction() {
 
     $('a[fellapp-data-confirm]').click(function(ev) {
 
-        var href = $(this).attr('href');
+        //var href = $(this).attr('href');
         //console.log("href="+href);
 
         if( !$('#fellappDataConfirmModal').length ) {
@@ -99,6 +99,133 @@ function fellappStatusNotificationConfirmAction() {
 
 }
 
+function fellappStatusNotificationConfirmAction_dynamic() {
+
+    $('a[fellapp-data-confirm]').click(function(ev) {
+
+        //var href = $(this).attr('href');
+        console.log("fellapp-data-confirm clicked");
+
+        //var btn = $(this);
+        //var lbtn = Ladda.create( btn );
+
+        var fellappId = $(this).attr('fellapp-data-email-fellappid');
+        var emailType = $(this).attr('fellapp-data-email-type');
+        
+        if( !fellappId ) {
+            var emailSubject = "Error getting email subject";
+            var emailBody = "Error getting email body";
+        }
+        //var emailBody = $(this).attr('fellapp-data-email-body');
+
+        // if( emailSubject == 'acceptedEmailSubjectModified' && emailBody == 'acceptedEmailBodyModified' ) {
+
+        var url = Routing.generate('fellapp_get_notification_email_infos');
+
+        $.ajax({
+            url: url,
+            timeout: _ajaxTimeout,
+            async: true,
+            data: {id: fellappId, emailType: emailType},
+        }).success(function(data) {
+            //console.log("data="+data);
+
+            if( data == "NOTOK" ) {
+                fellappStatusNotificationConfirmModal($(this),null,emailSubject,emailBody);
+            } else {
+                fellappStatusNotificationConfirmModal($(this),data.warning,data.subject,data.body);
+            }
+        }).done(function() {
+            console.log("Error getting subject and body");
+            //calllogStopBtn(lbtn);
+            //lbtn.stop();
+            //$('button').prop('disabled',false);
+        });
+
+        // }
+
+        return false;
+    }); //fellapp-data-confirm click
+
+}
+function fellappStatusNotificationConfirmModal(modalEl,emailWarning,emailSubject,emailBody) {
+
+    if( !$('#fellappDataConfirmModal').length ) {
+        var modalHtml =
+            '<div id="fellappDataConfirmModal" class="modal fade fellapp-data-confirm-modal">' +
+            '<div class="modal-dialog">' +
+            '<div class="modal-content">' +
+            '<div class="modal-header text-center">' +
+            '<button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>' +
+            '<h3 id="dataConfirmLabel">Confirmation</h3>' +
+            '</div>' +
+            '<div class="modal-body text-center">' +
+            '</div>' +
+            '<div class="modal-footer">' +
+            '<a class="btn btn-primary fellapp-data-confirm-ok fellapp-data-confirm-ok-statusnotify" id="dataConfirmStatusNotify">Change status and notify applicant</a>' +
+            '<a class="btn btn-primary fellapp-data-confirm-ok fellapp-data-confirm-ok-statuswithoutnotify" id="dataConfirmStatusWithoutNotify">Change status without notification</a>' +
+            //'<a class="btn btn-primary fellapp-data-confirm-ok" id="dataConfirmOK-statusnotify">OK</a>' +
+            //'<a class="btn btn-primary fellapp-data-confirm-ok" id="dataConfirmOK-statuswithoutnotify">OK</a>' +
+            //'<button style="display: none;" class="btn btn-primary data-comment-ok">OK</button>' +
+            '<button class="btn btn-default fellapp-data-confirm-cancel" data-dismiss="modal" aria-hidden="true">Cancel</button>' +
+            '</div>' +
+            '</div>' +
+            '</div>' +
+            '</div>';
+
+        $('body').append(modalHtml);
+    }
+
+    var confirmText = "<p>"+modalEl.attr('fellapp-data-confirm')+"</p>";
+
+    //var emailSubject = modalEl.attr('fellapp-data-email-subject');
+    //var emailBody = modalEl.attr('fellapp-data-email-body');
+
+    //if( emailSubject == 'acceptedEmailSubjectModified' && emailBody == 'acceptedEmailBodyModified' ) {
+    //}
+
+    if( emailWarning ) {
+        confirmText = "<p>"+emailWarning + "</p>" + confirmText;
+    }
+    if( emailSubject ) {
+        confirmText = confirmText + "<p>Subject: " + emailSubject + "</p>";
+    }
+    if( emailBody ) {
+        confirmText = confirmText + "<p>Body: " + emailBody + "</p>";
+    }
+
+    $('#fellappDataConfirmModal').find('.modal-body').html( confirmText );
+
+
+    $('#fellappDataConfirmModal').modal({show:true});
+
+    var callbackfn = modalEl.attr('fellapp-data-callback');
+    var href1 = modalEl.attr('fellapp-data-href1');
+    var href2 = modalEl.attr('fellapp-data-href2');
+    //console.log("href1="+href1);
+    //console.log("href2="+href2);
+
+    if( callbackfn ) {
+        var onclickStr1 = callbackfn+'("'+href1+'"'+',this'+')';
+        //console.log("onclickStr1="+onclickStr1);
+        $('#dataConfirmStatusNotify').attr('onclick',onclickStr1);
+
+        var onclickStr2 = callbackfn+'("'+href2+'"'+',this'+')';
+        //console.log("onclickStr2="+onclickStr2);
+        $('#dataConfirmStatusWithoutNotify').attr('onclick',onclickStr2);
+    } else {
+        $('#dataConfirmStatusNotify').attr('href', href1);
+        $('#dataConfirmStatusWithoutNotify').attr('href', href2);
+    }
+
+    $('.fellapp-data-confirm-ok').on('click', function(event){
+        //alert("on modal js: dataConfirmOK clicked");
+        var footer = modalEl.closest('.modal-footer');
+        footer.html('Please wait ...');
+    });
+
+    return false;
+}
 
 // function refreshpage2(href,btnEl) {
 //     console.log('refreshpage2 href='+href);
---
+++ orderflex/src/App/FellAppBundle/Controller/FellAppUtilController.php
@@ -0,0 +1,88 @@
+<?php
+/**
+ * Copyright (c) 2017 Cornell University
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+namespace App\FellAppBundle\Controller;
+
+use App\FellAppBundle\Entity\FellowshipApplication;
+use App\UserdirectoryBundle\Controller\OrderAbstractController;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+//use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Symfony\Component\Routing\Annotation\Route;
+
+
+class FellAppUtilController extends OrderAbstractController
+{
+
+    /**
+     * @Route("/get-notification-email-infos/", name="fellapp_get_notification_email_infos", methods={"GET"}, options={"expose"=true})
+     */
+    public function GetNotificationEmailInfosAction(Request $request) {
+
+        if(
+            $this->get('security.authorization_checker')->isGranted('ROLE_FELLAPP_COORDINATOR') === false &&
+            $this->get('security.authorization_checker')->isGranted('ROLE_FELLAPP_DIRECTOR') === false )
+        {
+            return $this->redirect( $this->generateUrl('fellapp-nopermission') );
+        }
+
+        $fellappId = trim($request->get('id'));
+        $emailType = trim($request->get('emailType')); //accepted, rejected
+        
+        $fellapp = $this->getDoctrine()->getRepository('AppFellAppBundle:FellowshipApplication')->find($fellappId);
+        if( !$fellapp ) {
+            throw $this->createNotFoundException('Unable to find Fellowship Application by id='.$fellappId);
+        }
+
+        if( false == $this->get('security.authorization_checker')->isGranted("update",$fellapp) ) {
+            return $this->redirect( $this->generateUrl('fellapp-nopermission') );
+        }
+
+        $fellappUtil = $this->container->get('fellapp_util');
+        $userSecUtil = $this->get('user_security_utility');
+
+        $warning = $fellappUtil->getRejectionAcceptanceEmailWarning($fellapp);
+
+        $emailSubject = $userSecUtil->getSiteSettingParameter($emailType.'EmailSubject',$this->getParameter('fellapp.sitename'));
+        $emailBody = $userSecUtil->getSiteSettingParameter($emailType.'EmailBody',$this->getParameter('fellapp.sitename'));
+
+        //$rejectedEmailSubject = $userSecUtil->getSiteSettingParameter('rejectedEmailSubject',$this->getParameter('fellapp.sitename'));
+        //$rejectedEmailBody = $userSecUtil->getSiteSettingParameter('rejectedEmailBody',$this->getParameter('fellapp.sitename'));
+
+        $subject = $fellappUtil->siteSettingsConstantReplace($emailSubject,$fellapp);
+        $body = $fellappUtil->siteSettingsConstantReplace($emailBody,$fellapp);
+
+        if( $subject && $body ) {
+            $res = array(
+                'warning' => $warning,
+                'subject' => $subject,
+                'body' => $body
+            );
+        } else {
+            $res = "NOTOK";
+        }
+
+        $response = new Response();
+        $response->setContent($res);
+        return $response;
+    }
+
+
+    
+
+}
