d712038b5389917ce6d97c1ecdf7ddedd83fab69
Oleg Ivanov <oli2002@med.cornell.edu>
2020-12-01 14:16:07 -0500

    working on moving report to old reports

--- orderflex/src/App/ResAppBundle/Util/PdfUtil.php
+++ orderflex/src/App/ResAppBundle/Util/PdfUtil.php
@@ -2038,6 +2038,7 @@ class PdfUtil {
         //$embedPdfHtml = '<embed src="'.$pdfDocumentPath.'" width="100%" height="100%" >';
         //$embedPdfHtml = '<iframe src="'.$pdfDocumentPath.'" style="width: 100%;height: 100%;border: none;"></iframe>';
         $embedPdfHtml = '<object type="application/pdf" width="400px" height="400px" data="'.$pdfDocumentPath.'"></object>';
+        //$embedPdfHtml = '<object type="application/pdf" width="100%" height="100%" data="'.$pdfDocumentPath.'"></object>';
 
         //$embedPdfHtml = '<br><br>Complete Application in PDF (ID='.$pdfDocument->getId().'):<br>' . $embedPdfHtml;
         $embedPdfHtml = '<br><br>This Complete Application in PDF will be attached to the invitation email:<br>' . $embedPdfHtml;
