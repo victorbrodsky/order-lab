1b471d6a9d6fa0059772962f613135bf3d87a254
Oleg Ivanov <oli2002@med.cornell.edu>
2021-09-27 13:06:49 -0400

    testing 57 in ios

--- orderflex/public/orderassets/AppTranslationalResearchBundle/form/js/transres-choices-plotly.js
+++ orderflex/public/orderassets/AppTranslationalResearchBundle/form/js/transres-choices-plotly.js
@@ -78,7 +78,7 @@ function transresGetCharts() {
                 ;
             transresAddErrorLine(errorMsg,'error');
 
-            //transresAddErrorLine("Unexpected Error. Please make sure that your session is not timed out and you are still logged in, or select a smaller time period for this chart. ",'error');
+            ////transresAddErrorLine("Unexpected Error. Please make sure that your session is not timed out and you are still logged in, or select a smaller time period for this chart. ",'error');
         });
 
     }
