99d446b16238c3166ff0022e508e6a72595f84a2
Oleg Ivanov <oli2002@med.cornell.edu>
2022-01-20 15:45:51 -0500

    updated packer

--- packer/centos_install.sh
+++ packer/centos_install.sh
@@ -254,7 +254,7 @@ f_install_order () {
 	echo -e ${COLOR} Clone ORDER and copy config and php.ini files, install composer ${NC}
 	ssh-keyscan github.com >> ~/.ssh/known_hosts
 	cd /usr/local/bin/
-	git clone --branch master https://github.com/victorbrodsky/order-lab.git /usr/local/bin/order-lab
+	git clone --single-branch --branch master https://github.com/victorbrodsky/order-lab.git /usr/local/bin/order-lab
 	#git clone --single-branch --branch sf4-php7 https://github.com/victorbrodsky/order-lab.git
 	
 	#chown -R apache:apache /var/www
