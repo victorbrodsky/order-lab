3d6c82945527754d5b34df106b6f96ca78b43443
Oleg Ivanov <oli2002@med.cornell.edu>
2022-01-21 11:06:20 -0500

    created mockup page for floating day

---
+++ orderflex/src/App/VacReqBundle/Controller/FloatingDayController.php
@@ -0,0 +1,232 @@
+<?php
+/**
+ * Copyright (c) 2017 Cornell University
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+namespace App\VacReqBundle\Controller;
+
+
+use App\UserdirectoryBundle\Controller\OrderAbstractController;
+//use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+//use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use App\VacReqBundle\Entity\VacReqRequest;
+use App\VacReqBundle\Form\VacReqFloatingDayType;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Form\FormError;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+//vacreq site
+
+class FloatingDayController extends OrderAbstractController
+{
+
+    /**
+     * @Route("/floating-day-request", name="vacreq_floating_day", methods={"GET"})
+     * @Template("AppVacReqBundle/FloatingDay/floating-day.html.twig")
+     */
+    public function FloatingDayAction(Request $request) {
+
+        if(
+            false == $this->get('security.authorization_checker')->isGranted('ROLE_VACREQ_OBSERVER') &&
+            false == $this->get('security.authorization_checker')->isGranted('ROLE_VACREQ_SUBMITTER') &&
+            false == $this->get('security.authorization_checker')->isGranted('ROLE_VACREQ_APPROVER') &&
+            false == $this->get('security.authorization_checker')->isGranted('ROLE_VACREQ_SUPERVISOR')
+        ) {
+            return $this->redirect( $this->generateUrl('vacreq-nopermission') );
+        }
+
+        $userServiceUtil = $this->get('user_service_utility');
+        $vacreqUtil = $this->get('vacreq_util');
+        $em = $this->getDoctrine()->getManager();
+        $user = $this->get('security.token_storage')->getToken()->getUser();
+
+        $entity = new VacReqRequest($user);
+        
+        $params = array();
+        $params['em'] = $em;
+        //$params['supervisor'] = $this->get('security.authorization_checker')->isGranted('ROLE_VACREQ_SUPERVISOR');
+
+        $floatingNote = "The Juneteenth Holiday may be used as a floating holiday 
+        only if you have an NYPH appointment. You can request a floating holiday however, 
+        it must be used in the same fiscal year ending June 30, 2022. It cannot be carried over";
+        $title = "Floating Day (Under Development)";
+        $cycle = 'new';
+
+        $form = $this->createRequestForm($entity,$cycle,$request);
+
+        $form->handleRequest($request);
+
+
+        if( $form->isSubmitted() && $form->isValid() ) { //new
+        
+        
+        }
+        
+        
+
+        return array(
+            'entity' => $entity,
+            'form' => $form->createView(),
+            'cycle' => $cycle,
+            'title' => $title,
+            'floatingNote' => $floatingNote
+        );
+    }
+
+    public function createRequestForm( $entity, $cycle, $request ) {
+
+        $em = $this->getDoctrine()->getManager();
+        $userServiceUtil = $this->get('user_service_utility');
+        $vacreqUtil = $this->get('vacreq_util');
+        $routeName = $request->get('_route');
+
+        $user = $this->get('security.token_storage')->getToken()->getUser();
+
+        $admin = false;
+        if( $this->get('security.authorization_checker')->isGranted('ROLE_VACREQ_ADMIN') ) {
+            $admin = true;
+        }
+
+        $roleApprover = false;
+        if( $this->get('security.authorization_checker')->isGranted("changestatus", $entity) ) {
+            $roleApprover = true;
+        }
+
+        $groupParams = array();
+
+        $groupParams['permissions'][] = array('objectStr'=>'VacReqRequest','actionStr'=>'create');
+        if( $this->get('security.authorization_checker')->isGranted('ROLE_VACREQ_ADMIN') == false ) {
+            $groupParams['exceptPermissions'][] = array('objectStr' => 'VacReqRequest', 'actionStr' => 'changestatus-carryover');
+        }
+
+        $tentativeInstitutions = null;
+
+        $organizationalInstitutions = $vacreqUtil->getGroupsByPermission($user,$groupParams);
+        //echo "1 organizationalInstitutions count=".count($organizationalInstitutions)."<br>";
+
+        //include this request institution to the $organizationalInstitutions array
+        $organizationalInstitutions = $vacreqUtil->addRequestInstitutionToOrgGroup( $entity, $organizationalInstitutions );
+        //echo "2 organizationalInstitutions count=".count($organizationalInstitutions)."<br>";
+
+        //include this request institution to the $tentativeInstitutions array
+        $tentativeInstitutions = $vacreqUtil->addRequestInstitutionToOrgGroup( $entity, $tentativeInstitutions, "tentativeInstitution" );
+
+        if( count($organizationalInstitutions) == 0 ) {
+            //If count($organizationalInstitutions) == 0 then try to run http://hosthame/order/directory/admin/sync-db/
+
+            if( $this->get('security.authorization_checker')->isGranted('ROLE_VACREQ_ADMIN') ) {
+                //admin user
+                $groupPageUrl = $this->generateUrl(
+                    "vacreq_approvers",
+                    array(),
+                    UrlGeneratorInterface::ABSOLUTE_URL
+                );
+                $warningMsg = "You don't have any group and/or assigned Submitter roles for the Business/Vacation Request site.".
+                    ' <a href="'.$groupPageUrl.'" target="_blank">Please create a group and/or assign a Submitter role to your user account.</a> ';
+            } else {
+                //regular user
+                $adminUsers = $em->getRepository('AppUserdirectoryBundle:User')->findUserByRole("ROLE_VACREQ_ADMIN", "infos.lastName", true);
+                $emails = array();
+                foreach ($adminUsers as $adminUser) {
+                    $singleEmail = $adminUser->getSingleEmail();
+                    if ($singleEmail) {
+                        $emails[] = $adminUser->getSingleEmail();
+                    }
+                }
+                $emailStr = "";
+                if (count($emails) > 0) {
+                    $emailStr = " Administrator email(s): " . implode(", ", $emails);
+                }
+
+                $warningMsg = "You don't have any group and/or assigned Submitter roles for the Business/Vacation Request site.".
+                    " Please contact the site administrator to create a group and/or get a Submitter role for your account.".$emailStr;
+            }
+            //Flash
+            $this->get('session')->getFlashBag()->add(
+                'warning',
+                $warningMsg
+            );
+        }
+
+        //get holidays url
+        $userSecUtil = $this->container->get('user_security_utility');
+        $holidaysUrl = $userSecUtil->getSiteSettingParameter('holidaysUrl','vacreq');
+        if( !$holidaysUrl ) {
+            throw new \InvalidArgumentException('holidaysUrl is not defined in Site Parameters.');
+        }
+        $holidaysUrl = '<a target="_blank" href="'.$holidaysUrl.'">holidays</a>';
+
+        //echo "roleApprover=".$roleApprover."<br>";
+        //echo "roleCarryOverApprover=".$roleCarryOverApprover."<br>";
+
+        $params = array(
+            'container' => $this->container,
+            'em' => $em,
+            'user' => $entity->getUser(),
+            'cycle' => $cycle,
+            'roleAdmin' => $admin,
+            'roleApprover' => $roleApprover,
+            'organizationalInstitutions' => $userServiceUtil->flipArrayLabelValue($organizationalInstitutions),
+            'tentativeInstitutions' => $userServiceUtil->flipArrayLabelValue($tentativeInstitutions),
+            'holidaysUrl' => $holidaysUrl,
+            'maxCarryOverVacationDays' => $userSecUtil->getSiteSettingParameter('maxCarryOverVacationDays','vacreq'),
+            'noteForCarryOverDays' => $userSecUtil->getSiteSettingParameter('noteForCarryOverDays','vacreq'),
+            //'maxVacationDays' => $userSecUtil->getSiteSettingParameter('maxVacationDays','vacreq'),
+            //'noteForVacationDays' => $userSecUtil->getSiteSettingParameter('noteForVacationDays','vacreq'),
+        );
+
+        $disabled = false;
+        $method = 'GET';
+
+        if( $cycle == 'show' ) {
+            $disabled = true;
+        }
+
+        if( $cycle == 'new' ) {
+            $method = 'POST';
+        }
+
+        if( $cycle == 'edit' ) {
+            $method = 'POST';
+        }
+
+        if( $cycle == 'review' ) {
+            $method = 'POST';
+        }
+
+        $params['review'] = false;
+        if( $request ) {
+            if( $routeName == 'vacreq_review' ) {
+                $params['review'] = true;
+            }
+        }
+
+        $form = $this->createForm(
+            VacReqFloatingDayType::class,
+            $entity,
+            array(
+                'form_custom_value' => $params,
+                'disabled' => $disabled,
+                'method' => $method,
+                //'action' => $action
+            )
+        );
+
+        return $form;
+    }
+
+}
---
+++ orderflex/src/App/VacReqBundle/Form/VacReqFloatingDayType.php
@@ -0,0 +1,283 @@
+<?php
+/**
+ * Copyright (c) 2017 Cornell University
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+namespace App\VacReqBundle\Form;
+
+
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\CallbackTransformer;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Doctrine\ORM\EntityRepository;
+use Symfony\Component\Form\FormEvents;
+use Symfony\Component\Form\FormEvent;
+
+use App\VacReqBundle\Form\VacReqRequestBusinessType;
+
+
+class VacReqFloatingDayType extends AbstractType
+{
+
+    protected $params;
+
+    public function formConstructor( $params )
+    {
+        $this->params = $params;
+    }
+
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $this->formConstructor($options['form_custom_value']);
+
+        //enable status radio only for admin or for reviewer
+        $readOnly = true;
+        if( $this->params['roleAdmin'] ||
+            ($this->params['review'] == true && $this->params['roleCarryOverApprover']) ) {
+            $readOnly = false;
+        }
+
+//        $builder->add('status', ChoiceType::class, array( //flipped
+//            //'disabled' => $readOnly,    //($this->params['roleAdmin'] ? false : true),
+////                'choices' => array(
+////                    //'pending' => 'Pending',
+////                    'approved' => 'Approved',
+////                    'rejected' => 'Rejected'
+////                ),
+//            'choices' => array(
+//                //'pending' => 'Pending',
+//                'Approved' => 'approved',
+//                'Rejected' => 'rejected'
+//            ),
+//            //'choices_as_values' => true,
+//            'label' => false,   //"Status:",
+//            'expanded' => true,
+//            'multiple' => false,
+//            'required' => true,
+//            //'data' => 'pending',
+//            'attr' => array('class' => 'horizontal_type_wide', 'readonly'=>$readOnly), //horizontal_type
+//        ));
+
+
+        if( $this->params['cycle'] == 'show' ) {
+            //approver
+            $builder->add('approver', EntityType::class, array(
+                'class' => 'AppUserdirectoryBundle:User',
+                'label' => "Approver:",
+                'required' => false,
+                //'disabled' => true,
+                'attr' => array('class' => 'combobox combobox-width', 'readonly'=>true),
+            ));
+        }
+
+        if( $this->params['cycle'] != 'show' && !$this->params['review'] ) {
+            $userAttr = array('class' => 'combobox combobox-width');
+            if( $this->params['review'] ) {
+                $userAttr['readonly'] = true;
+            }
+            $builder->add('submitter', EntityType::class, array(
+                'class' => 'AppUserdirectoryBundle:User',
+                'label' => "Request Submitter:",
+                'required' => true,
+                'multiple' => false,
+                //'choice_label' => 'name',
+                'attr' => $userAttr,    //array('class' => 'combobox combobox-width'),
+                //'disabled' => $readOnly,   //($this->params['review'] ? true : false),
+                'query_builder' => function (EntityRepository $er) {
+                    return $er->createQueryBuilder('user')
+                        ->leftJoin("user.infos","infos")
+                        ->leftJoin("user.employmentStatus", "employmentStatus")
+                        ->leftJoin("employmentStatus.employmentType", "employmentType")
+                        ->andWhere("user.keytype IS NOT NULL AND user.primaryPublicUserId != 'system'")
+                        ->andWhere("employmentType.name != 'Pathology Fellowship Applicant' OR employmentType.id IS NULL")
+                        ->orderBy("infos.lastName","ASC");
+                },
+            ));
+
+            $builder->add('user', EntityType::class, array(
+                'class' => 'AppUserdirectoryBundle:User',
+                'label' => "Person Away:",
+                //'required' => true,
+                'multiple' => false,
+                //'choice_label' => 'name',
+                'attr' => $userAttr,    //array('class' => 'combobox combobox-width'),
+                //'disabled' => $readOnly,   //($this->params['review'] ? true : false),
+                'query_builder' => function (EntityRepository $er) {
+                    return $er->createQueryBuilder('user')
+                        ->leftJoin("user.infos","infos")
+                        ->leftJoin("user.employmentStatus", "employmentStatus")
+                        ->leftJoin("employmentStatus.employmentType", "employmentType")
+                        ->andWhere("user.keytype IS NOT NULL AND user.primaryPublicUserId != 'system'")
+                        ->andWhere("employmentType.name != 'Pathology Fellowship Applicant' OR employmentType.id IS NULL")
+                        ->orderBy("infos.lastName","ASC");
+                },
+            ));
+        }
+
+        $builder->add('phone', null, array(
+            'label' => "Phone Number for the person away:",
+            'data' => "+1 123 456-7890",
+            'attr' => array('class' => 'form-control vacreq-phone'),
+            //'disabled' => ($this->params['review'] ? true : false)
+        ));
+
+        //floatingDayType
+        $builder->add('floatingDayType', ChoiceType::class, array(
+            'label' => "Floating Day:",
+            'choices' => array("Juneteenth"=>"Juneteenth"),
+            'required' => false,
+            'data' => "Juneteenth",
+            'mapped' => false,
+            'attr' => array('class' => 'combobox'),
+            //'disabled' => ($this->params['review'] ? true : false)
+        ));
+        
+        //worked
+        $builder->add('worked', CheckboxType::class, array(
+            'label' => 'I have worked on Juneteenth:',
+            'mapped' => false,
+            'required' => false,
+            'attr' => array('class' => 'floatingday-worked'),
+        ));
+
+        //organizationalInstitutions
+        $requiredInst = false;
+        if( count($this->params['organizationalInstitutions']) == 1 ) {
+            //echo "set org inst <br>";
+            $requiredInst = true;
+        }
+
+        //floatingDate
+        $builder->add('floatingDate', DateType::class, array(
+            'label' => "The floating day I am requesting is:",
+            'required' => false,
+            'widget' => 'single_text',
+            'format' => 'MM/dd/yyyy',
+            'mapped' => false,
+            'attr' => array('class' => 'form-control datetimepicker floatingDate', 'placeholder' => 'Floating Date', 'title'=>'The floating day I am requesting', 'data-toggle'=>'tooltip')
+        ));
+
+//        echo "organizationalInstitutions count=".count($this->params['organizationalInstitutions'])."<br>";
+//        foreach( $this->params['organizationalInstitutions'] as $tentativeInstitution ) {
+//            echo "tentativeInstitution=".$tentativeInstitution."<br>";
+//        }
+
+        //$requiredInst = true;
+        $institutionAttr = array('class' => 'combobox combobox-width vacreq-institution', 'placeholder' => 'Organizational Group');
+        if( $this->params['review'] ) {
+            $institutionAttr['readonly'] = true;
+        }
+        $builder->add('institution', ChoiceType::class, array( //flipped
+            'label' => "Organizational Group:",
+            'required' => $requiredInst,
+            'attr' => $institutionAttr, //array('class' => 'combobox combobox-width vacreq-institution', 'placeholder' => 'Organizational Group'),
+            'choices' => $this->params['organizationalInstitutions'],
+            //'choices_as_values' => true,
+            //'disabled' => ($this->params['review'] ? true : false)
+        ));
+        $builder->get('institution')
+            ->addModelTransformer(new CallbackTransformer(
+                //original from DB to form: institutionObject to institutionId
+                    function($originalInstitution) {
+                        //echo "originalInstitution=".$originalInstitution."<br>";
+                        if( is_object($originalInstitution) && $originalInstitution->getId() ) { //object
+                            return $originalInstitution->getId();
+                        }
+                        return $originalInstitution; //id
+                    },
+                    //reverse from form to DB: institutionId to institutionObject
+                    function($submittedInstitutionObject) {
+                        //echo "submittedInstitutionObject=".$submittedInstitutionObject."<br>";
+                        if( $submittedInstitutionObject ) { //id
+                            $institutionObject = $this->params['em']->getRepository('AppUserdirectoryBundle:Institution')->find($submittedInstitutionObject);
+                            return $institutionObject;
+                        }
+                        return null;
+                    }
+                )
+            );
+
+        //tentativeInstitution
+        if( $this->params['tentativeInstitutions'] && count($this->params['tentativeInstitutions']) > 0 ) {
+
+            //$readonlyTentativeInstitution = ($this->params['review'] ? true : false);
+
+            $requiredTentInst = false;
+            if (count($this->params['tentativeInstitutions']) == 1) {
+                //echo "set org inst <br>";
+                $requiredTentInst = true;
+            } else {
+                //$readonlyTentativeInstitution = false;
+            }
+            //$requiredTentInst = true;
+            $tentativeInstitutionAttr = array('class' => 'combobox combobox-width vacreq-tentativeInstitution', 'placeholder' => 'Organizational Group');
+            if( $this->params['review'] ) {
+                $tentativeInstitutionAttr['readonly'] = true;
+            }
+            $builder->add('tentativeInstitution', ChoiceType::class, array( //flipped
+                'label' => "Tentative Approval:",
+                'required' => $requiredTentInst,
+                'attr' => $tentativeInstitutionAttr, //array('class' => 'combobox combobox-width vacreq-tentativeInstitution', 'placeholder' => 'Organizational Group'),
+                'choices' => $this->params['tentativeInstitutions'],
+                //'choices_as_values' => true,
+                //'disabled' => ($this->params['review'] ? true : false)
+            ));
+            $builder->get('tentativeInstitution')
+                ->addModelTransformer(new CallbackTransformer(
+                    //original from DB to form: institutionObject to institutionId
+                        function ($originalInstitution) {
+                            //echo "originalInstitution=".$originalInstitution."<br>";
+                            if (is_object($originalInstitution) && $originalInstitution->getId()) { //object
+                                return $originalInstitution->getId();
+                            }
+                            return $originalInstitution; //id
+                        },
+                        //reverse from form to DB: institutionId to institutionObject
+                        function ($submittedInstitutionObject) {
+                            //echo "submittedInstitutionObject=".$submittedInstitutionObject."<br>";
+                            if ($submittedInstitutionObject) { //id
+                                $institutionObject = $this->params['em']->getRepository('AppUserdirectoryBundle:Institution')->find($submittedInstitutionObject);
+                                return $institutionObject;
+                            }
+                            return null;
+                        }
+                    )
+                );
+        }//if tentativeInstitutions
+
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'App\VacReqBundle\Entity\VacReqRequest',
+            'form_custom_value' => null,
+            //'csrf_protection' => false,
+        ));
+    }
+
+    public function getBlockPrefix()
+    {
+        return 'oleg_vacreqbundle_request';
+    }
+
+
+}
