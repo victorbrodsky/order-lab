e6ad78e78050909c0a09ba93e51a7055b238e445
Oleg Ivanov <oli2002@med.cornell.edu>
2022-01-25 16:48:33 -0500

    testing invoice pdf attachment path

--- orderflex/src/App/TranslationalResearchBundle/Controller/DefaultController.php
+++ orderflex/src/App/TranslationalResearchBundle/Controller/DefaultController.php
@@ -174,28 +174,30 @@ class DefaultController extends OrderAbstractController
 
         //$transresReminderUtil = $this->container->get('transres_reminder_util');
         //$res = $transresReminderUtil->sendReminderUnpaidInvoices(false,true);
-        $emailUtil = $this->container->get('user_mailer_utility');
-        $email = "oli2002@med.cornell.edu";
-        $invoice = NULL;
-        $userSecUtil = $this->container->get('user_security_utility');
-        $environment = $userSecUtil->getSiteSettingParameter('environment');
-        if( $environment == "dev" ) {
-            $invoice = $em->getRepository('AppTranslationalResearchBundle:Invoice')->find(4760); //dev
+        if(0) {
+            $emailUtil = $this->container->get('user_mailer_utility');
+            $email = "oli2002@med.cornell.edu";
+            $invoice = NULL;
+            $userSecUtil = $this->container->get('user_security_utility');
+            $environment = $userSecUtil->getSiteSettingParameter('environment');
+            if ($environment == "dev") {
+                $invoice = $em->getRepository('AppTranslationalResearchBundle:Invoice')->find(4760); //dev
+            }
+            if ($environment == "test") {
+                $invoice = $em->getRepository('AppTranslationalResearchBundle:Invoice')->find(4730); //test
+            }
+            if ($environment == "live") {
+                $invoice = $em->getRepository('AppTranslationalResearchBundle:Invoice')->find(7323); //prod
+            }
+            if (!$invoice) {
+                exit("Invoice not defined for environment=$environment");
+            }
+            $invoicePDF = $invoice->getRecentPDF();
+            $attachmentPath = $invoicePDF->getAttachmentEmailPath();
+            $emailUtil->sendEmail($email, "Test Invoice", "Test Invoice", null, $email, $attachmentPath);
+            $res = $invoice->getId() . ": attachmentPath=$attachmentPath <br>";
+            exit($res);
         }
-        if( $environment == "test" ) {
-            $invoice = $em->getRepository('AppTranslationalResearchBundle:Invoice')->find(4730); //test
-        }
-        if( $environment == "live" ) {
-            $invoice = $em->getRepository('AppTranslationalResearchBundle:Invoice')->find(7323); //prod
-        }
-        if( !$invoice ) {
-            exit("Invoice not defined for environment=$environment");
-        }
-        $invoicePDF = $invoice->getRecentPDF();
-        $attachmentPath = $invoicePDF->getAttachmentEmailPath();
-        $emailUtil->sendEmail($email, "Test Invoice", "Test Invoice", null, $email, $attachmentPath);
-        $res = $invoice->getId().": attachmentPath=$attachmentPath <br>";
-        exit($res);
 
         return array('sitename'=>$this->getParameter('translationalresearch.sitename'));
     }
