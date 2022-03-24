5bc63be8a8a661badd6fbc4f02009708c4633147
Oleg Ivanov <oli2002@med.cornell.edu>
2022-03-24 11:23:32 -0400

    testing populate all lists part A

--- orderflex/src/App/UserdirectoryBundle/Controller/AdminController.php
+++ orderflex/src/App/UserdirectoryBundle/Controller/AdminController.php
@@ -8669,6 +8669,7 @@ class AdminController extends OrderAbstractController
             $bool = hash_equals($administrator->getPassword(), $encodedPassword);
 
             echo 'testing4 <br>';
+            exit('testing res='.$res);
 
             if( !$bool ) {
                 $administrator->setPassword($encodedPassword);
