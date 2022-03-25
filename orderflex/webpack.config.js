8c628a8e3486f6158946e7998eaee7e608eb7843
Oleg Ivanov <oli2002@med.cornell.edu>
2022-03-25 17:06:23 -0400

    testing dashboard

--- orderflex/webpack.config.js
+++ orderflex/webpack.config.js
@@ -10,8 +10,8 @@ Encore
     // directory where compiled assets will be stored
     .setOutputPath('public/build/')
     // public path used by the web server to access the output path
-    .setPublicPath('/build')
-    //.setPublicPath('/order/build')
+    //.setPublicPath('/build')
+    .setPublicPath('/order/build')
     .enableReactPreset()
     // only needed for CDN's or sub-directory deploy
     //.setManifestKeyPrefix('build/')
