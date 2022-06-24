24fb8b3d65f5dc42fa6cf8a2b173b7985caecb98
Oleg Ivanov <oli2002@med.cornell.edu>
2022-06-24 10:42:55 -0400

    testing symfony 6 env

--- orderflex/public/index.php
+++ orderflex/public/index.php
@@ -1,9 +1,20 @@
 <?php
 
 use App\Kernel;
+use Symfony\Component\ErrorHandler\Debug;
+use Symfony\Component\HttpFoundation\Request;
 
 require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
 
 return function (array $context) {
     return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
+    //exit( 'debug='.$context['APP_DEBUG'] );
+
+//    Debug::enable();
+//    $kernel = new Kernel('dev', true);
+//    $request = Request::createFromGlobals();
+//    $response = $kernel->handle($request);
+//    $response->send();
+//    $kernel->terminate($request, $response);
+//    return $kernel;
 };
