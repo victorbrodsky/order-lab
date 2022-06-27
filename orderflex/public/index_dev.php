0785880e44b470c99a4d8b2c083a6dcd762918b9
Oleg Ivanov <oli2002@med.cornell.edu>
2022-06-27 17:29:10 -0400

    testing symfony 6 env - profiler working

--- orderflex/public/index_dev.php
+++ orderflex/public/index_dev.php
@@ -5,20 +5,19 @@ use App\Kernel;
 use Symfony\Component\ErrorHandler\Debug;
 use Symfony\Component\HttpFoundation\Request;
 
+
+$env = 'dev';
+$_SERVER['APP_DEBUG'] = true;
+$_SERVER['APP_ENV'] = $env;
+//putenv('APP_ENV='.$_SERVER['APP_ENV'] = $_ENV['APP_ENV'] = $env);
+
 //require dirname(__DIR__).'/config/bootstrap.php';
 //require_once dirname(__DIR__).'/vendor/autoload.php';
 require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
 
-Debug::enable();
+require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
 
-//return function (array $context) {
-//    return new Kernel('dev',true);
-//};
-
-$kernel = new Kernel('dev',true);
-//return $kernel;
-
-$request = Request::createFromGlobals();
-$response = $kernel->handle($request);
-$response->send();
-$kernel->terminate($request, $response);
+return function (array $context) {
+    //exit( 'APP_ENV='.$context['APP_ENV'].', debug='.$context['APP_DEBUG'] );
+    return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
+};
--- orderflex/src/Kernel.php
+++ orderflex/src/Kernel.php
@@ -14,6 +14,7 @@ namespace App;
 namespace App;
 
 use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
+use Symfony\Component\Dotenv\Dotenv;
 use Symfony\Component\HttpKernel\Kernel as BaseKernel;
 
 use Symfony\Component\Config\Loader\LoaderInterface;
@@ -56,27 +57,43 @@ class Kernel extends BaseKernel
 //    }
 
     ////////////// Working /////////////////
-    private const CONFIG_EXTS = '.{xml,yaml,yml}'; //'.{php,xml,yaml,yml}';
+    private const CONFIG_EXTS = '.{php,xml,yaml,yml}'; //'.{php,xml,yaml,yml}';
     public function getProjectDir(): string
     {
         return \dirname(__DIR__);
     }
 
+//    public function registerBundles(): iterable
+//    {
+//        $contents = require $this->getProjectDir().'/config/bundles.php';
+//        foreach ($contents as $class => $envs) {
+//            if ($envs[$this->environment] ?? $envs['all'] ?? false) {
+//                yield new $class();
+//            }
+//        }
+//    }
+
     protected function configureContainer(ContainerBuilder $container, LoaderInterface $loader): void
     {
+
+        //(new Dotenv(false))->loadEnv(dirname(__DIR__).'/.env');
+
+        //$container->addResource(new FileResource($this->getProjectDir().'/config/bundles.php'));
+        $container->setParameter('container.dumper.inline_class_loader', \PHP_VERSION_ID < 70400 || $this->debug);
+        $container->setParameter('container.dumper.inline_factories', true);
 
         //$container->setParameter('secret','123');
         $confDir = $this->getProjectDir().'/config';
 
-        echo "1=".$confDir.'/{packages}/*'.self::CONFIG_EXTS."<br>";
-        echo "2=".$confDir.'/{packages}/'.$this->environment.'/*'.self::CONFIG_EXTS."<br>";
-        echo "3=".$confDir.'/{services}'.self::CONFIG_EXTS."<br>";
-        echo "4=".$confDir.'/{services}_'.$this->environment.self::CONFIG_EXTS."<br>";
+//        echo "1=".$confDir.'/{packages}/*'.self::CONFIG_EXTS."<br>";
+//        echo "2=".$confDir.'/{packages}/'.$this->environment.'/*'.self::CONFIG_EXTS."<br>";
+//        echo "3=".$confDir.'/{services}'.self::CONFIG_EXTS."<br>";
+//        echo "4=".$confDir.'/{services}_'.$this->environment.self::CONFIG_EXTS."<br>";
         //exit('111');
 
         $loader->load($confDir.'/{packages}/*'.self::CONFIG_EXTS, 'glob');
-        //$loader->load($confDir.'/{packages}/'.$this->environment.'/*'.self::CONFIG_EXTS, 'glob');
+        $loader->load($confDir.'/{packages}/'.$this->environment.'/*'.self::CONFIG_EXTS, 'glob');
         $loader->load($confDir.'/{services}'.self::CONFIG_EXTS, 'glob');
-        //$loader->load($confDir.'/{services}_'.$this->environment.self::CONFIG_EXTS, 'glob');
+        $loader->load($confDir.'/{services}_'.$this->environment.self::CONFIG_EXTS, 'glob');
     }
 }
