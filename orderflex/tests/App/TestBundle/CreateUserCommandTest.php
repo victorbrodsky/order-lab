380416178caf8d28aae0b7348556e33e4f9f573d
Oleg Ivanov <oli2002@med.cornell.edu>
2022-07-05 14:11:38 -0400

    testing testing web

--- orderflex/tests/App/TestBundle/CreateUserCommandTest.php
+++ orderflex/tests/App/TestBundle/CreateUserCommandTest.php
@@ -22,8 +22,8 @@ class CreateUserCommandTest extends KernelTestCase
         $application = new Application($kernel);
 
         //$this->logIn();
-        $this->getTestClient();
-        $this->logIn();
+        //$this->getTestClient();
+        //$this->logIn();
 
         $command = $application->find('cron:statustest');
         $commandTester = new CommandTester($command);
@@ -66,68 +66,68 @@ class CreateUserCommandTest extends KernelTestCase
     }
 
 
-    public function logIn() {
-        $systemUser = $this->getUser();
-
-        $firewallContext = 'scan_auth';
-
-        // simulate $testUser being logged in
-        $this->client->loginUser($systemUser,$firewallContext);
-    }
-    public function getUser()
-    {
-        $userSecUtil = $this->client->getContainer()->get('user_security_utility');
-        $systemUser = $userSecUtil->findSystemUser();
-
-        if( !$systemUser ) {
-            $systemUser = $this->em->getRepository('AppUserdirectoryBundle:User')->findOneByPrimaryPublicUserId('Administrator');
-        }
-
-        if( !$systemUser ) {
-            $systemUser = $this->em->getRepository('AppUserdirectoryBundle:User')->findOneByPrimaryPublicUserId('administrator');
-        }
-
-        return $systemUser;
-    }
-    public function getTestClient(array $options = array(), array $server = array()) {
-        //TODO: detect if HTTP or HTTPS used by url
-
-        //To specify http channel run it as: HTTP=1 ./bin/phpunit
-        //To specify https channel (default) run it as: ./bin/phpunit
-        $channel = getenv('HTTP');
-        //echo "channel=[".$httpsChannel."]<br>";
-        if( $channel ) {
-            //echo "HTTP";
-            $httpsChannel = false;
-        } else {
-            //echo "HTTPS";
-            $httpsChannel = true;
-        }
-
-//        $client = static::createClient();
-//        $userUtil = $client->getContainer()->get('user_utility');
-//        $scheme = $userUtil->getScheme();
-//        //exit("scheme=$scheme");
-//        if( $scheme ) {
-//            if( strtolower($scheme) == 'http' ) {
-//                //echo "HTTP";
-//                $httpsChannel = false;
-//            } else {
-//                //echo "HTTPS";
-//                $httpsChannel = true;
-//            }
+//    public function logIn() {
+//        $systemUser = $this->getUser();
+//
+//        $firewallContext = 'scan_auth';
+//
+//        // simulate $testUser being logged in
+//        $this->client->loginUser($systemUser,$firewallContext);
+//    }
+//    public function getUser()
+//    {
+//        $userSecUtil = $this->client->getContainer()->get('user_security_utility');
+//        $systemUser = $userSecUtil->findSystemUser();
+//
+//        if( !$systemUser ) {
+//            $systemUser = $this->em->getRepository('AppUserdirectoryBundle:User')->findOneByPrimaryPublicUserId('Administrator');
 //        }
-
-        $client = static::createClient([], [
-            'HTTP_HOST' => '127.0.0.1',
-            'HTTP_USER_AGENT' => 'MySuperBrowser/1.0',
-            'HTTPS' => $httpsChannel
-        ]);
-
-        $this->client = $client;
-
-        //Alternative of setting HTTPS: When running on https this will follow redirect from http://127.0.0.1 to https://127.0.0.1
-        //$this->client->followRedirects();
-    }
+//
+//        if( !$systemUser ) {
+//            $systemUser = $this->em->getRepository('AppUserdirectoryBundle:User')->findOneByPrimaryPublicUserId('administrator');
+//        }
+//
+//        return $systemUser;
+//    }
+//    public function getTestClient(array $options = array(), array $server = array()) {
+//        //TODO: detect if HTTP or HTTPS used by url
+//
+//        //To specify http channel run it as: HTTP=1 ./bin/phpunit
+//        //To specify https channel (default) run it as: ./bin/phpunit
+//        $channel = getenv('HTTP');
+//        //echo "channel=[".$httpsChannel."]<br>";
+//        if( $channel ) {
+//            //echo "HTTP";
+//            $httpsChannel = false;
+//        } else {
+//            //echo "HTTPS";
+//            $httpsChannel = true;
+//        }
+//
+////        $client = static::createClient();
+////        $userUtil = $client->getContainer()->get('user_utility');
+////        $scheme = $userUtil->getScheme();
+////        //exit("scheme=$scheme");
+////        if( $scheme ) {
+////            if( strtolower($scheme) == 'http' ) {
+////                //echo "HTTP";
+////                $httpsChannel = false;
+////            } else {
+////                //echo "HTTPS";
+////                $httpsChannel = true;
+////            }
+////        }
+//
+//        $client = static::createClient([], [
+//            'HTTP_HOST' => '127.0.0.1',
+//            'HTTP_USER_AGENT' => 'MySuperBrowser/1.0',
+//            'HTTPS' => $httpsChannel
+//        ]);
+//
+//        $this->client = $client;
+//
+//        //Alternative of setting HTTPS: When running on https this will follow redirect from http://127.0.0.1 to https://127.0.0.1
+//        //$this->client->followRedirects();
+//    }
     
 }
