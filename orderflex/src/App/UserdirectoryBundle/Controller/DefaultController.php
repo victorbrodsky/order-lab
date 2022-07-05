9c96d396c556dcf0b6a188097adc50647ee0608d
Oleg Ivanov <oli2002@med.cornell.edu>
2022-07-05 13:55:55 -0400

    testing testing web

--- orderflex/src/App/UserdirectoryBundle/Controller/DefaultController.php
+++ orderflex/src/App/UserdirectoryBundle/Controller/DefaultController.php
@@ -266,7 +266,7 @@ class DefaultController extends OrderAbstractController
 
         //testing
         //$testFilesStr = "CalllogShortTest.php,UserTest.php"; //"TrpTest.php";
-        $testFilesStr = "CalllogTest.php";
+        //$testFilesStr = "CalllogTest.php";
 
         //exit('111');
 
