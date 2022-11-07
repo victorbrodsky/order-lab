88dc7837d2c260efb448321f32a738e78d92e3d6
Oleg Ivanov <oli2002@med.cornell.edu>
2022-11-07 08:55:36 -0500

    working on mount script

--- backup/mountdrive.py
+++ backup/mountdrive.py
@@ -76,7 +76,7 @@ def check_and_mountdrive(accessuser, networkfolder, localfolder, username, passw
 
     command = "sudo mount -t cifs -o"
     #command = "mount -t cifs -o"
-    command = command + " --verbose"
+    #command = command + " --verbose"
     command = command + " username='"+username+"',password='"+password+"'"
     command = command + ",uid="+str(userid)+",forceuid,gid="+str(userid)
     command = command + ",forcegid,file_mode=0664,dir_mode=0775"
