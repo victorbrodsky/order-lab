1f45abab496e04bde864cec0d162ab586bd72144
Oleg Ivanov <oli2002@med.cornell.edu>
2022-11-29 12:15:08 -0500

    working on start/end dates

--- orderflex/assets/userdates/js/Components/UserTableRow.jsx
+++ orderflex/assets/userdates/js/Components/UserTableRow.jsx
@@ -54,6 +54,30 @@ const UserTableRow = ({ data, setfunc }) => {
 };
 
 
+// <div class="input-group input-group-reg date allow-future-date">
+//     <input type="text" id="filter_startdate" name="filter[startdate]" class="datepicker form-control" placeholder="Start Date/Time">
+//         <span class="add-on input-group-addon"><i class="glyphicon glyphicon-calendar"></i></span>
+// </div>
+// <div class="container">
+//     <div class="col-sm-6" style="height:130px;">
+//         <div class="form-group">
+//             <div class='input-group date' id='datetimepicker11'>
+//                 <input type='text' class="form-control" />
+//             <span class="input-group-addon">
+//             <span class="glyphicon glyphicon-calendar">
+//             </span>
+//             </span>
+//             </div>
+//         </div>
+//     </div>
+//     <script type="text/javascript">
+//         $(function () {
+//         $('#datetimepicker11').datetimepicker({
+//             daysOfWeekDisabled: [0, 6]
+//         });
+//     });
+//     </script>
+// </div>
 
 
 //<input type="checkbox" value=""></input>
