b9ed26de5ed8417cd794e6971245637023c9a4c6
Oleg Ivanov <oli2002@med.cornell.edu>
2022-12-14 08:20:55 -0500

    testing user dates check status

--- orderflex/assets/userdates/js/index.jsx
+++ orderflex/assets/userdates/js/index.jsx
@@ -1,8 +1,8 @@
-import React from 'react';
-import ReactDOM from "react-dom/client";
+import React from 'react'
+import ReactDOM from "react-dom/client"
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
-import '../css/index.css';
-import UserTable from './components/UserTable';
+import '../css/index.css'
+import UserTable from './components/UserTable'
 
 // ReactDOM.render(
 // <React.StrictMode>
