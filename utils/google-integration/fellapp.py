4f51b47128c5a547f30f3bb9888f8e3d9cd01506
Oleg Ivanov <oli2002@med.cornell.edu>
2023-04-21 12:57:38 -0400

    working on install gas

--- utils/google-integration/fellapp.py
+++ utils/google-integration/fellapp.py
@@ -29,7 +29,11 @@ from datetime import datetime
 import subprocess
 from subprocess import PIPE
 from subprocess import check_output
-
+import glob, shutil
+#import filecmp
+#from pathlib import Path
+import webbrowser
+import re
 
 DIR = ""
 TITLE = ""
@@ -47,16 +51,125 @@ def help():
         "-H, --help             this help"
     )
 
-def install( dir, title ):
-    command = "clasp login"
-    #command = "clasp --version"
-    output = runCommand(command.strip())
+def install( dest_dir, title ):
 
-    command = "clasp create --type webapp --title " + title + " --rootDir " + dir
-    output = runCommand(command.strip())
+    output = []
+
+    # 1) Go to this folder and login to your Google Account: $ clasp login
+    command = "clasp login"
+    res = runCommand(command.strip())
+    output.append(res)
+
+    # 2) Create new folder, for example “MyFellowshipApplication”
+    if not os.path.exists(dest_dir):
+        os.makedirs(dest_dir)
+    else:
+        res = "Folder already exists: " + dest_dir
+        output.append(res)
+        print(res)
+        return output
+
+    #test
+    # projectid = "1qOC476n4UCg2lfWzAUSbdg7uRGX3reTCHK9PcNDBDogqGpYw969kmBSO"
+    # command = "clasp open "+projectid
+    # res = runCommand(command.strip())
+    # #output.append(res)
+    # return output
+
+    # 3) Create a new Apps Script project: $ clasp create --title “MyFellApp” --type webapp
+    dest_path = os.path.abspath(dest_dir)
+    command = "clasp create --type webapp --title " + title + " --rootDir " + dest_path
+    res = runCommand(command.strip())
+    output.append(res)
+
+    # 4) Copy all GAS files from C:\Users\ch3\Documents\MyDocs\WCMC\ORDER\order-lab\orderflex\src\App\FellAppBundle\Util\GoogleForm\FellowshipApplication\script
+    # to local folder "MyFellowshipApplication”, except .clasp.json
+
+    #"C:/Users/ch3/Documents/MyDocs/WCMC/ORDER/"
+    source_dir = "../../"+"orderflex/src/App/FellAppBundle/Util/GoogleForm/FellowshipApplication/script/"
+    print("source_dir="+source_dir)
+
+    #Get absolute path
+    source_path = os.path.abspath(source_dir)
+    dest_path = os.path.abspath(dest_dir)
+    print("source_path=", source_path)
+    print("dest_path=", dest_path)
+
+    #Prevent over write the original GAS files in the source directoy
+    if source_path == dest_path:
+        res = "Source and destination folders are the same. You can not overwrite the original GAS files in the source directory"
+        print(res)
+        output.append(res)
+        return output
+
+    #Copy all files except .clasp.json
+    copyfiles(source_dir, dest_dir, "*.*")
+
+    # 5) Push all files from local folder to Google Drive: $ clasp push
+    #Switch to dest_dir
+    os.chdir(dest_dir)
+
+    print("push files to Google Drive")
+    command = "clasp push -f"
+    res = runCommand(command.strip())
+    #print("push res=" + res)
+    output.append(res)
+
+    # 6) Create new version: $ clasp version
+    # This command displays the newly created version number 1.
+    command = "clasp version 'auto created Fellapp script'"
+    res = runCommand(command.strip())
+    #print("version="+res)
+    output.append(res)
+
+    # 7) Using that version number, you can deploy instances of your project: $ clasp deploy -V 1
+    command = "clasp deploy -V 1"
+    res = runCommand(command.strip())
+    #- AKfycbyPTUG0fNRdb0QO-DdHB3KYG356b8GPr5YhxDEzHJxut8wI782U4e4u45w-VqFMqZJN @1.
+    #deploymentId = str(res).replace("b'- ",'')
+    #deploymentId = deploymentId.replace("@1.",'')
+    #deploymentId = deploymentId.strip()
+    #print("1 deploymentId=" + deploymentId)
+
+    res = str(res)
+    res = res.strip()
+    print("res=" + res)
+    deploymentId = find_between_r( res, "- ", "@1" )
+    deploymentId = deploymentId.strip()
+    print("2 deploymentId=" + deploymentId)
+    output.append(res)
+
+    # Open project script and set permission by running Code.gs
+    command = "clasp open"
+    #command = "clasp run Code.gs"
+    res = runCommand(command.strip())
+    output.append(res)
+
+    # 8) Test deployment web url
+    #https://script.google.com/macros/s/AKfycbypzF0jiZHcUSEVK9TV7_ZrD2llrMNN9ZHGxHi6rZlWVG8PspVfl3UmzEFO1PvJfKZW2g/exec
+    url = "https://script.google.com/macros/s/"+deploymentId+"/exec"
+    print("URL="+url)
+    webbrowser.open(url)  # Go to example.com
+    output.append(res)
 
     return output
 
+def copyfiles( source_dir, dest_dir, pattern ):
+    files = glob.iglob(os.path.join(source_dir,pattern))
+    # print("files=", len(list(files)))
+    for file in files:
+        # print(file)
+        if os.path.isfile(file):
+            print(file)
+            shutil.copy2(file, dest_dir)
+
+def find_between_r( s, first, last ):
+    try:
+        start = s.rindex( first ) + len( first )
+        end = s.rindex( last, start )
+        return s[start:end]
+    except ValueError:
+        return ""
 
 def runCommand(command):
     print("run: " + command)
@@ -132,9 +245,9 @@ def main(argv):
 
     runCommand('whoami') #testing runCommand
 
-    res = install(dir,title)
+    output = install(dir,title)
 
-    print(res)
+    print(output)
 
 if __name__ == '__main__':
     #python fellapp.py --dir "C:\Users\ch3\Documents\MyDocs\WCMC\ORDER\FellApp\MyFellowshipApplication" --title “MyFellApp”
