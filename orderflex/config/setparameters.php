f35407e1e4978c161924fb2f81d430c31065c1b4
Oleg Ivanov <oli2002@med.cornell.edu>
2023-07-26 16:19:18 -0400

    testing https

--- orderflex/config/setparameters.php
+++ orderflex/config/setparameters.php
@@ -451,7 +451,7 @@ if( $conn ) {
                 $database_password_pacsvendor = getDBParameter($row, $database_password_pacsvendor, 'pacsvendorSlideManagerDBPassword');
 
                 $connection_channel = getDBParameter($row, $connection_channel, 'connectionChannel');
-                $connection_channel = 'http'; //testing
+                //$connection_channel = 'http'; //testing
                 echo "connection_channel=[".$connection_channel."]\n"; //testing
 
                 /////////////////// mailer_dsn ///////////////////
