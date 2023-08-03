276baaf844944e8b036531c4b08a67f91725acfd
Oleg Ivanov <oli2002@med.cornell.edu>
2023-08-03 17:01:59 -0400

    before using rector

--- orderflex/rector.php
+++ orderflex/rector.php
@@ -3,14 +3,14 @@
 use Rector\Doctrine\Set\DoctrineSetList;
 use Rector\Symfony\Set\SymfonySetList;
 use Rector\Symfony\Set\SensiolabsSetList;
-use Rector\Nette\Set\NetteSetList;
+//use Rector\Nette\Set\NetteSetList;
 use Rector\Config\RectorConfig;
 
 return function (RectorConfig $rectorConfig): void {
     $rectorConfig->sets([
         DoctrineSetList::ANNOTATIONS_TO_ATTRIBUTES,
         SymfonySetList::ANNOTATIONS_TO_ATTRIBUTES,
-        NetteSetList::ANNOTATIONS_TO_ATTRIBUTES,
+        //NetteSetList::ANNOTATIONS_TO_ATTRIBUTES,
         SensiolabsSetList::FRAMEWORK_EXTRA_61,
     ]);
 };
