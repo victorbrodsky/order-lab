5d29570d1be9c1d7cd544ff1251d1b42ab417bdf
Oleg Ivanov <oli2002@med.cornell.edu>
2023-09-06 14:14:14 -0400

    disable test send invoice on live

--- orderflex/tests/App/TestBundle/TrpTest.php
+++ orderflex/tests/App/TestBundle/TrpTest.php
@@ -56,12 +56,10 @@ class TrpTest extends WebTestBase
         //disable for live env
         $userSecUtil = $this->testContainer->get('user_security_utility');
         $environment = $userSecUtil->getSiteSettingParameter('environment');
-        //echo "environment=$environment";
         if( $environment == 'live' ) {
             echo "Skip test to send invoice on live";
             return;
         }
-        //exit('111');
         return;
 
         $this->logIn();
