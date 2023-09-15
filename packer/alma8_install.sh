aaa791623424d603349847f4ef6ea91a28a1afe8
Oleg Ivanov <oli2002@med.cornell.edu>
2023-09-15 15:43:17 -0400

    testing digitalocean

--- packer/alma8_install.sh
+++ packer/alma8_install.sh
@@ -334,6 +334,9 @@ f_install_prepare () {
 	cp /etc/php.ini /etc/php_ORIG.ini
 	yes | cp /usr/local/bin/order-lab/packer/php.ini /etc/
 	
+	echo -e ${COLOR} Copy sample.config to /usr/local/bin/order-lab/utils/db-manage/postgres-manage-python/ ${NC}
+	cp /usr/local/bin/order-lab/packer/sample.config /usr/local/bin/order-lab/utils/db-manage/postgres-manage-python/db.config
+	
 	#sudo service apache2 restart
 	sudo systemctl restart httpd.service
 	
