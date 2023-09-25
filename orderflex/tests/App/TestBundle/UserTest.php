7f55f88a3c7f057b0884c7261a58614e0f56e7a9
Oleg Ivanov <oli2002@med.cornell.edu>
2023-09-25 09:23:30 -0400

    testing login

--- orderflex/tests/App/TestBundle/UserTest.php
+++ orderflex/tests/App/TestBundle/UserTest.php
@@ -25,8 +25,8 @@ class UserTest extends WebTestBase
         //$client = static::createClient();
         //$crawler = $client->request('GET', '/directory/login');
 
-        //$content = $this->client->getResponse()->getContent();
-        //exit("content=$content");
+        $content = $this->client->getResponse()->getContent();
+        exit("content=$content");
 
         $this->assertGreaterThan(
             0,
