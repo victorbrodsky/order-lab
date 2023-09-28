d06bb85b925f470a8d62d6bb32e040a680b141a9
Oleg Ivanov <oli2002@med.cornell.edu>
2023-09-28 08:53:38 -0400

    adding estimated time

--- orderflex/src/App/UserdirectoryBundle/Controller/DataBackupManagementController.php
+++ orderflex/src/App/UserdirectoryBundle/Controller/DataBackupManagementController.php
@@ -215,7 +215,7 @@ class DataBackupManagementController extends OrderAbstractController
         $si_prefix = array( 'B', 'KB', 'MB', 'GB', 'TB', 'EB', 'ZB', 'YB' );
         $base = 1024;
         $class = min((int)log($bytes , $base) , count($si_prefix) - 1);
-        echo $bytes . '<br />';
+        echo $folder.": ".$bytes . '<br />';
         $res = sprintf('%1.2f' , $bytes / pow($base,$class)) . ' ' . $si_prefix[$class] . '<br />';
         return array($base,$res);
     }
