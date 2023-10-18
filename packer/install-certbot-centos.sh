f243a6196678e5d4a15df12535f19468b6405447
Oleg Ivanov <oli2002@med.cornell.edu>
2023-10-18 16:00:50 -0400

    testing cerbot

--- packer/install-certbot-centos.sh
+++ packer/install-certbot-centos.sh
@@ -46,7 +46,7 @@ echo -e ${COLOR} install-cerbot bash script: create symbolik link ${NC}
 sudo ln -s /snap/bin/certbot /usr/bin/certbot
 
 echo -e ${COLOR} install-cerbot bash script: Get a certificate and have Certbot edit your apache configuration automatically to serve it, turning on HTTPS access in a single step ${NC}
-sudo certbot -n -v --apache --agree-tos --email oli2002@med.cornell.edu --domains "$bashdomainname"
+sudo certbot -n -v --apache --agree-tos --email oli2002@med.cornell.edu --domains www."$bashdomainname"
 
 #Result: success
 #Successfully received certificate.
