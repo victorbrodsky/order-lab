56ea97ebb9e56bfb3ff382808546b80eeec01433
Oleg Ivanov <oli2002@med.cornell.edu>
2023-10-24 15:42:00 -0400

    testing certbot

--- packer/install-certbot.sh
+++ packer/install-certbot.sh
@@ -10,8 +10,10 @@ DROPLETIP=$(ip -o route get to 8.8.8.8 | sed -n 's/.*src \([0-9.]\+\).*/\1/p')
 echo -e ${COLOR} Script install-cerbot.sh: DROPLETIP="$DROPLETIP" ${NC}
 
 #IMAGENAME='packer-1698102450' IMAGEID=142936498
-IMAGEID=$(curl http://$DROPLETIP/metadata/v1/id)
+IMAGEID="mmm"#$(curl http://$DROPLETIP/metadata/v1/id)
 #IMAGEID=$(curl https://api.digitalocean.com/v2/droplets/"$DROPLETIP")
+droplet_id=$(curl http://142.93.65.236/metadata/v1/id)
+echo "droplet id: $droplet_id"
 IMAGENAME="nnn"
 echo -e ${COLOR} *** Creating droplet IMAGENAME=$IMAGENAME, IMAGEID=$IMAGEID ... *** ${NC}
 exit 0
