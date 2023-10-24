c9ad701b08f81f740408b878f029507074306418
Oleg Ivanov <oli2002@med.cornell.edu>
2023-10-24 16:38:10 -0400

    testing certbot

--- packer/install-certbot.sh
+++ packer/install-certbot.sh
@@ -12,9 +12,11 @@ echo -e ${COLOR} *** Building VM droplet from image *** ${NC}
 LASTLINE=$(doctl compute image list --public | tail -n1) #get the last line of the public images
 echo "LASTLINE=$LASTLINE"
 
+LASTLINEINFO=( LASTLINE )
+
 echo -e ${COLOR} *** Getting the first IMAGEID and the second IMAGENAME elements from LASTLINE *** ${NC}
-IMAGEID="${$LASTLINE | awk '{print $1;}'}"
-IMAGENAME="${$LASTLINE | awk '{print $2;}'}"
+IMAGEID="${LASTLINEINFO | awk '{print $1;}'}"
+IMAGENAME="${LASTLINEINFO | awk '{print $2;}'}"
 echo -e ${COLOR} IMAGEID="$IMAGEID", IMAGENAME="$IMAGENAME" ${NC}
 
 echo -e ${COLOR} *** Creating droplet IMAGENAME=$IMAGENAME, IMAGEID=$IMAGEID ... *** ${NC}
