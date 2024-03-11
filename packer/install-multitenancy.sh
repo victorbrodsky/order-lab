20c56af7bc8ffb1cbf021f273e98878ef7a0c43b
Oleg Ivanov <oli2002@med.cornell.edu>
2024-03-11 12:39:18 -0400

    working on auto install with multi-tenancy

--- packer/install-multitenancy.sh
+++ packer/install-multitenancy.sh
@@ -33,7 +33,7 @@ f_install_haproxy () {
 	
 	echo -e ${COLOR} Copy haproxy from packer ${NC}
 	sudo mv /etc/haproxy/haproxy.cfg
-	sudo cp /usr/local/bin/order-lab/packer/haproxy.cfg /etc/haproxy
+	sudo cp /usr/local/bin/order-lab/packer/haproxy.cfg /etc/haproxy/haproxy.cfg
 
 	#https://ideneal.medium.com/how-to-export-symfony-4-environment-variables-into-front-end-application-with-encore-ed45463bee5a
 	#dotenv installed via: sudo yarn install --frozen-lockfile 
