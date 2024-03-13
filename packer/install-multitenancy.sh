c8f606199f6d6d7e0b34a5d9257f1b2cb7215e19
Oleg Ivanov <oli2002@med.cornell.edu>
2024-03-13 17:29:24 -0400

    testing on digitalocean

--- packer/install-multitenancy.sh
+++ packer/install-multitenancy.sh
@@ -82,14 +82,15 @@ COLOR='\033[1;36m'
 NC='\033[0m' # No Color
 
 tenantsArray1=(
-	"'homepagemanager' '8081' " 
-	"'tenantmanager' '8082' 'tenant-manager'"
+	"homepagemanager 8081 " 
+	"tenantmanager 8082 tenant-manager"
 	"tenantappdemo 8083 c/demo-institution/demo-department"
 	"tenantapptest 8084 c/test-institution/test-department"
 	"tenantapp1 8085 c/wcm/pathology"
 	"tenantapp2 8086 c/wcm/psychiatry"
 )
-declare -a tenantsArray=(
+#declare -a 
+tenantsArray=(
 	"homepagemanager 8081 " 
 	"tenantmanager 8082 tenant-manager"
 )
