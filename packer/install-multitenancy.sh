8d5ccecefbcc92a83152325de5277dab1cc5d5f1
Oleg Ivanov <oli2002@med.cornell.edu>
2024-03-13 15:03:08 -0400

    testing on digitalocean

--- packer/install-multitenancy.sh
+++ packer/install-multitenancy.sh
@@ -58,11 +58,13 @@ echo bashemail=$bashemail
 COLOR='\033[1;36m'
 NC='\033[0m' # No Color
 
-tenantsArray=("homepagemanager 8081 " "tenantmanager 8082 tenant-manager")
-	#"tenantappdemo 8083 c/demo-institution/demo-department"
-	#"tenantapptest 8084 c/test-institution/test-department"
-	#"tenantapp1 8085 c/wcm/pathology"
-	#"tenantapp2 8086 c/wcm/psychiatry"
+tenantsArray=(
+"homepagemanager 8081 " 
+"tenantmanager 8082 tenant-manager")
+	"tenantappdemo 8083 c/demo-institution/demo-department"
+	"tenantapptest 8084 c/test-institution/test-department"
+	"tenantapp1 8085 c/wcm/pathology"
+	"tenantapp2 8086 c/wcm/psychiatry"
 #)
 
 f_test () {
