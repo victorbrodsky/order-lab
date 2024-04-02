81ab071a072d4683ead94de2ec3da52d20b2ece0
Oleg Ivanov <oli2002@med.cornell.edu>
2024-04-02 13:18:29 -0400

    working on restart

--- orderflex/src/App/UserdirectoryBundle/Util/UserTenantUtil.php
+++ orderflex/src/App/UserdirectoryBundle/Util/UserTenantUtil.php
@@ -402,7 +402,7 @@ class UserTenantUtil
         $projectRoot = $this->container->get('kernel')->getProjectDir(); //C:\Users\ch3\Documents\MyDocs\WCMC\ORDER\order-lab\orderflex
         $haproxyRestartScript = $projectRoot.'/../utils/executables/haproxy-restart.sh';
         //shell_exec($haproxyRestartScript);
-        $output = $this->runProcessShell("/usr/bin/sudo ".$haproxyRestartScript);
+        $output = $this->runProcessShell("bash ".$haproxyRestartScript);
         //exit('end haproxy-restart.sh');
         return $output;
 
