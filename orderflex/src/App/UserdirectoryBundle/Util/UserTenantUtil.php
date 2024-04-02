606438a582319ee44c01cade2fbd32daf77b0e56
Oleg Ivanov <oli2002@med.cornell.edu>
2024-04-02 12:51:22 -0400

    working on restart

--- orderflex/src/App/UserdirectoryBundle/Util/UserTenantUtil.php
+++ orderflex/src/App/UserdirectoryBundle/Util/UserTenantUtil.php
@@ -399,11 +399,11 @@ class UserTenantUtil
         //$logger->notice("end restartHaproxy ".date('h:i:s'));
         //$this->restartTenantHttpd();
 
-        $output = shell_exec('/usr/bin/sudo /usr/bin/systemctl restart haproxy');
-        sleep(3);
-        $logger->notice("shell_exec output ".date('h:i:s') . ": ".$output);
-        echo "<pre>$output</pre>";
-        return null;
+        //$output = shell_exec('/usr/bin/sudo /usr/bin/systemctl restart haproxy');
+        //sleep(3);
+        //$logger->notice("shell_exec output ".date('h:i:s') . ": ".$output);
+        //echo "<pre>$output</pre>";
+        //return null;
 
         $logger->notice("start systemctl haproxy ".date('h:i:s'));
         $output = $this->runProcessShell("/usr/bin/sudo /usr/bin/systemctl restart haproxy", false);
