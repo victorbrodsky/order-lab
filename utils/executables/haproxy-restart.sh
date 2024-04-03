f29978e901c0e3cbe59d71091a6ec9dd257af312
Oleg Ivanov <oli2002@med.cornell.edu>
2024-04-03 08:12:38 -0400

    testing bash

--- utils/executables/haproxy-restart.sh
+++ utils/executables/haproxy-restart.sh
@@ -1,8 +1,30 @@
 #!/bin/sh
+
+POSITIONAL=()
+while [[ $# -gt 0 ]]
+do
+key="$1"
+case $key in
+	-m|--multitenant)
+		multitenant="$2"
+		shift # past argument
+		shift # past value
+    ;;
+    *)    # unknown option
+		POSITIONAL+=("$1") # save it in an array for later
+		shift # past argument
+    ;;
+esac
+done
+set -- "${POSITIONAL[@]}" # restore positional parameters
+
+echo multitenant=$multitenant
+
+sleep 1  # Waits 1 seconds.
 /usr/bin/sudo /usr/bin/systemctl restart haproxy
-sleep 3  # Waits 3 seconds.
+sleep 1  # Waits 1 seconds.
 #/usr/bin/sudo journalctl -xeu haproxy.service
 /usr/bin/sudo service php-fpm restart
-sleep 3  # Waits 3 seconds.
+sleep 1  # Waits 1 seconds.
 
 
