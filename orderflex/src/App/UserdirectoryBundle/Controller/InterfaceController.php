14ce3f1f0390b683d50861aeb7f92a162ec3e14a
Oleg Ivanov <cinava@yahoo.com>
2024-05-16 17:23:06 -0400

    working on interface transfer

---
+++ orderflex/src/App/UserdirectoryBundle/Controller/InterfaceController.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * Copyright (c) 2017 Cornell University
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ *  Created by Oleg Ivanov
+ */
+
+namespace App\UserdirectoryBundle\Controller;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Bridge\Twig\Attribute\Template;
+use Symfony\Component\Routing\Annotation\Route;
+use App\UserdirectoryBundle\Controller\OrderAbstractController;
+
+
+class InterfaceController extends OrderAbstractController
+{
+    
+    #[Route(path: '/interface-log', name: 'employees_logger_interface', methods: ['GET'])]
+    #[Template('AppUserdirectoryBundle/Logger/index.html.twig')]
+    public function indexAction(Request $request)
+    {
+        exit("Under Construction");
+
+        $params = array(
+            'sitename'=>$this->getParameter('employees.sitename')
+        );
+        return $this->listLogger($params,$request);
+    }
+
+
+}
