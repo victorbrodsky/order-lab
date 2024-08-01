e8b406905e6a41ccc9da7cb0f1af3b6b5569230a
Oleg Ivanov <cinava@yahoo.com>
2024-08-01 10:34:45 -0400

    working on 316 (5) send email gmail

--- webmonitor/webmonitor.py
+++ webmonitor/webmonitor.py
@@ -236,7 +236,7 @@ def isLocalServer(url):
 
 def main(argv):
 
-    print("\n### webmonitor.py "+datetime.now().strftime('%Y-%B-%d %H:%M:%S')+"###")
+    #print("\n### webmonitor.py "+datetime.now().strftime('%Y-%B-%d %H:%M:%S')+"###")
     #logging.basicConfig(filename='checksites.log',level=logging.INFO)
     #logging.info('main start')
 
