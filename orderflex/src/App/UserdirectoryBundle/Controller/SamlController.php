c90936e898bdaa4305603ea19f093398f689e8c2
Oleg Ivanov <cinava@yahoo.com>
2024-10-23 12:03:09 -0400

    working on saml

--- orderflex/src/App/UserdirectoryBundle/Controller/SamlController.php
+++ orderflex/src/App/UserdirectoryBundle/Controller/SamlController.php
@@ -134,8 +134,9 @@ class SamlController extends AbstractController
 //    /**
 //     * @Route("/saml/metadata/{client}", name="saml_metadata", requirements={"client"=".+"})
 //     */
+    //https://view.online/c/wcm/pathology/saml/metadata/oli2002@med.cornell.edu
     //127.0.0.1/directory/saml/metadata/oli2002@med.cornell.edu
-    #[Route(path: '/saml/metadata/{client}', name: 'user_saml_metadata', requirements: ['client' => '.+'])]
+    #[Route(path: '/directory/saml/metadata/{client}', name: 'user_saml_metadata', requirements: ['client' => '.+'])]
     public function metadata(string $client): Response
     {
         $config = $this->samlConfigProvider->getConfig($client);
