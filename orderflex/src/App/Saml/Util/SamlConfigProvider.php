39f354d0e1abd1b5f132146bf8f8d0aeb84d68fa
Oleg Ivanov <cinava@yahoo.com>
2024-10-28 13:31:25 -0400

    working on saml

--- orderflex/src/App/Saml/Util/SamlConfigProvider.php
+++ orderflex/src/App/Saml/Util/SamlConfigProvider.php
@@ -89,7 +89,7 @@ class SamlConfigProvider
         
         $scheme = $this->requestStack->getCurrentRequest()->getScheme();
         //echo "1 scheme=$scheme <br>"; //http
-        //$scheme = 'https'; //tenants are behind haproxy, therefore, schema will be http
+        $scheme = 'https'; //tenants are behind haproxy, therefore, schema will be http
         //echo "2 scheme=$scheme <br>"; //http
 
         if(isset($_SERVER['HTTP_X_FORWARDED_PROTO'])) {
