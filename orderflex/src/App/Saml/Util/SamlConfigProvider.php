c829311d25e6295caae0a8c36a4e74fd777a158f
Oleg Ivanov <cinava@yahoo.com>
2024-11-01 12:17:45 -0400

    testing key-cert saml

--- orderflex/src/App/Saml/Util/SamlConfigProvider.php
+++ orderflex/src/App/Saml/Util/SamlConfigProvider.php
@@ -220,52 +220,52 @@ class SamlConfigProvider
             'debug' => true,
             'security' => [
                 'nameIdEncrypted' => false,
-                'authnRequestsSigned' => false,
+                'authnRequestsSigned' => true,
                 'logoutRequestSigned' => false,
                 'logoutResponseSigned' => false,
-                'signMetadata' => false,
-                'wantMessagesSigned' => false,
-                'wantAssertionsEncrypted' => false,
+                'signMetadata' => true,
+                //'wantMessagesSigned' => false,
+                //'wantAssertionsEncrypted' => false,
 
-                // Indicates a requirement for the <saml:Assertion> elements received by
-                // this SP to be signed. [Metadata of the SP will offer this info]
-                'wantAssertionsSigned' => false,
-
-                // Indicates a requirement for the NameID element on the SAMLResponse
-                // received by this SP to be present.
-                'wantNameId' => true,
-
-                // Indicates a requirement for the NameID received by
-                // this SP to be encrypted.
-                'wantNameIdEncrypted' => false,
-
-                // Authentication context.
-                // Set to false and no AuthContext will be sent in the AuthNRequest.
-                // Set true or don't present this parameter and you will get an AuthContext 'exact' 'urn:oasis:names:tc:SAML:2.0:ac:classes:PasswordProtectedTransport'.
-                // Set an array with the possible auth context values: array ('urn:oasis:names:tc:SAML:2.0:ac:classes:Password', 'urn:oasis:names:tc:SAML:2.0:ac:classes:X509').
-                'requestedAuthnContext' => false,
-
-                // Indicates if the SP will validate all received xmls.
-                // (In order to validate the xml, 'strict' and 'wantXMLValidation' must be true).
-                'wantXMLValidation' => true,
-
-                // If true, SAMLResponses with an empty value at its Destination
-                // attribute will not be rejected for this fact.
-                'relaxDestinationValidation' => true, //false,
-
-                // If true, the toolkit will not raised an error when the Statement Element
-                // contain atribute elements with name duplicated
-                'allowRepeatAttributeName' => false,
-
-                // If true, Destination URL should strictly match to the address to
-                // which the response has been sent.
-                // Notice that if 'relaxDestinationValidation' is true an empty Destination
-                // will be accepted.
-                'destinationStrictlyMatches' => false,
-
-                // If true, SAMLResponses with an InResponseTo value will be rejected if not
-                // AuthNRequest ID provided to the validation method.
-                'rejectUnsolicitedResponsesWithInResponseTo' => false,
+//                // Indicates a requirement for the <saml:Assertion> elements received by
+//                // this SP to be signed. [Metadata of the SP will offer this info]
+//                'wantAssertionsSigned' => false,
+//
+//                // Indicates a requirement for the NameID element on the SAMLResponse
+//                // received by this SP to be present.
+//                'wantNameId' => true,
+//
+//                // Indicates a requirement for the NameID received by
+//                // this SP to be encrypted.
+//                'wantNameIdEncrypted' => false,
+//
+//                // Authentication context.
+//                // Set to false and no AuthContext will be sent in the AuthNRequest.
+//                // Set true or don't present this parameter and you will get an AuthContext 'exact' 'urn:oasis:names:tc:SAML:2.0:ac:classes:PasswordProtectedTransport'.
+//                // Set an array with the possible auth context values: array ('urn:oasis:names:tc:SAML:2.0:ac:classes:Password', 'urn:oasis:names:tc:SAML:2.0:ac:classes:X509').
+//                'requestedAuthnContext' => false,
+//
+//                // Indicates if the SP will validate all received xmls.
+//                // (In order to validate the xml, 'strict' and 'wantXMLValidation' must be true).
+//                'wantXMLValidation' => true,
+//
+//                // If true, SAMLResponses with an empty value at its Destination
+//                // attribute will not be rejected for this fact.
+//                'relaxDestinationValidation' => true, //false,
+//
+//                // If true, the toolkit will not raised an error when the Statement Element
+//                // contain atribute elements with name duplicated
+//                'allowRepeatAttributeName' => false,
+//
+//                // If true, Destination URL should strictly match to the address to
+//                // which the response has been sent.
+//                // Notice that if 'relaxDestinationValidation' is true an empty Destination
+//                // will be accepted.
+//                'destinationStrictlyMatches' => false,
+//
+//                // If true, SAMLResponses with an InResponseTo value will be rejected if not
+//                // AuthNRequest ID provided to the validation method.
+//                'rejectUnsolicitedResponsesWithInResponseTo' => false,
 
                 // Algorithm that the toolkit will use on signing process. Options:
                 //    'http://www.w3.org/2000/09/xmldsig#rsa-sha1'
