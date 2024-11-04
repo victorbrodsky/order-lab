38d89836e042d9b67d5f76a284b41b0eb1fe4933
Oleg Ivanov <cinava@yahoo.com>
2024-11-04 17:45:39 -0500

    enabling public antibody

--- orderflex/src/App/TranslationalResearchBundle/Controller/DefaultController.php
+++ orderflex/src/App/TranslationalResearchBundle/Controller/DefaultController.php
@@ -3157,7 +3157,7 @@ class DefaultController extends OrderAbstractController
 
     #[Route(path: '/antibody-add-category-tag/', name: 'translationalresearch_antibody-add-category-tag', methods: ['GET'])]
     public function antibodyAddCategoryTagAction( Request $request ) {
-        exit("antibodyAddCategoryTagAction not allowed");
+        //exit("antibodyAddCategoryTagAction not allowed");
         if( false === $this->isGranted('ROLE_PLATFORM_DEPUTY_ADMIN') ) {
             return $this->redirect( $this->generateUrl($this->getParameter('employees.sitename').'-nopermission') );
         }
@@ -3330,7 +3330,7 @@ class DefaultController extends OrderAbstractController
         $dql->select('antibody');
 
         $dql->leftJoin('antibody.categoryTags','categoryTags');
-        //$dql->andWhere("categoryTags.openToPublic = TRUE");
+        $dql->andWhere("categoryTags.openToPublic = TRUE");
         $dql->orderBy("antibody.id","ASC");
 
         $query = $dql->getQuery(); //$query = $em->createQuery($dql);
