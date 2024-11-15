9790d98e01668703ce5bfde712da77a34c7132d1
Oleg Ivanov <cinava@yahoo.com>
2024-11-15 10:58:26 -0500

    chnaged view patient record to simple

--- orderflex/src/App/Saml/Controller/SamlController.php
+++ orderflex/src/App/Saml/Controller/SamlController.php
@@ -231,6 +231,9 @@ class SamlController extends OrderAbstractController //AbstractController
 
         $this->logger->notice("logoutNew: Start");
 
+        $user = $this->getUser();
+        echo "User=".$user."<br>";
+        exit('111');
         $userSecUtil = $this->container->get('user_security_utility');
         return $userSecUtil->userLogoutSymfony7();
 
@@ -276,9 +279,6 @@ class SamlController extends OrderAbstractController //AbstractController
 
             //exit('logoutNew: after logout');
 
-            $user = $this->getUser();
-            echo "User=".$user."<br>";
-            exit('111');
             $userSecUtil = $this->container->get('user_security_utility');
             return $userSecUtil->userLogout($request,$sitename='employees');
 
