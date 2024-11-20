512da9de2942310ccf296a9ec28d7277054f4292
Oleg Ivanov <cinava@yahoo.com>
2024-11-20 11:17:13 -0500

    working on saml login keep url

--- orderflex/src/App/Saml/Util/SamlConfigProvider.php
+++ orderflex/src/App/Saml/Util/SamlConfigProvider.php
@@ -173,17 +173,17 @@ class SamlConfigProvider
                 'singleLogoutService' => ['url' => $config->getIdpSloUrl()],
                 'x509cert' => $config->getIdpCert(),
                 //'privateKey' => $config->getSpPrivateKey(),
-                'url' => '',
+                //'url' => '',
             ],
             'sp' => [
                 'entityId' => $schemeAndHost,
                 'assertionConsumerService' => [
                     'url' => $schemeAndHost."saml/acs/".$client,
-                    //"binding" => "urn:oasis:names:tc:SAML:2.0:bindings:HTTP-POST"
+                    "binding" => "urn:oasis:names:tc:SAML:2.0:bindings:HTTP-POST"
                 ],
                 'singleLogoutService' => [
                     'url' => $schemeAndHost."saml/logout/".$client,
-                    //"binding" => "urn:oasis:names:tc:SAML:2.0:bindings:HTTP-Redirect"
+                    "binding" => "urn:oasis:names:tc:SAML:2.0:bindings:HTTP-Redirect"
                 ],
                 //'x509cert' => $config->getIdpCert(),
                 'privateKey' => $config->getSpPrivateKey(),
