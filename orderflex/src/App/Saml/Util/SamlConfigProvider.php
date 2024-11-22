9e646b61ae551744019156cb87f974a4ea9522c7
Oleg Ivanov <cinava@yahoo.com>
2024-11-22 13:32:03 -0500

    working on saml login keep url

--- orderflex/src/App/Saml/Util/SamlConfigProvider.php
+++ orderflex/src/App/Saml/Util/SamlConfigProvider.php
@@ -171,8 +171,8 @@ class SamlConfigProvider
                 'entityId' => $schemeAndHost."saml/metadata/".$client,
                 'singleSignOnService' => [
                     //'url' => 'https://login-proxy-test.weill.cornell.edu/idp/profile/SAML2/Redirect/SSO/'.$client, //$config->getIdpSsoUrl(),
-                    //'url' => $config->getIdpSsoUrl(),
-                    'url' => $schemeAndHost."saml/acs/".$client,
+                    'url' => $config->getIdpSsoUrl(),
+                    //'url' => $schemeAndHost."saml/acs/".$client,
                     //'binding' => 'urn:oasis:names:tc:SAML:2.0:bindings:HTTP-Redirect'
                 ],
                 'singleLogoutService' => [
