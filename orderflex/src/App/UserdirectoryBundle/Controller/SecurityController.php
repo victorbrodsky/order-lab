14b0b5e33cfb6b8f29065c0cb8e502657783e20b
Oleg Ivanov <cinava@yahoo.com>
2024-12-04 17:17:18 -0500

    working on saml logout

--- orderflex/src/App/UserdirectoryBundle/Controller/SecurityController.php
+++ orderflex/src/App/UserdirectoryBundle/Controller/SecurityController.php
@@ -799,8 +799,8 @@ class SecurityController extends OrderAbstractController
 //     * @Route("/directory/logout", name="employees_logout")
 //     * @Template()
 //     */
-    //#[Route(path: '/logout', name: 'employees_logout')]
-    public function logoutAction( Request $request, Security $security, TokenStorageInterface $tokenStorage )
+    //[Route(path: '/logout', name: 'employees_logout')]
+    public function logoutTestAction( Request $request, Security $security, TokenStorageInterface $tokenStorage )
     {
         echo "logout Action! <br>";
         exit();
