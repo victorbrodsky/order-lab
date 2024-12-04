0f07667422181c17a6e3507b71f97dc68ed2128f
Oleg Ivanov <cinava@yahoo.com>
2024-12-04 14:03:22 -0500

    working on saml logout

--- orderflex/src/App/UserdirectoryBundle/Controller/SecurityController.php
+++ orderflex/src/App/UserdirectoryBundle/Controller/SecurityController.php
@@ -829,11 +829,11 @@ class SecurityController extends OrderAbstractController
         }
 
         //Symfony logout
-        //$response = $security->logout();
+        $response = $security->logout();
         // you can also disable the csrf logout
         $response = $security->logout(false);
         $session->invalidate();
-        //$tokenStorage->setToken(null);
+        $tokenStorage->setToken(null);
 
         //$routename = $request->get('_route');
         //echo "routename=".$routename."<br>";
