a72212a4fcfcbeaf117319eec710151fea9a89b8
Oleg Ivanov <cinava@yahoo.com>
2024-12-18 09:26:22 -0500

    testing backup

--- orderflex/src/App/UserdirectoryBundle/Services/LogoutEventSubscriber.php
+++ orderflex/src/App/UserdirectoryBundle/Services/LogoutEventSubscriber.php
@@ -118,7 +118,7 @@ class LogoutEventSubscriber implements EventSubscriberInterface
 
         //invalidate_session manually
         //$this->security->setToken(null);
-        $session->invalidate(); //auto loggout flashbag message not display
+//        $session->invalidate(); //auto loggout flashbag message not display
         //$this->security->logout(false);
 
         //samlLogout will redirect by $auth->logout(); to $sitename homepage
