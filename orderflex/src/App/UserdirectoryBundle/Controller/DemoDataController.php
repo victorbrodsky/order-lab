c446c8e6d5f13455c68a6da4c808f3ee5b21dcae
Oleg Ivanov <cinava@yahoo.com>
2025-01-17 13:45:32 -0500

    working on demo db

--- orderflex/src/App/UserdirectoryBundle/Controller/DemoDataController.php
+++ orderflex/src/App/UserdirectoryBundle/Controller/DemoDataController.php
@@ -136,8 +136,8 @@ class DemoDataController extends OrderAbstractController
         //$crawler = $browser->request('GET', '/directory/user/new');
         $crawler = $browser->request('GET', 'https://view.online/c/demo-institution/demo-department/directory/login');
 
-        $content = $this->client->getResponse()->getContent();
-        exit("content=$content");
+        //$content = $this->client->getResponse()->getContent();
+        //exit("content=$content");
 
         // select the form and fill in some values
         $form = $crawler->selectButton('Log In')->form();
