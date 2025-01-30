d4e0439c1aa62aa3f6dd6c0f752687085e38fad8
Oleg Ivanov <cinava@yahoo.com>
2025-01-30 11:51:37 -0500

    testing user https

--- orderflex/tests/App/TestBundle/UserTest.php
+++ orderflex/tests/App/TestBundle/UserTest.php
@@ -46,14 +46,9 @@ class UserTest extends WebTestBase
         //$content = $this->client->getResponse()->getContent();
         //exit("content=$content");
 
-        //$this->assertGreaterThan(
-        //    0,
-        //    $crawler->filter('html:contains("Welcome to the Employee Directory!")')->count()
-        //);
-
         $this->assertGreaterThan(
             0,
-            $crawler->filter('html:contains("My Team")')->count()
+            $crawler->filter('html:contains("Welcome to the Employee Directory!")')->count()
         );
     }
 
