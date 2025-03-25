cda445b02a0798f7fcd1e2dcfc098991f50bea78
Oleg Ivanov <cinava@yahoo.com>
2025-03-25 17:40:22 -0400

    working on demo db

--- utils/scraper/scraper.py
+++ utils/scraper/scraper.py
@@ -1,8 +1,12 @@
 # import the required library
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+#set PYTHONIOENCODING=utf-8
+#export PYTHONIOENCODING=utf-8
 
 options = webdriver.ChromeOptions()
-options.add_argument("--encoding=utf-8")
+#options.add_argument("--encoding=utf-8")
 options.add_experimental_option("detach", True)
 driver = webdriver.Chrome(
     options=options,
@@ -13,11 +17,26 @@ driver = webdriver.Chrome(
 #driver = webdriver.Chrome(service=chrome_path)
 driver = webdriver.Chrome()
 
+driver.page_source.encode('utf-8')
+
 # visit your target site
-driver.get("https://www.scrapingcourse.com/ecommerce/")
+url = "https://view.online/c/demo-institution/demo-department/time-away-request/login";
+#driver.get("https://www.scrapingcourse.com/ecommerce/")
+driver.get(url)
 
 # output the full-page HTML
-print(driver.page_source)
+#print(driver.page_source)
+
+username = driver.find_element(By.ID, "display-username")
+password = driver.find_element(By.ID, "password")
+username.send_keys("administrator")
+password.send_keys("1234567890_demo")
+
+usertype = driver.find_element(By.ID, "usernametypeid_show")
+usertype.send_keys("local-user")
+
+usertype = driver.find_element(By.ID, "s2id_usernametypeid_show")
+usertype.send_keys("Local User")
 
 # release the resources allocated by Selenium and shut down the browser
 driver.quit()
