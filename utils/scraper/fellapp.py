265d9700f0fca6568ce2fa1f8e53fa7ec9d7f10a
Oleg Ivanov <cinava@yahoo.com>
2025-04-07 12:21:01 -0400

    working fellapp config

--- utils/scraper/fellapp.py
+++ utils/scraper/fellapp.py
@@ -17,6 +17,32 @@ class FellApp:
         self.users = Users(automation)
         self.existing_users = self.users.get_existing_users()
 
+    def get_fell_apps(self):
+        users = []
+        users.append({
+            'type': '1',  # 'Clinical Informatics'
+            'firstName': 'Joe',
+            'lastName': 'Simpson',
+            'displayName': 'Joe Simpson',
+            'email': 'cinava@yahoo.com'
+        })
+        users.append({
+            'type': '1',  # 'Clinical Informatics'
+            'firstName': 'Soleil',
+            'lastName': 'Teresia',
+            'displayName': 'Soleil Teresia',
+            'email': 'cinava@yahoo.com'
+        })
+        users.append({
+            'type': '1',  # 'Clinical Informatics'
+            'firstName': 'Haides',
+            'lastName': 'Neon',
+            'displayName': 'Haides Neon',
+            'email': 'cinava@yahoo.com'
+        })
+
+        return users
+
     def configs(self):
         driver = self.automation.get_driver()
         #Add Fellowship Subspecialty: https://view.online/c/demo-institution/demo-department/directory/admin/list-manager/id/1/37
@@ -57,11 +83,39 @@ class FellApp:
         except:
             print("Unable to find or create")
 
+        time.sleep(3)
+
         #Create fellowship type
+        fellowship_type_url = "https://view.online/c/demo-institution/demo-department/fellowship-applications/fellowship-types-settings"
+        driver.get(fellowship_type_url)
+
         #<a href="/c/demo-institution/demo-department/fellowship-applications/fellowship-type/edit/1">Clinical Informatics</a>
-        fellowship_type = table.find_element(By.XPATH, './/a[text()="Clinical Informatics"]')
+        #fellowship_type = table.find_element(By.XPATH, './/a[text()="Clinical Informatics"]')
+        fellowship_type = driver.find_element("xpath", "//h4/a[contains(text(), 'Clinical Informatics')]")
         if fellowship_type:
-            pass
+            fellowship_type.click()
+            time.sleep(3)
+
+            users = self.users.get_users()
+
+            #add coordinator
+            coordinator = users[2]
+
+            #s2id_oleg_fellappbundle_fellowshipSubspecialty_coordinators
+            self.automation.select_option("s2id_oleg_fellappbundle_fellowshipSubspecialty_coordinators", "CSS_SELECTOR",
+                                          ".select2-choices .select2-input",
+                                          coordinator["displayName"]
+                                          )
+
+            time.sleep(3)
+
+            WebDriverWait(driver, 10).until(
+                EC.invisibility_of_element((By.ID, "select2-drop-mask"))
+            )
+
+            #click Update button btn btn-warning
+            self.automation.click_button("btn-warning")
+
         else:
             #create new fellowship type "Clinical Informatics"
             self.automation.click_button("btn-primary")
