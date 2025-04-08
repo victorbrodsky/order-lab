38482b9e227e8fe723ea6d1524699035d204b5d9
Oleg Ivanov <cinava@yahoo.com>
2025-04-08 12:55:05 -0400

    testing fellapp config

--- utils/scraper/fellapp.py
+++ utils/scraper/fellapp.py
@@ -2,13 +2,14 @@ from web_automation import WebAutomation
 from users import Users
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
+#from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.action_chains import ActionChains
+#from selenium.webdriver.common.action_chains import ActionChains
 import time
-from datetime import date
-from dateutil.relativedelta import relativedelta
-from selenium.webdriver.support.expected_conditions import visibility_of_all_elements_located
+import datetime
+#from datetime import date
+#from dateutil.relativedelta import relativedelta
+#from selenium.webdriver.support.expected_conditions import visibility_of_all_elements_located
 from selenium.common.exceptions import NoSuchElementException
 
 
@@ -137,25 +138,59 @@ class FellApp:
 
             time.sleep(3)
             self.automation.click_button_by_id("oleg_fellappbundle_fellappfellowshipapplicationtype_save")
-            time.sleep(3)
 
-
-        #Create a new fellapp https://view.online/c/demo-institution/demo-department/fellowship-applications/new/
-        url = "https://view.online/c/demo-institution/demo-department/fellowship-applications/new/"
-        driver.get(fellowship_type_url)
-
-        time.sleep(10)
+        time.sleep(3)
 
     def create_fellapps(self):
         for fellapp in self.get_fell_apps():
             self.create_single_fellapp(fellapp)
             break
 
-    def create_single_fellapp(self):
+    def create_single_fellapp(self, fellapp):
         driver = self.automation.get_driver()
-        url = "https://view.online/c/demo-institution/demo-department/call-log-book/entry/new"
+        url = "https://view.online/c/demo-institution/demo-department/fellowship-applications/new/"
         driver.get(url)
         time.sleep(1)
+
+        #Create a new fellapp https://view.online/c/demo-institution/demo-department/fellowship-applications/new/
+        print("create new fellowship application")
+
+        applicant_data_element = driver.find_element(By.CSS_SELECTOR,
+                                                     "h4.panel-title > a[href='#fellowshipApplicantData']")
+        applicant_data_element.click()
+        time.sleep(3)
+
+        self.automation.select_option(
+            "s2id_oleg_fellappbundle_fellowshipapplication_fellowshipSubspecialty", "CSS_SELECTOR",
+            "#select2-drop .select2-input",
+            "Clinical Informatics"
+        )
+        time.sleep(3)
+
+        #oleg_fellappbundle_fellowshipapplication_user_infos_0_firstName
+        first_name = driver.find_element(By.ID, "oleg_fellappbundle_fellowshipapplication_user_infos_0_firstName")
+        first_name.send_keys(fellapp["firstName"])
+        #oleg_fellappbundle_fellowshipapplication_user_infos_0_lastName
+        last_name = driver.find_element(By.ID, "oleg_fellappbundle_fellowshipapplication_user_infos_0_lastName")
+        last_name.send_keys(fellapp["lastName"])
+        #oleg_fellappbundle_fellowshipapplication_user_infos_0_email
+        email = driver.find_element(By.ID, "oleg_fellappbundle_fellowshipapplication_user_infos_0_email")
+        email.send_keys(fellapp["email"])
+
+        signature = driver.find_element(By.ID, "oleg_fellappbundle_fellowshipapplication_signatureName")
+        signature.send_keys(fellapp["displayName"])
+
+        today = datetime.date.today().strftime("%m-%d-%Y")
+        signature_date = driver.find_element(By.ID, "oleg_fellappbundle_fellowshipapplication_signatureDate")
+        signature_date.clear()
+        signature_date.send_keys(today)
+
+        #click submit btn-warning
+        self.automation.click_button("btn-warning")
+
+        print("Finish new fellapp")
+        time.sleep(10)
+
 
 def main():
     url = "https://view.online/c/demo-institution/demo-department/directory/login"
@@ -166,7 +201,7 @@ def main():
 
     fellapp = FellApp(automation)
     fellapp.configs()
-    #fellapp.create_calllogs()
+    fellapp.create_fellapps()
 
     print("FellApp done!")
 
