1af036e0771abd6ddbabf9c5d09b3539ae6c80a9
Oleg Ivanov <cinava@yahoo.com>
2025-04-24 15:11:12 -0400

    testing multitenancy installation

--- packer/install_ssh.sh
+++ packer/install_ssh.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#Install sshkey for Alma, Centos
+#install_ssh.sh: Install sshkey for Alma, Centos
 
 ###
 #"echo @### Copy public key to authorized_keys: cat ~/.ssh/sshkey.pub >> ~/.ssh/authorized_keys ###",
@@ -26,14 +26,19 @@ echo "sshfingerprint=$sshfingerprint"
 
 if [ ! -z "$sshfingerprint" ]
 	then
-	  echo -e ${COLOR} SSH fingerprint exists: $sshfingerprint ${NC}
-		echo -e ${COLOR} Copy public key to authorized_keys: cat ~/.ssh/sshkey.pub >> ~/.ssh/authorized_keys ${NC}
+	  echo -e "${COLOR} SSH fingerprint exists: $sshfingerprint ${NC}"
+		echo -e "${COLOR} Copy public key to authorized_keys: cat ~/.ssh/sshkey.pub >> ~/.ssh/authorized_keys ${NC}"
 		cat ~/.ssh/sshkey.pub >> ~/.ssh/authorized_keys
 		chmod 700 /root/.ssh
 		chmod 600 /root/.ssh/authorized_keys
+
+		echo -e "${COLOR} Comment out line: PasswordAuthentication no ${NC}"
+		sed -i -e 's/PasswordAuthentication no/#PasswordAuthentication no/g' /etc/ssh/sshd_config
+
 		echo 'PubkeyAuthentication yes' >> /etc/ssh/sshd_config
 		echo 'PasswordAuthentication yes' >> /etc/ssh/sshd_config
-		echo -e ${COLOR} Restart ssh ${NC}
+
+		echo -e "${COLOR} Restart ssh ${NC}"
 		sudo systemctl restart sshd	
 	else
 		echo "File not found!"
