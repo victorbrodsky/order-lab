771927c13a88804c882b4c23e3cd97b298fc8765
Oleg Ivanov <cinava@yahoo.com>
2025-06-03 11:50:08 -0400

    update demo db verify

--- utils/scraper/create_demo_db.py
+++ utils/scraper/create_demo_db.py
@@ -254,48 +254,52 @@ def run_demos(automation, demo_ids, attempts, max_attempts, mailer_user, mailer_
 def main(mailer_user, mailer_password):
     print("script directory:", os.getcwd())  # This will show the directory where your script is running
 
+    os.system("/usr/bin/bash /srv/order-lab-tenantappdemo/orderflex/deploy.sh")
+    print("main: after deploy.sh")
+    sys.exit()
+
     run_by_symfony_command = True
     # run_by_symfony_command = False
 
-    if run_by_symfony_command is True:
+    # if run_by_symfony_command is True:
         # write output to a file
         #It's better to use: /usr/bin/php /srv/order-lab-tenantappdemo/orderflex/bin/console cron:demo-db-reset --env=prod >> /srv/order-lab-tenantappdemo/orderflex/scraper.log 2>&1
-        #Option 1
-        if 0:
-            log_file_path = "/srv/order-lab-tenantappdemo/orderflex/scraper.log"
-            if not os.path.exists(log_file_path):
-                log_file_path = os.getcwd() + "/scraper.log"
-            log_file = open(log_file_path, "w")
-            sys.stdout = log_file
-
-        #Option 2 - to be able to read in real time
-        if 0:
-            log_file_path = "/srv/order-lab-tenantappdemo/orderflex/scraper.log"
-            process = subprocess.Popen(["tail", "-f", log_file_path], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-            while True:
-                line = process.stdout.readline().decode("utf-8").strip()
-                if line:
-                    print(line)
-
-        #Option 3 - to be able to read in real time
-        if 0:
-            log_file_path = os.getcwd()+"/scraper.log"
-            #log_file_path = "/srv/order-lab-tenantappdemo/orderflex/scraper.log"
-
-            # Ensure the directory exists
-            os.makedirs(os.path.dirname(log_file_path), exist_ok=True)
-            # Create the log file if it doesn't exist
-            if not os.path.exists(log_file_path):
-                open(log_file_path, "w").close()  # Creates an empty file
-
-            with open(log_file_path, "r") as log_file:
-                log_file.seek(0, 2)  # Move to the end of the file
-                while not stop_monitoring:
-                    line = log_file.readline()
-                    if line:
-                        print(line.strip())  # Process the new log entry
-                    else:
-                        time.sleep(1)  # Wait before checking again
+        # #Option 1
+        # if 0:
+        #     log_file_path = "/srv/order-lab-tenantappdemo/orderflex/scraper.log"
+        #     if not os.path.exists(log_file_path):
+        #         log_file_path = os.getcwd() + "/scraper.log"
+        #     log_file = open(log_file_path, "w")
+        #     sys.stdout = log_file
+        #
+        # #Option 2 - to be able to read in real time
+        # if 0:
+        #     log_file_path = "/srv/order-lab-tenantappdemo/orderflex/scraper.log"
+        #     process = subprocess.Popen(["tail", "-f", log_file_path], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        #     while True:
+        #         line = process.stdout.readline().decode("utf-8").strip()
+        #         if line:
+        #             print(line)
+        #
+        # #Option 3 - to be able to read in real time
+        # if 0:
+        #     log_file_path = os.getcwd()+"/scraper.log"
+        #     #log_file_path = "/srv/order-lab-tenantappdemo/orderflex/scraper.log"
+        #
+        #     # Ensure the directory exists
+        #     os.makedirs(os.path.dirname(log_file_path), exist_ok=True)
+        #     # Create the log file if it doesn't exist
+        #     if not os.path.exists(log_file_path):
+        #         open(log_file_path, "w").close()  # Creates an empty file
+        #
+        #     with open(log_file_path, "r") as log_file:
+        #         log_file.seek(0, 2)  # Move to the end of the file
+        #         while not stop_monitoring:
+        #             line = log_file.readline()
+        #             if line:
+        #                 print(line.strip())  # Process the new log entry
+        #             else:
+        #                 time.sleep(1)  # Wait before checking again
 
     print("script directory:",os.getcwd())  # This will show the directory where your script is running
 
@@ -337,6 +341,15 @@ def main(mailer_user, mailer_password):
     # Keep running demos until all sections are successful or exceed max attempts
     while any(demo_ids.values()):
         demo_ids = run_demos(automation, demo_ids, attempts, max_attempts, mailer_user, mailer_password, run_by_symfony_command)
+
+    #clean cach: 'bash deploy.sh'
+    #automation = WebAutomation(run_by_symfony_command)
+    #automation.login_to_site()
+    #init = Init(automation)
+    #init.run_deploy()
+    #/srv/order-lab-tenantappdemo/orderflex
+    os.system("/usr/bin/bash /srv/order-lab-tenantappdemo/orderflex/deploy.sh")
+    print("main: after deploy.sh")
 
     print("All demos done!")
     #automation.quit_driver()
