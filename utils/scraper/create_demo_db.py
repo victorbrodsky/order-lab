90782d0b8b4da5917e63123c234b3c4eff3b43fd
Oleg Ivanov <cinava@yahoo.com>
2025-06-03 12:08:19 -0400

    update demo db verify

--- utils/scraper/create_demo_db.py
+++ utils/scraper/create_demo_db.py
@@ -17,7 +17,7 @@ import subprocess
 
 #run demo db generation only if value is True
 #if run successfully then set value flag to False so it does not run again second time
-def run_demos(automation, demo_ids, attempts, max_attempts, mailer_user, mailer_password, run_by_symfony_command):
+def run_demos(demo_ids, attempts, max_attempts, mailer_user, mailer_password, run_by_symfony_command):
     #run_by_symfony_command = True
     #run_by_symfony_command = False
     # Sections
@@ -255,9 +255,9 @@ def run_demos(automation, demo_ids, attempts, max_attempts, mailer_user, mailer_
 def main(mailer_user, mailer_password):
     print("script directory:", os.getcwd())  # This will show the directory where your script is running
 
-    subprocess.run(["/usr/bin/bash", "/srv/order-lab-tenantappdemo/orderflex/deploy.sh"], check=True)
-    print("main: after deploy.sh")
-    sys.exit()
+    #subprocess.run(["/usr/bin/bash", "/srv/order-lab-tenantappdemo/orderflex/deploy.sh"], check=True)
+    #print("main: after deploy.sh")
+    #sys.exit()
 
     run_by_symfony_command = True
     # run_by_symfony_command = False
@@ -272,41 +272,8 @@ def main(mailer_user, mailer_password):
         #         log_file_path = os.getcwd() + "/scraper.log"
         #     log_file = open(log_file_path, "w")
         #     sys.stdout = log_file
-        #
-        # #Option 2 - to be able to read in real time
-        # if 0:
-        #     log_file_path = "/srv/order-lab-tenantappdemo/orderflex/scraper.log"
-        #     process = subprocess.Popen(["tail", "-f", log_file_path], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        #     while True:
-        #         line = process.stdout.readline().decode("utf-8").strip()
-        #         if line:
-        #             print(line)
-        #
-        # #Option 3 - to be able to read in real time
-        # if 0:
-        #     log_file_path = os.getcwd()+"/scraper.log"
-        #     #log_file_path = "/srv/order-lab-tenantappdemo/orderflex/scraper.log"
-        #
-        #     # Ensure the directory exists
-        #     os.makedirs(os.path.dirname(log_file_path), exist_ok=True)
-        #     # Create the log file if it doesn't exist
-        #     if not os.path.exists(log_file_path):
-        #         open(log_file_path, "w").close()  # Creates an empty file
-        #
-        #     with open(log_file_path, "r") as log_file:
-        #         log_file.seek(0, 2)  # Move to the end of the file
-        #         while not stop_monitoring:
-        #             line = log_file.readline()
-        #             if line:
-        #                 print(line.strip())  # Process the new log entry
-        #             else:
-        #                 time.sleep(1)  # Wait before checking again
 
     print("script directory:",os.getcwd())  # This will show the directory where your script is running
-
-    #url = "https://view.online/c/demo-institution/demo-department/directory/login"
-    #username_text = "administrator"
-    #password_text = "1234567890_demo"
 
     if mailer_user is None:
         mailer_user = "maileruser"
@@ -315,12 +282,6 @@ def main(mailer_user, mailer_password):
         mailer_password = "mailerpassword"
 
     print("mailer_user=", mailer_user, "mailer_password=", mailer_password)
-
-    automation = None #create driver each time for a new demo to reset the memory
-    #automation = WebAutomation(run_by_symfony_command=True)
-    #automation.login_to_site(url, username_text, password_text)
-    #print("EOF testing")
-    #exit()
 
     # Add demo IDs to retry in case of failure. True flag means that this demo has to be run
     demo_ids = {
@@ -341,7 +302,7 @@ def main(mailer_user, mailer_password):
 
     # Keep running demos until all sections are successful or exceed max attempts
     while any(demo_ids.values()):
-        demo_ids = run_demos(automation, demo_ids, attempts, max_attempts, mailer_user, mailer_password, run_by_symfony_command)
+        demo_ids = run_demos(demo_ids, attempts, max_attempts, mailer_user, mailer_password, run_by_symfony_command)
 
     #clean cach: 'bash deploy.sh'
     #automation = WebAutomation(run_by_symfony_command)
@@ -349,35 +310,13 @@ def main(mailer_user, mailer_password):
     #init = Init(automation)
     #init.run_deploy()
     #/srv/order-lab-tenantappdemo/orderflex
-    #os.system("cd /srv/order-lab-tenantappdemo/orderflex/")
+    #os.chdir("/srv/order-lab-tenantappdemo/orderflex/")
     #os.system("/usr/bin/bash /srv/order-lab-tenantappdemo/orderflex/deploy.sh")
     subprocess.run(["/usr/bin/bash", "/srv/order-lab-tenantappdemo/orderflex/deploy.sh"], check=True)
     print("main: after deploy.sh")
 
     print("All demos done!")
     #automation.quit_driver()
-
-# Execute the main function
-# if __name__ == "__main1__":
-#     #password = getpass.getpass("Enter your password: ")  # Secure input
-#     user = None
-#     password = None
-#     main(user,password)
-
-# if __name__ == "__main__":
-#     if "--mailerpassword" in sys.argv:
-#         index = sys.argv.index("--mailerpassword") + 1
-#         if index < len(sys.argv):
-#             mailer_password = sys.argv[index]
-#             main(mailer_password)
-#         else:
-#             print("Error: No password provided after --mailerpassword")
-#     else:
-#         print("Error: --mailerpassword not found in arguments")
-#
-#     main()
-
-
 
 def get_arg_value(args, key):
     try:
