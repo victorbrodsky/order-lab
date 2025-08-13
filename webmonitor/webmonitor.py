d50680a9bcf38da62a4b2bcd9b1f31a4f690db02
Oleg Ivanov <cinava@yahoo.com>
2025-08-13 15:09:40 -0400

    testing webmonitor/heartbeat

--- webmonitor/webmonitor.py
+++ webmonitor/webmonitor.py
@@ -139,8 +139,8 @@ def is_url_accessible(url):
     try:
         # This will verify SSL by default
         #If using HaProxy => fail
-        #Use: response = requests.get(url, verify="/path/to/certificate.crt")
-        response = requests.get(url, timeout=5, verify="/etc/letsencrypt/live/view.online/cert_key.pem")
+        #Use: response = requests.get(url, timeout=5, verify="/path/to/certificate.crt")
+        response = requests.get(url, verify="/etc/letsencrypt/live/view.online/cert_key.pem")
         return response.status_code == 200
     except requests.exceptions.SSLError as e:
         print(f"SSL error: {e}")
