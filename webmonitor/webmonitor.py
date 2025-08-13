b32484ee88b3f0051e14b4b468007aa65fa6d334
Oleg Ivanov <cinava@yahoo.com>
2025-08-13 13:23:32 -0400

    testing webmonitor/heartbeat

--- webmonitor/webmonitor.py
+++ webmonitor/webmonitor.py
@@ -82,8 +82,8 @@ def get_site_status(url, sendSuccEmail=False):
 
     try:
         #is expired, self-signed, or invalid,  will still succeed and return a 200 if the server responds
-        #response = requests.get(url,verify=False)
-        response = requests.get(url, timeout=5)
+        response = requests.get(url,verify=False)
+        #response = requests.get(url, timeout=5)
     except:
         status = "Exception: requests.get("+url+")"
         #sendEmail(url, 'down')
