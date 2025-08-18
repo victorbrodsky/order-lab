86b5f5ea32e46d437da0848a095d051e0784568a
Oleg Ivanov <cinava@yahoo.com>
2025-08-18 11:58:43 -0400

    testing webmonitor/heartbeat with fullchain.pem

--- webmonitor/webmonitor.py
+++ webmonitor/webmonitor.py
@@ -113,7 +113,8 @@ def get_site_status(url, sendSuccEmail=False):
         #status = "Exception: requests.get("+url+")"
         #print('get_site_status: status='+status)
         sendEmail(url, 'down')
-        return 'down: except'
+        #return 'down: except'
+        return 'down'
 
     #print(response)
     #print("status_code="+str(response.status_code))
