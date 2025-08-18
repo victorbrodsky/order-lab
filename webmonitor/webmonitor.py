91e17082568077a675abba8e0032fdd559b3d846
Oleg Ivanov <cinava@yahoo.com>
2025-08-18 11:40:02 -0400

    testing webmonitor/heartbeat with fullchain.pem

--- webmonitor/webmonitor.py
+++ webmonitor/webmonitor.py
@@ -255,7 +255,7 @@ def sendEmail(url, status):
     # site is down
     emailSubject = "Site '" + url + "' appears inaccessible!"
 
-    url = urlsplit(url).path
+    #url = urlsplit(url).path
     emailBody = "Site '" + url + "' does not appear to be accessible. Please verify the site is operational! \n\n Sent by the independent script webmonitor.py from "+ENV_NAME
     send_email_alert(SENDER, RECEIVERS, emailSubject, emailBody)
     return False
