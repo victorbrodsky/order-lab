8f11f8846dbe56a68f9e206ca5c73fe0fa6711d9
Oleg Ivanov <cinava@yahoo.com>
2025-08-18 11:20:08 -0400

    testing webmonitor/heartbeat with fullchain.pem

--- webmonitor/webmonitor.py
+++ webmonitor/webmonitor.py
@@ -240,6 +240,7 @@ def get_site_status(url, sendSuccEmail=False):
 def sendEmail(url, status):
     #Remove http from url, somehow gmail has problem with urls in the email body or wcm filter them out
     #url = urlsplit(url).path
+    url = url.parse.quote()
     
     if status == "up":
         # site is up
