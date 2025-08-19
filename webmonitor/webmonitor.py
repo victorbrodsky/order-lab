4f6b09c2711e436bc4fcd0d80c89d8d4a88f0057
Oleg Ivanov <cinava@yahoo.com>
2025-08-19 14:59:37 -0400

    testing by heartbeatInput element

--- webmonitor/webmonitor.py
+++ webmonitor/webmonitor.py
@@ -44,10 +44,12 @@ import urllib
 #from check_webpage import Checker
 #from ..utils.scraper.check_webpage import Checker
 #from ..utils.scraper.check_webpage import Checker
-#scraper_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..', 'utils', 'scrapper'))
-#sys.path.insert(0, scraper_path)
-#from check_webpage import Checker
-from utils.scrapper.check_webpage import Checker
+scraper_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..', 'utils', 'scraper'))
+sys.path.insert(0, scraper_path)
+print(scraper_path)
+from check_webpage import Checker
+
+#from utils.scraper.check_webpage import Checker
 
 
 
