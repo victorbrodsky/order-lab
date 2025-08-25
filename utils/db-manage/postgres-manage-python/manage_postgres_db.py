483bf710c904c0e0764d2ae837602225cd336cd2
Oleg Ivanov <cinava@yahoo.com>
2025-08-25 17:55:02 -0400

    reviewing backup changes for multitenancy

--- utils/db-manage/postgres-manage-python/manage_postgres_db.py
+++ utils/db-manage/postgres-manage-python/manage_postgres_db.py
@@ -493,8 +493,10 @@ def main():
         #print("Backup complete.")
         logger.info("Compressing {}".format(local_file_path))
         comp_file = compress_file(local_file_path)
+
         # Delete the original file after compression
         os.remove(local_file_path)
+        logger.info(f"Deleted the temporary, not compressed db file {local_file_path} in BACKUP_PATH {manager_config.get('BACKUP_PATH')}")
 
         if storage_engine == 'LOCAL':
             logger.info('Moving {} to local storage...'.format(comp_file))
