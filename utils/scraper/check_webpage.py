50baf6a25e88a7a41c874579acc860d4ceaf92c2
Oleg Ivanov <cinava@yahoo.com>
2025-08-27 17:48:08 -0400

    testing webchecker

--- utils/scraper/check_webpage.py
+++ utils/scraper/check_webpage.py
@@ -2,6 +2,7 @@ import os
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import NoSuchElementException
+import tempfile
 
 class Checker:
     def __init__(self):
