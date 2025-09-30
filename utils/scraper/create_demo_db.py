bfd26bd1f1092c45bd3d35a09f178f9eadb29d5a
Oleg Ivanov <cinava@yahoo.com>
2025-09-30 11:57:25 -0400

    test demo db fellapp

--- utils/scraper/create_demo_db.py
+++ utils/scraper/create_demo_db.py
@@ -329,7 +329,7 @@ def main(mailer_user, mailer_password, captcha_sitekey, captcha_secretkey):
     #sys.exit()
 
     run_by_symfony_command = True
-    run_by_symfony_command = False
+    #run_by_symfony_command = False
 
     # if run_by_symfony_command is True:
         # write output to a file
