cfb3a4fd7c22f6e36e9232c1978bd9af0df35874
Oleg Ivanov <cinava@yahoo.com>
2025-10-16 15:03:21 -0400

    testing ssl ldap

--- utils/ldap/ssl_ldap.py
+++ utils/ldap/ssl_ldap.py
@@ -10,7 +10,7 @@ CAFILE  = os.getenv('LDAP_CA_FILE', '/tmp/bjc-bundle.pem')
 
 # DOMAIN\user (or UPN works too)
 #USER    = os.getenv('LDAP_NTLM_USER', r'ACCOUNTS\PATH-SVC-BindUser')
-USER = 'accounts\Path-SVC-BindUser'
+USER = 'accounts\\Path-SVC-BindUser'
 PWD     = os.getenv('LDAP_NTLM_PASS')
 BASE_DN = os.getenv('LDAP_BASE_DN', 'DC=bjc-nt,DC=bjc,DC=org')
 
