c016e1cbf1b8eaf514e455ccfdcd956355cc6f5b
Oleg Ivanov <cinava@yahoo.com>
2025-10-29 14:39:05 -0400

    testing empty global fellowship types

--- orderflex/src/App/FellAppBundle/Controller/FellAppController.php
+++ orderflex/src/App/FellAppBundle/Controller/FellAppController.php
@@ -995,9 +995,9 @@ class FellAppController extends OrderAbstractController {
 //        }
         $serverRole = $userSecUtil->getSiteSettingParameter('authServerNetwork');
         if( $serverRole."" != 'Internet (Hub)' ) {
-            $fellTypes = $fellappUtil->getFellowshipTypesByUser($user);
-            //$fellTypes = $fellappUtil->getFellowshipTypesByInstitution(true); //array of entities
-            echo "fellowshipTypes count=".count($fellTypes)."<br>";
+            //$fellTypes = $fellappUtil->getFellowshipTypesByUser($user);
+            $fellTypes = $fellappUtil->getFellowshipTypesByInstitution(true); //array of entities
+            //echo "fellowshipTypes count=".count($fellTypes)."<br>";
         } else {
             $globalFellTypes = $fellappUtil->getGlobalFellowshipTypesByInstitution($institution=null,$asArray=false); //return as entities
             //echo "globalFellTypes count=".count($globalFellTypes)."<br>";
