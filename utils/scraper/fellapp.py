4a9ecfd07a933ce5396bc633cfb8f45af05170c7
Oleg Ivanov <cinava@yahoo.com>
2025-10-30 17:34:18 -0400

    working on fellapp demo

--- utils/scraper/fellapp.py
+++ utils/scraper/fellapp.py
@@ -364,8 +364,25 @@ class FellApp:
         #     fellapp["fellowship_specialty"][0]
         # )
         # time.sleep(1)
-        signature = driver.find_element(By.ID, "oleg_fellappbundle_fellowshipapplication_trainings_4_majors")
-        signature.send_keys(fellapp["fellowship_specialty"][0])
+        #signature = driver.find_element(By.ID, "oleg_fellappbundle_fellowshipapplication_trainings_4_majors")
+        #signature.send_keys(fellapp["fellowship_specialty"][0])
+
+        ############ Post-Residency Fellowship Area of training ##############
+        wait = WebDriverWait(driver, 10)
+
+        # Step 1: Focus the Select2 input
+        select2_input = wait.until(EC.element_to_be_clickable(
+            (By.CSS_SELECTOR, "#s2id_oleg_fellappbundle_fellowshipapplication_trainings_4_majors .select2-input")))
+        select2_input.click()
+
+        # Step 2: Type 'AP' into the input
+        select2_input.send_keys(fellapp["fellowship_specialty"][0])
+
+        # Step 3: Wait for the AJAX dropdown to populate and select the first matching result
+        dropdown_option = wait.until(
+            EC.presence_of_element_located((By.CSS_SELECTOR, ".select2-results li.select2-result-selectable")))
+        dropdown_option.click()
+        ############ EOF ##############
 
         self.automation.select_option(
             "s2id_oleg_fellappbundle_fellowshipapplication_trainings_4_institution", "CSS_SELECTOR",
