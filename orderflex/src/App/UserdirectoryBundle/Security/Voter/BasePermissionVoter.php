5399c9611458d9adf0412ee7fe22575a5f17018c
Oleg Ivanov <cinava@yahoo.com>
2025-11-10 11:49:58 -0500

    testing fellapp roles

--- orderflex/src/App/UserdirectoryBundle/Security/Voter/BasePermissionVoter.php
+++ orderflex/src/App/UserdirectoryBundle/Security/Voter/BasePermissionVoter.php
@@ -220,10 +220,10 @@ abstract class BasePermissionVoter extends Voter {
         //check if the user has role with a permission $subject class name (i.e. "Patient") and "read"
         if( $this->em->getRepository(User::class)->isUserHasPermissionObjectAction( $user, $className, "read" ) ) {
             //exit('can View! exxit');
-            //echo "isUserHasPermissionObjectAction!!! className=".$className."<br>";
+            echo "isUserHasPermissionObjectAction!!! className=".$className."<br>";
             return true;
         } else {
-            //echo "can not view ".$className."<br>";
+            echo "can not view ".$className."<br>";
         }
 
         exit('can not View exit'); //testing exit
