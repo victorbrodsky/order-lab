18e6d4dd496e1292e82caa566b3ed9f36c8f0922
Oleg Ivanov <cinava@yahoo.com>
2025-11-12 14:04:25 -0500

    testing fellapp roles

--- orderflex/src/App/FellAppBundle/Controller/FellAppController.php
+++ orderflex/src/App/FellAppBundle/Controller/FellAppController.php
@@ -93,6 +93,7 @@ class FellAppController extends OrderAbstractController {
         if( $route == "fellapp_home" ) {
             if( false == $this->isGranted("read","FellowshipApplication") ){
                 //check if has role interviewer => redirect to 'fellapp_myinterviewees'
+                exit("indexAction: no permission to read FellowshipApplication"); //testing exit
                 //if( $this->isGranted("ROLE_FELLAPP_INTERVIEWER") ) {
                 if( $this->isGranted("create","Interview") ) {
                     return $this->redirect( $this->generateUrl('fellapp_myinterviewees') );
