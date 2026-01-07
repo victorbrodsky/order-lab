208434550e5d61451c55c687466459f6527b9f74
Oleg Ivanov <cinava@yahoo.com>
2026-01-07 13:09:32 -0500

    testing js users

--- orderflex/assets/userdates/js/components/UserTable.jsx
+++ orderflex/assets/userdates/js/components/UserTable.jsx
@@ -193,10 +193,10 @@ const UserTable = ({cycle}) => {
         //url = "http://view.online/c/demo-institution/demo-department/directory/users/api/?page=1";
         console.log("url2=["+url+"]"); ///c/demo-institution/demo-department/directory/users/api/?page=1
 
-        //let response = await axios.get(url);
-        let response = await axios.get(url,{withCredentials: true});
-        console.log("FULL RESPONSE DATA:", response.data);
-        console.log("response.data.results=",response.data.results);
+        let response = await axios.get(url);
+        //let response = await axios.get(url,{withCredentials: true});
+        //console.log("FULL RESPONSE DATA:", response.data);
+        //console.log("response.data.results=",response.data.results);
         let all = new Set([...allUsers, ...response.data.results]);
         const merged = [...all];
         setAllUsers([...all]);
