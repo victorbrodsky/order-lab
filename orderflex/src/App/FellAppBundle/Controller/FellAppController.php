e57ea39cd6b552e6c8cacc10b42246744d014138
Oleg Ivanov <cinava@yahoo.com>
2026-01-27 16:21:15 -0500

    testing login/logout

--- orderflex/src/App/FellAppBundle/Controller/FellAppController.php
+++ orderflex/src/App/FellAppBundle/Controller/FellAppController.php
@@ -137,6 +137,7 @@ class FellAppController extends OrderAbstractController {
             if(
                 false == $this->isGranted("read","FellowshipApplication")
             ){
+                exit('indexAction: $route='.$route);
                 return $this->redirect( $this->generateUrl('fellapp-nopermission') );
             }
         }
