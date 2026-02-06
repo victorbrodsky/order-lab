593067a1e28772624ab0b8d0f747f93b50cf427e
Oleg Ivanov <cinava@yahoo.com>
2026-02-06 18:53:05 -0500

    testing expand collapse

--- orderflex/src/App/UserdirectoryBundle/Util/UserTenantUtil.php
+++ orderflex/src/App/UserdirectoryBundle/Util/UserTenantUtil.php
@@ -1651,11 +1651,12 @@ class UserTenantUtil
             $connectionChannel = 'http';
         }
         //$connectionChannel = 'https'; //testing
+        $connectionChannel = 'http'; //testing
 
         $context = $this->container->get('router')->getContext();
 
         //$replaceContext = true;
-        $replaceContext = false;
+        //$replaceContext = false;
         if ($replaceContext) {
             //$context = $this->container->get('router')->getContext();
             //http://192.168.37.128/order/app_dev.php/fellowship-applications/download-pdf/49
