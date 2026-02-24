3eefff1b1b699275391c50ce28e31a5e378c2850
Oleg Ivanov <cinava@yahoo.com>
2026-02-24 17:50:15 -0500

    working on new users

--- orderflex/src/App/UserdirectoryBundle/Util/UserGenerator.php
+++ orderflex/src/App/UserdirectoryBundle/Util/UserGenerator.php
@@ -2214,7 +2214,7 @@ class UserGenerator {
 
             echo "new user=".$user."<br>";
 
-            if(1) {
+            if(0) {
                 $this->em->persist($user);
                 $this->em->flush();
                 $count++;
