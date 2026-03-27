af0305e92250d74809dc76a21d1eae6258bb7ddc
Oleg Ivanov <cinava@yahoo.com>
2026-03-27 19:25:56 -0400

    add recomm letter form

--- orderflex/src/App/FellAppBundle/Controller/FellAppRetrievalController.php
+++ orderflex/src/App/FellAppBundle/Controller/FellAppRetrievalController.php
@@ -449,7 +449,7 @@ class FellAppRetrievalController extends OrderAbstractController
             $apiImportKeyGlobals = $globalFellappSpecialty->getApiImportKeys();
             $values = [];
             foreach ($apiImportKeyGlobals as $keyObj) {
-                $logger->notice('getApiKeyValue='.$keyObj->getApiKeyValue());
+                $logger->notice($keyObj->getId().': getApiKeyValue='.$keyObj->getApiKeyValue());
                 $values[] = $keyObj->getApiKeyValue();
             }
             $apiImportKeysGlobalJson = json_encode($values);
