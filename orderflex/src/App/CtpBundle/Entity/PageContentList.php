e94820074ca56b3857b0aa20f414fbfb67c4bfc8
Oleg Ivanov <cinava@yahoo.com>
2026-06-12 08:11:38 -0400

    working on wiki edit

---
+++ orderflex/src/App/CtpBundle/Entity/PageContentList.php
@@ -0,0 +1,71 @@
+<?php
+/**
+ * Copyright (c) 2017 Cornell University
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+namespace App\FellAppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+
+use App\UserdirectoryBundle\Entity\ListAbstract;
+
+#[ORM\Table(name: 'ctp_pagecontent')]
+#[ORM\Entity]
+class PageContentList extends ListAbstract
+{
+
+    #[ORM\OneToMany(targetEntity: 'PageContentList', mappedBy: 'original', cascade: ['persist'])]
+    protected $synonyms;
+
+    #[ORM\ManyToOne(targetEntity: 'PageContentList', inversedBy: 'synonyms', cascade: ['persist'])]
+    #[ORM\JoinColumn(name: 'original_id', referencedColumnName: 'id', nullable: true)]
+    protected $original;
+
+
+    //Page Content: each page has header, content, footer
+    //header and footer will be static files
+    //content can be edited via wiki-style editor
+    /**
+     * Page Content
+     */
+    #[ORM\Column(type: 'text', nullable: true)]
+    private $pageContent;
+
+
+    public function __toString() {
+        return $this->name;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getPageContent()
+    {
+        return $this->pageContent;
+    }
+
+    /**
+     * @param mixed $pageContent
+     */
+    public function setPageContent($pageContent)
+    {
+        $this->pageContent = $pageContent;
+    }
+
+
+
+
+}
