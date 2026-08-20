dbf58191fde9ab5a22a0f870011cafa575e102a3
Oleg Ivanov <cinava@yahoo.com>
2026-08-20 17:59:49 -0400

    fixed fellapp test

--- orderflex/tests/App/TestBundle/CreateUserCommandTest.php
+++ orderflex/tests/App/TestBundle/CreateUserCommandTest.php
@@ -75,7 +75,7 @@ class CreateUserCommandTest extends KernelTestCase
 
         // the output of the command in the console
         $output = $commandTester->getDisplay();
-        $this->assertStringContainsString('verify fellowship application import', $output);
+        $this->assertStringContainsStringIgnoringCase('fellowship application import', $output);
     }
 
 
