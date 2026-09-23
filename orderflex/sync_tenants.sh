181d52e4644357fd4ac8940dc052ad4ed8a37209
Oleg Ivanov <cinava@yahoo.com>
2026-09-23 15:54:39 -0400

    added private folder check

--- orderflex/sync_tenants.sh
+++ orderflex/sync_tenants.sh
@@ -197,8 +197,10 @@ f_sync() {
         then
             echo -e ${COLOR} Test public/private folder for "$1" ${NC}
             git --git-dir="$homedir"/order-lab-"$1"/.git branch
-            ls -lrt "$homedir"/order-lab-"$1"/orderflex/public
-            ls -lrt "$homedir"/order-lab-"$1"/orderflex/private
+            #ls -lrt "$homedir"/order-lab-"$1"/orderflex/public
+            du -sh "$homedir"/order-lab-"$1"/orderflex/public/Uploaded
+            #ls -lrt "$homedir"/order-lab-"$1"/orderflex/private
+            du -sh "$homedir"/order-lab-"$1"/orderflex/private/Uploaded
     fi
 
 #    if [ -n "$type" ] && [ "$type" == "createdb" ]
